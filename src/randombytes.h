70067f7c1d4d9858f91283dfb2c1a2532773ebc8
Jaromil <jaromil@dyne.org>
2018-02-20 09:48:01 +0100

    imported randombytes
    copied over in luazen on build

---
+++ src/randombytes.h
@@ -0,0 +1,19 @@
+#ifndef sss_RANDOMBYTES_H
+#define sss_RANDOMBYTES_H
+
+#ifdef _WIN32
+/* Load size_t on windows */
+#include <crtdefs.h>
+#else
+#include <sys/syscall.h>
+#include <unistd.h>
+#endif /* _WIN32 */
+
+
+/*
+ * Write `n` bytes of high quality random bytes to `buf`
+ */
+int randombytes(void *buf, size_t n);
+
+
+#endif /* sss_RANDOMBYTES_H */
