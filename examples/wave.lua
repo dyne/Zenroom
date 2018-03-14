204bf233cd82ec7c3d1c3c2ec4033a6b04c2a990
Jaromil <jaromil@dyne.org>
2018-03-14 11:14:38 +0100

    reinstated io module with restriction
    be more permissive to enhance compatibility with lua scripts requiring
    io.write/read. this is useful for modeling and will have later our own
    implementation with more checks.
    
    added the wave example :^)

---
+++ examples/wave.lua
@@ -0,0 +1,34 @@
+#!/usr/bin/env zenroom
+--
+-- Copy me if you can.
+-- by parazyd
+--
+
+require "os"
+require "io"
+require "math"
+
+local clock = os.clock
+
+function sleep(n) -- seconds
+	local t0 = clock()
+	while clock() - t0 <= n do end
+end
+
+wave = 0
+t = {"*","*","*","*","*"," ","+","+","+"," "}
+
+while true do
+	print("")
+	for j = 1, math.floor(math.sin(wave)*50+50) do
+		io.write(" ")
+	end
+	for i = 1, 10 do
+		io.write(t[i])
+	end
+	temp = t[1]
+	table.remove(t, 1)
+	table.insert(t, temp)
+	wave = wave + 0.1
+	sleep(0.025)
+end
