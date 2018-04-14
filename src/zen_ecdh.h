96c5f5fce09fccfb31c5b8cc675570ff0dc5f35d
Jaromil <jaromil@dyne.org>
2018-04-14 15:34:11 +0200

    added support for more elliptic curves under ECDH
    made a factory to distribute function pointers from milagro (may be
    generated code in some future according to makefile configuration)
    also updated test. detected malfunction in goldilocks.

---
+++ src/zen_ecdh.h
@@ -0,0 +1,31 @@
+#ifndef __ZEN_ECDH_H__
+#define __ZEN_ECDH_H__
+
+#include <zen_octet.h>
+#include <pbc_support.h>
+
+typedef struct {
+	// function pointers
+	int (*ECP__KEY_PAIR_GENERATE)(csprng *R,octet *s,octet *W);
+	int (*ECP__PUBLIC_KEY_VALIDATE)(octet *W);
+	int (*ECP__SVDP_DH)(octet *s,octet *W,octet *K);
+	void (*ECP__ECIES_ENCRYPT)(int h,octet *P1,octet *P2,
+	                           csprng *R,octet *W,octet *M,int len,
+	                           octet *V,octet *C,octet *T);
+	int (*ECP__ECIES_DECRYPT)(int h,octet *P1,octet *P2,
+	                          octet *V,octet *C,octet *T,
+	                          octet *U,octet *M);
+	int (*ECP__SP_DSA)(int h,csprng *R,octet *k,octet *s,
+	                   octet *M,octet *c,octet *d);
+	int (*ECP__VP_DSA)(int h,octet *W,octet *M,octet *c,octet *d);
+	csprng *rng;
+	int keysize;
+	int hash; // hash type is also bytes length of hash
+	char curve[16]; // just short names
+	octet *pubkey;
+	int publen;
+	octet *seckey;
+	int seclen;
+} ecdh;
+
+#endif
---
+++ src/zen_ecdh_factory.c
@@ -0,0 +1,82 @@
+#include <jutils.h>
+#include <zen_ecdh.h>
+
+#include <ecdh_ED25519.h>
+#include <ecdh_NIST256.h>
+#include <ecdh_GOLDILOCKS.h>
+#include <ecdh_BN254CX.h>
+#include <ecdh_FP256BN.h>
+
+ecdh *ecdh_new_curve(lua_State *L, const char *curve) {
+	ecdh *e = NULL;
+	if(strcasecmp(curve,"ec25519")==0) {
+		e = (ecdh*)lua_newuserdata(L, sizeof(ecdh));
+		e->keysize = EGS_ED25519;
+		e->rng = NULL;
+		e->hash = HASH_TYPE_ECC_ED25519;
+		e->ECP__KEY_PAIR_GENERATE = ECP_ED25519_KEY_PAIR_GENERATE;
+		e->ECP__PUBLIC_KEY_VALIDATE	= ECP_ED25519_PUBLIC_KEY_VALIDATE;
+		e->ECP__SVDP_DH = ECP_ED25519_SVDP_DH;
+		e->ECP__ECIES_ENCRYPT = ECP_ED25519_ECIES_ENCRYPT;
+		e->ECP__ECIES_DECRYPT = ECP_ED25519_ECIES_DECRYPT;
+		e->ECP__SP_DSA = ECP_ED25519_SP_DSA;
+		e->ECP__VP_DSA = ECP_ED25519_VP_DSA;
+
+	} else if(strcasecmp(curve,"nist256")==0) {
+			e = (ecdh*)lua_newuserdata(L, sizeof(ecdh));
+			e->keysize = EGS_NIST256;
+			e->rng = NULL;
+			e->hash = HASH_TYPE_ECC_NIST256;
+			e->ECP__KEY_PAIR_GENERATE = ECP_NIST256_KEY_PAIR_GENERATE;
+			e->ECP__PUBLIC_KEY_VALIDATE	= ECP_NIST256_PUBLIC_KEY_VALIDATE;
+			e->ECP__SVDP_DH = ECP_NIST256_SVDP_DH;
+			e->ECP__ECIES_ENCRYPT = ECP_NIST256_ECIES_ENCRYPT;
+			e->ECP__ECIES_DECRYPT = ECP_NIST256_ECIES_DECRYPT;
+			e->ECP__SP_DSA = ECP_NIST256_SP_DSA;
+			e->ECP__VP_DSA = ECP_NIST256_VP_DSA;
+
+	} else if(strcasecmp(curve,"goldilocks")==0) {
+		e = (ecdh*)lua_newuserdata(L, sizeof(ecdh));
+		e->keysize = EGS_GOLDILOCKS;
+		e->rng = NULL;
+		e->hash = HASH_TYPE_ECC_GOLDILOCKS;
+		e->ECP__KEY_PAIR_GENERATE = ECP_GOLDILOCKS_KEY_PAIR_GENERATE;
+		e->ECP__PUBLIC_KEY_VALIDATE	= ECP_GOLDILOCKS_PUBLIC_KEY_VALIDATE;
+		e->ECP__SVDP_DH = ECP_GOLDILOCKS_SVDP_DH;
+		e->ECP__ECIES_ENCRYPT = ECP_GOLDILOCKS_ECIES_ENCRYPT;
+		e->ECP__ECIES_DECRYPT = ECP_GOLDILOCKS_ECIES_DECRYPT;
+		e->ECP__SP_DSA = ECP_GOLDILOCKS_SP_DSA;
+		e->ECP__VP_DSA = ECP_GOLDILOCKS_VP_DSA;
+
+	} else if(strcasecmp(curve,"bn254cx")==0) {
+		e = (ecdh*)lua_newuserdata(L, sizeof(ecdh));
+		e->keysize = EGS_BN254CX;
+		e->rng = NULL;
+		e->hash = HASH_TYPE_ECC_BN254CX;
+		e->ECP__KEY_PAIR_GENERATE = ECP_BN254CX_KEY_PAIR_GENERATE;
+		e->ECP__PUBLIC_KEY_VALIDATE	= ECP_BN254CX_PUBLIC_KEY_VALIDATE;
+		e->ECP__SVDP_DH = ECP_BN254CX_SVDP_DH;
+		e->ECP__ECIES_ENCRYPT = ECP_BN254CX_ECIES_ENCRYPT;
+		e->ECP__ECIES_DECRYPT = ECP_BN254CX_ECIES_DECRYPT;
+		e->ECP__SP_DSA = ECP_BN254CX_SP_DSA;
+		e->ECP__VP_DSA = ECP_BN254CX_VP_DSA;
+
+	} else if(strcasecmp(curve,"fp256bn")==0) {
+		e = (ecdh*)lua_newuserdata(L, sizeof(ecdh));
+		e->keysize = EGS_FP256BN;
+		e->rng = NULL;
+		e->hash = HASH_TYPE_ECC_FP256BN;
+		e->ECP__KEY_PAIR_GENERATE = ECP_FP256BN_KEY_PAIR_GENERATE;
+		e->ECP__PUBLIC_KEY_VALIDATE	= ECP_FP256BN_PUBLIC_KEY_VALIDATE;
+		e->ECP__SVDP_DH = ECP_FP256BN_SVDP_DH;
+		e->ECP__ECIES_ENCRYPT = ECP_FP256BN_ECIES_ENCRYPT;
+		e->ECP__ECIES_DECRYPT = ECP_FP256BN_ECIES_DECRYPT;
+		e->ECP__SP_DSA = ECP_FP256BN_SP_DSA;
+		e->ECP__VP_DSA = ECP_FP256BN_VP_DSA;
+
+	} else {
+		error(L, "%s: curve not found: %s",__func__,curve);
+		return NULL;
+	}
+	return e;
+}
