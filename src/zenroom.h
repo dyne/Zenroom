fb585f21fee74dd4330c488b78c178b39e74876e
Jaromil <jaromil@dyne.org>
2018-04-24 07:52:09 +0200

    exposed lower-level api with init/exec/teardown

--- src/zenroom.h
+++ src/zenroom.h
@@ -21,21 +21,23 @@
 #ifndef __ZENROOM_H__
 #define __ZENROOM_H__
 
-#define UMM_HEAP (64*1024) // 64KiB (masked with 0x7fff)
-#define MAX_FILE (64*512) // load max 32KiB files
-#define MAX_STRING 4097 // max 4KiB strings
-#define MAX_OCTET 2049 // max 2KiB octets
+/////////////////////////////////////////
+// high level api: one simple call
 
-#define LUA_BASELIBNAME "_G"
+int zenroom_exec(char *script, char *conf, char *keys,
+                 char *data, int verbosity);
 
-#define ZEN_BITS 8
-#ifndef SIZE_MAX
- #if ZEN_BITS == 32
-  #define SIZE_MAX 4294967296
- #elif ZEN_BITS == 8
-  #define SIZE_MAX 65536
- #endif
-#endif
+// in case buffers should be used instead of stdout/err file
+// descriptors, this call defines where to print out the output and
+// the maximum sizes allowed for it. Output is NULL terminated.
+int zenroom_exec_tobuf(char *script, char *conf, char *keys,
+                       char *data, int verbosity,
+                       char *stdout_buf, size_t stdout_len,
+                       char *stderr_buf, size_t stderr_len);
+////////////////////////////////////////
+
+
+// lower level api: init (exec_line*) teardown
 
 // heap initialised by the memory manager
 typedef struct {
@@ -54,10 +56,7 @@ typedef struct {
 typedef struct {
 	void *lua; // (lua_State*)
 	zen_mem_t *mem; // memory manager heap
-	// TODO: void *mem; // (umm_block*)
-	// short int mem_type;
 
-	// if !NULL then print will use these buffers
 	char *stdout_buf;
 	size_t stdout_len;
 	size_t stdout_pos;
@@ -68,13 +67,25 @@ typedef struct {
 	void *userdata; // anything passed at init (reserved for caller)
 } zenroom_t;
 
-zenroom_t *zen_init(const char *conf);
+
+zenroom_t *zen_init(const char *conf, char *keys, char *data);
+int  zen_exec_script(zenroom_t *Z, const char *script);
 void zen_teardown(zenroom_t *zenroom);
-int zenroom_exec(char *script, char *conf, char *keys,
-                 char *data, int verbosity);
-int zenroom_exec_tobuf(char *script, char *conf, char *keys,
-                       char *data, int verbosity,
-                       char *stdout_buf, size_t stdout_len,
-                       char *stderr_buf, size_t stderr_len);
+
+#define UMM_HEAP (64*1024) // 64KiB (masked with 0x7fff)
+#define MAX_FILE (64*512) // load max 32KiB files
+#define MAX_STRING 4097 // max 4KiB strings
+#define MAX_OCTET 2049 // max 2KiB octets
+
+#define LUA_BASELIBNAME "_G"
+
+#define ZEN_BITS 8
+#ifndef SIZE_MAX
+#if ZEN_BITS == 32
+#define SIZE_MAX 4294967296
+#elif ZEN_BITS == 8
+#define SIZE_MAX 65536
+#endif
+#endif
 
 #endif
