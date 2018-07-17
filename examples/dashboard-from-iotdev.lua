c7bc4d15714521dee2a5d42c723be525dc9edeee
Jaromil <jaromil@dyne.org>
2018-07-17 17:46:17 +0200

    new smart rules for the IoT pilot use-case
    this uses asymmetric crypto and a constant session key made directly
    on known public and secret keys because there is not (yet?) a session
    channel to make the key temporary.

---
+++ examples/dashboard-from-iotdev.lua
@@ -0,0 +1,39 @@
+-- a dashboard receives an ID and payload packet from an IoT device
+
+
+-- key schema
+keys_schema = schema.Record {
+   dashboard_seckey = schema.String
+}
+-- same as output in iotdev-to-dashboard
+data_schema = schema.Record {
+   device_pubkey = schema.String,
+   payload       = schema.String
+}
+-- same as payload in iotdev-to-dashboard
+payload_schema = schema.Record {
+   device_id = schema.String,
+   data      = schema.String
+}
+
+data = read_json(DATA,data_schema)
+keys = read_json(KEYS,keys_schema)
+
+dashkey = ecdh.new()
+dashkey:private(
+   octet.from_base64(keys['dashboard_seckey']))
+
+devkey = ecdh.new()
+devkey:public(
+   octet.from_base64(data['device_pubkey']))
+
+session = dashkey:session(devkey)
+
+payload = 
+   dashkey:decrypt(
+	  session,
+	  octet.from_base64(data['payload']))
+-- validate the schema
+read_json(payload:string(),payload_schema)
+-- payload is already json encoded
+print(payload:string())
---
+++ examples/iotdev-to-dashboard.lua
@@ -0,0 +1,52 @@
+-- one iot device encrypts its data and ID to a dashboard
+
+-- data schemas
+keys_schema = schema.Record {
+   device_seckey = schema.String,
+   device_id     = schema.String,
+   dashboard_key = schema.String
+}
+payload_schema = schema.Record {
+   device_id = schema.String,
+   data      = schema.String
+}
+output_schema = schema.Record {
+   device_pubkey = schema.String,
+   payload       = schema.String
+}
+
+-- import and validate keys 
+keys = read_json(KEYS, keys_schema)
+
+-- import dashboard's public key
+dashkey = ecdh.new()
+dashkey:public(
+   octet.from_base64(keys['dashboard_key']))
+-- import devices private key
+devkey = ecdh.new()
+devkey:private(
+   octet.from_base64(keys['device_seckey']))
+
+-- compute the session key using private/public keys
+-- it may change to use random, but then we need a session channel
+session = devkey:session(dashkey)
+
+-- payload is a nested json structure to be encrypted
+payload = {}
+payload['device_id'] = keys['device_id']
+payload['data']      = DATA
+schema.CheckSchema(payload, payload_schema)
+
+-- output is the packet, json formatted
+-- only the device's public key is transmitted in clear
+output = {}
+output['device_pubkey'] = devkey:public():base64()
+output['payload'] =
+   devkey:encrypt(
+	  session,
+	  octet.from_string(json.encode(payload))
+   ):base64()
+schema.CheckSchema(output, output_schema)
+
+-- print out the json packet ready to be sent
+print(json.encode(output))
