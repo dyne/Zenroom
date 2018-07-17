d8789b76e5b4814e0f7cbbce4ae6fafd17eeda5b
Jaromil <jaromil@dyne.org>
2018-07-17 13:25:05 +0200

    Fixed BIG on 32bit, Milagro renamed AMCL_CHUNK -> WORD_SIZE

--- src/zen_big_types.h
+++ src/zen_big_types.h
@@ -1,6 +1,6 @@
 #ifndef __ZEN_BIG_TYPES_H__
 #define __ZEN_BIG_TYPES_H__
-#include <amcl.h>
+#include <arch.h>
 
 #if CHUNK == 64
 #pragma message "BIGnum CHUNK size: 64bit"
@@ -17,20 +17,19 @@
 #define big_toBytes(b,a) BIG_384_58_toBytes(b,a)
 #elif CHUNK == 32
 #pragma message "BIGnum CHUNK size: 32bit"
-#include <big_383_29.h>
-#define big BIG_383_29
-#define modbytes MODBYTES_383_29
-#define big_zero(b) BIG_383_29_zero(b) 
-#define big_fromBytesLen(b,v,l) BIG_383_29_fromBytesLen(b,v,l)
-#define big_inc(b,n) BIG_383_29_inc(b,n)
-#define big_norm(b) BIG_383_29_norm(b)
-#define big_nbits(b) BIG_383_29_nbits(b)
-#define big_copy(b,a) BIG_383_29_copy(b,a)
-#define big_shr(b,a) BIG_383_29_shr(b,a)
-#define big_toBytes(b,a) BIG_383_29_toBytes(b,a)
+#include <big_384_29.h>
+#define big BIG_384_29
+#define modbytes MODBYTES_384_29
+#define big_zero(b) BIG_384_29_zero(b) 
+#define big_fromBytesLen(b,v,l) BIG_384_29_fromBytesLen(b,v,l)
+#define big_inc(b,n) BIG_384_29_inc(b,n)
+#define big_norm(b) BIG_384_29_norm(b)
+#define big_nbits(b) BIG_384_29_nbits(b)
+#define big_copy(b,a) BIG_384_29_copy(b,a)
+#define big_shr(b,a) BIG_384_29_shr(b,a)
+#define big_toBytes(b,a) BIG_384_29_toBytes(b,a)
 #elif CHUNK == 16
 #error "BIGnum CHUNK size: 16bit PLATFORM NOT SUPPORTED"
-#include <big_383_29.h>
 #endif
 
 #endif
