33051c5af63d19411088685fbd51b49381a5e54e
Jaromil <jaromil@dyne.org>
2018-07-19 17:22:22 +0200

    small improvement to zenroom_big wrapper

--- src/lua/zenroom_big.lua
+++ src/lua/zenroom_big.lua
@@ -3,6 +3,8 @@ local big = require'big'
 function big._import(s, f)
    if getmetatable(s).__name == 'zenroom.octet' then
 	  return big.new(s)
+   elseif f == nil then
+	  big.new(s)
    else
 	  -- metatable returns nil, use function to convert
 	  return big.new(f(s))
@@ -14,5 +16,7 @@ function big.hex(s, f) return big._import(s, octet.from_hex) end
 function big.base64(s) return big._import(s, octet.from_base64) end
 
 function big.string(s) return big._import(s, octet.from_string) end
+
+function big.octet(s)  return big._import(s, nil) end
 
 return big
