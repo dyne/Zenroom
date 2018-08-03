f56cebb2dd435d6034c6926c34fb118981341696
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-08-03 11:26:43 +0200

    ADD Elgamal tally ZKP - withouth ecdsa signing

--- test/elgamal/tally.lua
+++ test/elgamal/tally.lua
@@ -19,6 +19,41 @@ function readEcp(table)
 	return ECP.new(x, y)
 end
 
+function writeEcp(ecp)
+	ecp = ecp:affine()
+	local x = ecp:x()
+	local y = ecp:y()
+	
+	return { x = tostring(x), y = tostring(y) }
+end
+
+
+-- Concatenates everything and hashes it
+function to_challenge(list)
+	local c = ""
+	for i = 1, #list do
+		c = c .. tostring(list[i])
+	end
+
+	local hash = H:process(str(c))
+	return BIG.new(hash)
+end
+
+
+-- Creates a ZKP that (a,b) encrypts a value of 0
+function provezero(a, b)
+	local wx = rng:big()
+
+	local Aw = a * wx
+	local Bw = g * wx
+
+	local c = to_challenge({g, h, public, a, b, Aw, Bw})
+
+	local rx = (wx - c:modmul(private, order)) % order
+
+	return c, rx
+end
+
 function generateLookupTable(max)
 	table = {}
 
@@ -57,22 +92,39 @@ public = readEcp(DATA_TABLE["public"])
 private = readBig(KEYS_TABLE["private"])
 
 scores = DATA_TABLE['scores']
-
--- decrypt the scores, now we have h*num_votes
 scores = LAMBDA.map(scores, function(k,v) 
 	a = readEcp(v['a'])
 	b = readEcp(v['b'])
-	
-	d = decrypt(a, b)
-	return d
- end)
+
+	return { a = a, b = b}
+end)
+
+-- decrypt the scores, now we have h*num_votes
+outcome = LAMBDA.map(scores, function(k,v) return decrypt(v['a'], v['b']) end)
 
 -- Lookup on the table for the exact num of votes
-scores = LAMBDA.map(scores, function(k,v) return lookup(v, lookupTable) end)
+outcome = LAMBDA.map(outcome, function(k,v) return lookup(v, lookupTable) end)
+
+proof = LAMBDA.map(outcome, function(i,v)
+	a = scores[i]['a']
+	b = scores[i]['b']
+	num_votes = BIG.new(v)
+
+	b = b + (h * num_votes):negative()
+
+	c, rx = provezero(a, b)
+
+	return { c = tostring(c) , rx = tostring(rx) }
+end)
+
+scores = LAMBDA.map(scores, function(k,v) return { a = writeEcp(v['a']), b = writeEcp(v['b'])} end)
 
 export = JSON.encode(
    {
-      scores = scores
+      outcome = outcome,
+      proof = proof,
+      scores = scores,
+      public = writeEcp(public)
    }
 )
 print(export)
