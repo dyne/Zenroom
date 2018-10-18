abab9c73703609986eb93a11ffdb861eca4d3d82
Jordi <kozko2001@gmail.com>
2018-10-18 22:03:48 +0200

    ADD test for shows zenroom_exec_tobuf

--- src/zenroom_jni.c
+++ src/zenroom_jni.c
@@ -1,20 +1,19 @@
 #include "zenroom_jni.h"
 #include "zenroom.h"
 
+#ifdef __ANDROID
 #include <android/log.h>
-
-
+#endif
 
 JNIEXPORT jstring JNICALL Java_decode_zenroom_Zenroom_zenroom
   (JNIEnv *env, jobject obj, jstring jni_script, jstring jni_conf, jstring jni_keys, jstring jni_data) {
+
     jstring result;
     char* script = (*env)->GetStringUTFChars(env, jni_script, 0);
     char* conf = (*env)->GetStringUTFChars(env, jni_conf, 0);
     char* keys = (*env)->GetStringUTFChars(env, jni_keys, 0);
     char* data = (*env)->GetStringUTFChars(env, jni_data, 0);
 
-__android_log_write(ANDROID_LOG_ERROR, "XXX", "XXXXXX");//Or ANDROID_LOG_INFO, ...  
-    
 
     size_t outputSize = 1024 * 8;
     char *z_output = (char*)malloc(outputSize * sizeof(char));
@@ -23,8 +22,13 @@ __android_log_write(ANDROID_LOG_ERROR, "XXX", "XXXXXX");//Or ANDROID_LOG_INFO, .
 
     int ret = zenroom_exec_tobuf(script, conf, keys, data, 3, z_output, outputSize, z_error, errorSize);
 
-__android_log_write(ANDROID_LOG_ERROR, "XXX", z_output);//Or ANDROID_LOG_INFO, ...  
-__android_log_write(ANDROID_LOG_ERROR, "XXX", z_error);//Or ANDROID_LOG_INFO, ...  
+#ifdef __ANDROID__
+__android_log_print(ANDROID_LOG_VERBOSE, "MyApp", "len %i", strlen(z_output));
+__android_log_print(ANDROID_LOG_VERBOSE, "MyApp", "output %s", z_output);
+
+__android_log_print(ANDROID_LOG_VERBOSE, "MyApp", "len %i", strlen(z_error));
+__android_log_print(ANDROID_LOG_VERBOSE, "MyApp", "error %s", z_error);
+#endif
     
     (*env)->ReleaseStringUTFChars(env, jni_script, script);
     (*env)->ReleaseStringUTFChars(env, jni_conf, conf);
