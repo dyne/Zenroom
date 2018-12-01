fc3a8c565e299227f58a2bdb90445f1162e37f53
Jaromil <jaromil@dyne.org>
2018-12-01 14:05:52 +0100

    further development of zencode certificate example

--- examples/zencode_certificate.lua
+++ examples/zencode_certificate.lua
@@ -7,38 +7,35 @@ local function x(n,s)
 	  warn(s) end
 end
 
--- -- stateful globals
--- keyring = {}
--- declaration = ""
--- -- certreq = {}
--- certification = nil
--- certpriv = nil
--- certpub = nil
--- random = nil
--- order = nil
--- G = nil
+-- stateful globals
+-- TODO: use finite state machine
+whoami = nil
+keyring = nil
+declared = nil
+certreq = nil
+certificate = nil
+certpub = nil
+certpriv = nil
 
 -- crypto setup
 random = RNG.new()
 order = ECP.order()
 G = ECP.generator()
 
-schema_certificate = S.record {
-   requester = S.string,
-   authority = S.string,
-   public = S.oneof(S.hex, S.ecp),
-   declaration = S.string
-}
-
-schema_certificate = S.record {
-   requester = S.string,
-   authority = S.string,
-   declaration = S.string,
-   public = S.oneof(S.hex, S.ecp) }
-
-schema_keyring = S.record {
-   private = S.string,
-   public = S.string }
+schemas = { certificate = S.record {
+			   certpriv = S.OneOf( S.string, S.big),
+			   certpub = S.ecp,
+			   certhash = S.OneOf( S.hex, S.big),
+			   issuer = S.string },
+			declaration = S.record {
+			   requester = S.string,
+			   authority = S.string,
+			   declared = S.string,
+			   public = S.ecp },
+			keyring = S.record {
+			   private = S.string,
+			   public = S.ecp }
+		  }
 
 function keygen()
    local key = INT.new(random,order)
@@ -77,87 +74,106 @@ end
 -- steps
 
 -- request
-Given("I introduce myself as ''", function (nam)
-		 x(0,"introduce me: "..nam)
-		 whoami = nam
-end)
-Given("I am known as ''",         function (nam)
-		 x(0,"known as "..nam)
-		 whoami = nam end)
-Given("I have the '' key '' in keyring", function (keytype, keyname)
-		 x(0,"have "..keytype.." key for "..keyname)
-         local keys = JSON.decode(KEYS) -- array of keypairs
-         local keypair = L.property(keyname)(keys)
-		 assert(validate(keypair,schema_keyring),
-				"invalid keyring for "..keyname)
-		 if not keypair then
-			error("keyring not found: "..keyname)
-			return nil end
-		 keyring = {}
-         if keytype == "private" or keytype == "public" then
-            keyring[keytype] = L.property(keytype)(keypair)
-         else
-            error("invalid keytype: "..keytype)
-			return nil
-		 end
-		 if keytype == "public" then
-			-- check public key validity
-			ECP.new(hex(keyring[keytype]))
-		 end
-end)
+f_hello = function(nam) whoami = nam end
+Given("I introduce myself as ''", f_hello)
+Given("I am known as ''", f_hello)
+
+f_havekey = function (keytype, keyname)
+   local keyname = keyname or whoami
+   x(0,"have "..keytype.." key for "..keyname)
+   local keys = JSON.decode(KEYS) -- array of keypairs
+   local keypair = L.property(keyname)(keys)
+   assert(validate(keypair,schemas['keyring']),
+		  "invalid keyring for "..keyname)
+   if not keypair then
+	  error("keyring not found: "..keyname)
+	  return nil end
+   keyring = {}
+   -- saved in keyring
+   if keytype == "private" or keytype == "public" then
+	  keyring[keytype] = L.property(keytype)(keypair)
+   else
+	  error("invalid keytype: "..keytype)
+	  return nil
+   end
+   if keytype == "public" then
+	  -- check public key validity
+	  ECP.new(hex(keyring[keytype]))
+   end
+end
+
+Given("I have the '' key '' in keyring", f_havekey)
+Given("I have my '' key in keyring", f_havekey)
+
 When("I declare to '' that I am ''",   function (auth,decl)
 		x(0,"declare to "..auth.." that I am "..decl)
 		 -- request certificate
 		 if not certreq then
 			certreq = keygen(random,order) end
 		 -- declaration
-		 if not declaration then declaration = decl
-		 else declaration = declaration .." and ".. decl end
+		 if not declared then declared = decl
+		 else declared = declared .." and ".. decl end
 		 -- authority
 		 authority = auth
 end)
+When("I issue my certificate", function()
+		-- state declaration should be set
+		certkey = keygen(random, order)
+		declpub = ECP.new(declaration.public)
+		certpub = declpub + certkey.public
+		local cert = { public = hex(certpub),
+					   requester = declaration.requester,
+					   statement = declaration.statement,
+					   certifier = whoami }
+		local certhash = INT.new(sha256(OCTET.serialize(cert)),order)
+		certpriv = certhash * certkey.private + INT.new(hex(keyring.private))
+		-- set state 
+		certificate = { certpriv = hex(certpriv),
+						certpub = hex(certpub),
+						certhash = hex(certhash),
+						issuer = whoami	}
+		write_json(certificate)
+end)
+When("I issue my declaration", function()
+		declaration = {
+		   requester = whoami,
+		   authority = authority,
+		   declared = declared,
+		   public = hex(certreq.public) }
+		write_json(declaration)
+end)
 Then("my '' should be valid", function (what)
-		x(0,"my "..what.." is valid")
-		if what == "declaration" then
-		   local decl = {
-			  requester = whoami,
-			  authority = authority,
-			  declaration = declaration,
-			  public = hex(certreq.public) }
-		   assert(validate(decl,schema_certificate),"invalid "..what)
-		   write_json(decl)
+		x(0,"my "..what.." should be valid")
+		local schema = schemas[what]
+		if not schema then
+		   error("Don't know how to validate: "..what)
+		else
+		   assert(validate(_G[what],schema), "Schema validation failed on: ".. what)
 		end
 end)
 
 -- issue
-Given("I receive a valid '' from ''", function(obj, sender) debug2(obj,sender) end)
-
-When("I send the declaration", function()
-		x(0,"send declaration")
-		write_json({
-			  requester = whoami,
-			  certreq = certreq.public:octet():hex(),
-			  declaration = declaration })
-end)
-
-When("I certify the declaration", function ()
-		x(0,"certify declaration")
-        local certkey = keygen()
-        certpub = cpub + certkey.public
-        certification = { public = certpub,
-                          statement = decl,
-                          certifier = str("CA") }
-        CERThash = hash_certificate(certification)
-        certpriv = (CERThash * certkey.private + CA.private) % order
-end)
-
-Then("certificate should be valid", function ()
-		x(0,"validate certificate")
-        -- TODO: schema check
-        CERThash = hash_certificate(certificate)
-        CERTprivate = (CERThash * certreq.private + certpriv) % order
-        CERTpublic  = certpub * CERThash + CA.public
-        assert(CERTpublic == G * CERTprivate, "Certificate was expected to be valid, but is not")
+Given("I receive a '' from ''", function(obj, sender)
+		 certificate = nil
+		 declaration = nil
+		 x(0,"receive "..obj.." from "..sender)
+		 if obj == "declaration" then
+			local data = JSON.decode(DATA)
+			local decl = data.declaration
+			assert(decl.requester == sender,
+				   "declaration expected from "..sender
+					  .." but is from "..decl.requester.." instead")
+			assert(validate(decl,schemas['declaration']),"invalid declaration from "..sender)
+			declaration = decl -- set state
+		 elseif obj == "certificate" then
+			local data = JSON.decode(DATA)
+			local cert = data.certificate
+			assert(cert.issuer == sender,
+				   "certificate expected from "..sender
+					  .." but is from "..cert.issuer.." instead")
+			assert(validate(cert,schemas['certificate']),"invalid certificate from "..sender)
+			certificate = cert -- set state
+		 end
 end)
 
 
@@ -177,13 +193,15 @@ Feature: Produce a verifiable 'certificate' for a 'declaration'
     Given that I introduce myself as 'Alice'
     and I have the 'public' key 'Mad Hatter' in keyring
     When I declare to 'Mad Hatter' that I am 'lost in Wonderland'
+    and I issue my declaration
     Then my 'declaration' should be valid
 
   Scenario 'issue': Receive a declaration request and issue a certificate
     Given that I am known as 'Mad Hatter'
     and I receive a 'declaration' from 'Alice'
-    and I have the 'private' key 'Mad Hatter' in keyring
+    and I have my 'private' key in keyring
     When the 'declaration' by 'Alice' is true
+    and I issue my certificate
     Then my 'certificate' should be valid
 
   Scenario 'save': Receive a certificate of a declaration and save it
