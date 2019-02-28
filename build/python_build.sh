481b2ae836e57403cd7f401ab14ef24164f85a44
Puria Nafisi Azizi <puria@dyne.org>
2019-02-28 14:46:11 +0100

    :snake: Add more python versions and python version install

--- build/python_build.sh
+++ build/python_build.sh
@@ -7,10 +7,10 @@
 
 cd ../
 
-for VERSION in 3.5.6 3.6.8 3.7.2
+for VERSION in 3.5.0 3.5.1 3.5.2 3.5.3 3.5.4 3.5.5 3.5.6 3.6.0 3.6.1 3.6.2 3.6.3 3.6.4 3.6.5 3.6.6 3.6.7 3.6.8 3.7.0 3.7.1 3.7.2
 do
+  PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install $VERSION
   pyenv local $VERSION
   make linux-python3
-  PY=$(python -c "import sys; print('_'.join(map(str, sys.version_info[:2])))")
-  mv "build/python3/_zenroom.so" "build/python3/_zenroom_$PY.so"
+  mv "build/python3/_zenroom.so" "build/python3/_zenroom_$VERSION.so"
 done
