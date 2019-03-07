5499df1b555704528db2d045febd59d8fd304c60
Jaromil <jaromil@dyne.org>
2019-03-07 17:13:51 +0100

    removed parse_ast from zenroom header

--- src/zenroom.h
+++ src/zenroom.h
@@ -42,12 +42,6 @@ int zencode_exec_tobuf(char *script, char *conf, char *keys,
                        char *stdout_buf, size_t stdout_len,
                        char *stderr_buf, size_t stderr_len);
 
-// to obtain the Abstract Syntax Tree (AST) of a script
-// (output is in metalua formatted as JSON)
-int zenroom_parse_ast(char *script, int verbosity,
-                      char *stdout_buf, size_t stdout_len,
-                      char *stderr_buf, size_t stderr_len);
-
 void set_debug(int lev);
 
 ////////////////////////////////////////
