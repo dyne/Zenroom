1d6d2675b75b64dd4a8975f4edc331f99c3fe4e4
Jaromil <jaromil@dyne.org>
2019-06-27 10:18:51 +0200

    test script to check determinism across two runs

---
+++ test/deterministic_random_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+seed="000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+exe=${1:-zenroom}
+first=`${exe} -S $seed test/deterministic_mode.lua`
+second=`${exe} -S $seed test/deterministic_mode.lua`
+
+# echo "$first"
+
+if test "$first" = "$second"; then
+	echo
+	echo "====================="
+	echo "Deterministic mode OK"
+	echo "====================="
+	echo
+	return 0
+else
+	echo
+	echo "Error in deterministic mode"
+	echo
+	echo "$first" > /tmp/first
+	echo "$second" > /tmp/second
+	diff /tmp/first /tmp/second
+	echo
+	return 1
+fi
