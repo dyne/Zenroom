be8203810ca9a9239d563f413fd11f6f5e947747
Jaromil <jaromil@dyne.org>
2019-07-11 23:25:20 +0200

    removed jmp for cortex

--- lib/lua53/src/ldo.c
+++ lib/lua53/src/ldo.c
@@ -139,9 +139,13 @@ int luaD_rawrunprotected (lua_State *L, Pfunc f, void *ud) {
   lj.status = LUA_OK;
   lj.previous = L->errorJmp;  /* chain new error handler */
   L->errorJmp = &lj;
+#ifdef ARCH_CORTEX
+  (*f)(L, ud);
+#else
   LUAI_TRY(L, &lj,
     (*f)(L, ud);
   );
+#endif
   L->errorJmp = lj.previous;  /* restore old error handler */
   L->nCcalls = oldnCcalls;
   return lj.status;
