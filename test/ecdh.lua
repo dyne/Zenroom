910c94f8aa77fdce72f374156ca5226adbe3fc69
Jaromil <jaromil@dyne.org>
2019-07-28 11:20:34 +0200

    extend ECDH / ECP parity tests and fix usage of new aead_ API

--- test/ecdh.lua
+++ test/ecdh.lua
@@ -22,22 +22,28 @@ function test_curve (name)
    -- AES-GCM encryption
    iv = O.random(16)
    -- iv = octet.hex('00000000000000000000000000000000')
-   header = octet.string('This is the header!')
+   ciphermsg = { header = octet.string('This is the header!') }
+   session = alice:session(bob:public())
+   ciphermsg.text, ciphermsg.checksum =
+	  ECDH.aead_encrypt(session, secret, iv, ciphermsg.header)
 
-   ciphermsg = ECDH.encrypt(alice, bob, secret, header)
+   print ('AES-GCM encrypt : '  .. ciphermsg.text:url64())
+   print ('AES-GCM checksum : ' .. ciphermsg.checksum:url64())
 
-   print ('AES-GCM encrypt : '  .. ciphermsg.text:base64())
-   print ('AES-GCM checksum : ' .. ciphermsg.checksum:base64())
-
-   decipher = ECDH.decrypt(alice, bob, ciphermsg)
+   session = bob:session(alice:public())
+   decode = { header = ciphermsg.header }
+   decode.text, decode.checksum =
+	  ECDH.aead_decrypt(session, ciphermsg.text, iv, decode.header)
 
    -- print ('AES-GCM checksum : ' .. ck2:base64())
 
-   assert(secret == decipher.text)
-   assert(header == decipher.header)
+   assert(decode.checksum == ciphermsg.checksum,
+		  "Checksum differs when de/coding")
+   assert(secret == decode.text, "Secret differs from de/coded text")
+   assert(ciphermsg.header == decode.header, "Header differs from de/coded text" )
    print 'decipher message:'
-   print(decipher.header:string())
-   print(decipher.text:string())
+   print(decode.header:string())
+   print(decode.text:string())
    print (' AES-GCM on ' .. name .. ' OK')
 end
 
@@ -53,16 +59,20 @@ print('  DSA SIGN/VERIFY')
 
 G = ECP.generator()
 modulo = ECP.order()
-local skey = INT.modrand(modulo)
-function ecp_keygen(modulo)
-   local key = INT.modrand(modulo)
-   return { private = key,
-			public = key * G }
-end
+local skey = OCTET.random(32)
+I.print({ octlen = #skey})
+I.print({ intlen = #INT.new(skey)})
+local pkey = INT.new(skey):mod(ECP.order()) * G
 
 ecdh = ECDH.new('bls383')
 ecdh:private(skey)
-assert(ecdh:public() == skey *G, "ECDH and ECP public key calculation gives different results")
+I.print({ ECP = skey,
+		  ECDH = ecdh:private()})
+assert(#ecdh:private() == #skey, "ECDH and ECP private key lenghts differ")
+assert(ecdh:private() == skey, "ECDH and ECP private key import reports incongruence")
+I.print({ p__ECP = pkey:octet(),
+		  ECDH = ecdh:public() })
+assert(ecdh:public() == pkey, "ECDH and ECP public key calculation gives different results")
 local m = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 c, d = ecdh:sign(m)
 print(c)
@@ -72,4 +82,3 @@ ecdh2:public(skey*G)
 assert(ecdh2:verify(m,c,d), "ECDH verify failed")
 print "OK"
 -- vk, sk = ecdh:keygen()
-
