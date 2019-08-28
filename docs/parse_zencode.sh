16ad5412a27a56aa40916160538aea2b48e5a219
Jaromil <jaromil@dyne.org>
2019-08-28 15:31:07 +0200

    clean zencode and add a parser for JSON auto documentation

---
+++ docs/parse_zencode.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+# Parse and print a JSON map of Zencode calls in Given() When() Then()
+# that are recognised and order them by section for documentation
+#
+# requires the zenroom interpreter to be installed in PATH
+R=../src/lua
+
+function _parse() {
+    section=$1
+    action=$2
+    print "$(grep -i ''"${action}"'(' $R/zencode_${section}.lua \
+	| cut -d '"' -f2 \
+    | jq -Rj '. | { '"${action}"': .}' \
+    | jq -s . \
+    | jq -j '.| {'"${section}"': .}')"
+}
+
+tmp=`mktemp`
+print $(_parse data then
+		_parse simple when
+		_parse coconut when) | jq -s . > $tmp
+
+# TODO: see if transformation below needed, else just print $tmp
+cat <<EOF | zenroom -a $tmp
+raw_docs = JSON.decode(DATA)
+tbl = { }
+for k,v in ipairs(raw_docs) do
+	for k,v in pairs(v) do
+  	    tbl[k] = v
+	end
+end
+I.print(tbl)
+EOF
+rm $tmp
