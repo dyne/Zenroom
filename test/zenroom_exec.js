97c92c312262729a42ecbbbef7cb55aa4275f43f
Puria Nafisi Azizi <puria@dyne.org>
2019-09-03 16:12:48 +0200

    🧪 fixing js tests

--- test/zenroom_exec.js
+++ test/zenroom_exec.js
@@ -1,15 +1,28 @@
 const fs = require('fs')
+const util = require('util')
 // you have to copy the js.mem file in the CWD as per design or --memory-init-file 0
 // but it is not recommended as stated in https://github.com/kripken/emscripten/issues/2537
 // fs.writeFileSync('zenroom.js.mem', fs.readFileSync('src/zenroom.js.mem'));
 
-const zenroom_module = require(process.argv[2])
+const C = require(process.argv[2])()
 
-zenroom_module.exec_ok    = () => 0
-zenroom_module.exec_error = () => 0
-zenroom_module.print = text => console.log(text)
+C.exec_ok = () => 0
+C.exec_error = () => 0
+C.print = text => console.log(text)
+C.printErr = text => console.log(text)
 
-const zenroom = (script_file, conf_file=null, keys_file=null, data_file=process.argv[4], verbosity=1) => {
+const zenroomExec = function(script, conf = null, keys = null, data = null, verbosity = 1) {
+  C.then(function(Module){
+    Module.ccall(
+      'zenroom_exec',
+      'number',
+      ['string', 'string', 'string', 'string', 'number'],
+      [script, conf, keys, data, verbosity]
+    )   
+  })  
+}
+
+const zenroom = function(script_file, conf_file=null, keys_file=null, data_file=process.argv[4], verbosity=1) {
 	// process.stderr._write = function(chunk, encoding, callback) { callback() }
   const enc = { encoding: 'utf8' }
   const script = fs.readFileSync(script_file, enc)
@@ -17,10 +30,7 @@ const zenroom = (script_file, conf_file=null, keys_file=null, data_file=process.
   const keys = (keys_file) ? fs.readFileSync(keys_file, enc) : null
   const data = (data_file) ? fs.readFileSync(data_file, enc) : null
 
-
-  return zenroom_module.ccall('zenroom_exec', 'number',
-                              ['string', 'string', 'string', 'string', 'number'],
-                              [script, conf, keys, data, verbosity])
+  return zenroomExec(script, conf, keys, data, verbosity)
 }
 
 console.log("[JS] zenroom_exec %s %s",
