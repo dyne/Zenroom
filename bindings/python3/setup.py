b964d67c038d10a4cadf48ef67fbac6661bf7f0b
Puria Nafisi Azizi <puria@dyne.org>
2019-09-09 17:05:23 +0200

    💾 Automated `publish` reading from VERSION file

--- bindings/python3/setup.py
+++ bindings/python3/setup.py
@@ -2,11 +2,12 @@ import setuptools
 import os
 import sys
 
-VERSION = "1.1.0rc1"
+with open(os.path.join('..', '..', 'VERSION')) as version_file:
+    VERSION = version_file.read().strip()
 
 if sys.argv[-1] == "publish":
-    os.system(f"git tag -a {VERSION}")
-    os.system(f"git push origin {VERSION}")
+    #os.system(f"git tag -a {VERSION}")
+    #os.system(f"git push origin {VERSION}")
     os.system("python setup.py sdist bdist_wheel")
     os.system("twine upload dist/*")
     sys.exit()
