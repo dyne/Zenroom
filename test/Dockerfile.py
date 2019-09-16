c8adbcbac855b8ee47b218a0cf8b623a2f0215f0
Puria Nafisi Azizi <puria@dyne.org>
2019-09-16 14:33:45 +0200

    Activating virtualenv to run correctly the check-py tests over Docker

--- test/Dockerfile.py
+++ test/Dockerfile.py
@@ -8,6 +8,6 @@ RUN apk add python3 python3-dev swig
 RUN ln -s /usr/bin/python3 /usr/bin/python
 RUN make clean && make linux-python3
 RUN awk '/ZENROOM_VERSION :=/ { print $3 }' src/Makefile > bindings/VERSION
-RUN cd build && python3 -m venv venv && source venv/bin/activate \
-	&& pip3 install -e ../bindings/python3
-CMD check-py
+RUN python3 -m venv venv && source venv/bin/activate \
+	&& pip3 install -e /code/zenroom/bindings/python3
+CMD source venv/bin/activate && make check-py
