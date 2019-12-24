b7a0689d84bf2fcedcc81ba99945a70014cf929e
Jaromil <jaromil@dyne.org>
2019-12-24 14:15:44 +0100

    extended tests on elgamal including homomorphic properties

--- test/elgamal.lua
+++ test/elgamal.lua
@@ -1,30 +1,89 @@
+-- This file is part of Zenroom (https://zenroom.dyne.org)
+--
+-- Copyright (C) 2019-2020 Dyne.org foundation
+-- Written by Denis Roio
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 print''
-print '= TEST ELGAMAL COMMITMENT'
+print '= TEST ELGAMAL'
 print''
---- small test to see if ElGamal commitment works
+--- small test to see if ElGamal signature works
 
 G = ECP.generator()
 O = ECP.order()
 salt = ECP.hashtopoint("Constant random string")
-secret = INT.new(sha256("Secret message to be hashed to a number"))
+secret = INT.new(sha256("Message to be authenticated"))
 r = INT.modrand(O)
 commitment = G * r + salt * secret
 
 -- keygen
 seckey = INT.modrand(O)
-pubkey = seckey * G
+pubkey = G * seckey
 
--- encrypt
+-- sign
 k = INT.modrand(O)
 cipher = { a = G * k,
 		   b = pubkey * k + commitment * secret }
 
--- decrypt
+-- verify
 assert(cipher.b - cipher.a * seckey
 		  ==
 		  commitment * secret, "ELGAMAL failure")
 
-
+print('ELGAMAL SIGNATURE OK')
 print''
-print('ELGAMAL OK')
+
+---------------
+-- homomorphism
+-- A and B keygen 
+A = { sk = INT.modrand(O) }
+A.pk = G * A.sk
+-- pick two random ECP points
+m1 = ECP.random()
+m2 = ECP.random()
+-- calculate operations in clear
+ms = m1 + m2
+mm = m1 * INT.new(2)
+mu = m1 - m2
+-- encrypt values
+r = INT.modrand(O)
+M1 = { c1 = G * r }
+M1.c2 = A.pk * r + m1
+r = INT.modrand(O)
+M2 = { c1 = G * r }
+M2.c2 = A.pk * r + m2
+-- check decryption of values
+assert(M1.c2 - M1.c1 * A.sk == m1)
+assert(M2.c2 - M2.c1 * A.sk == m2)
+-- perform homomorphic sum
+MS = { C1 = M1.c1 + M2.c1,
+	   C2 = M1.c2 + M2.c2 }
+assert(MS.C2 - MS.C1 * A.sk == ms)
+-- perform homomorphic subtraction
+MU = { C1 = M1.c1 - M2.c1,
+	   C2 = M1.c2 - M2.c2 }
+assert(MU.C2 - MU.C1 * A.sk == mu)
+-- perform homomorphic multiplication
+MM = { C1 = M1.c1 * INT.new(2),
+	   C2 = M1.c2 * INT.new(2) }
+assert(MM.C2 - MM.C1 * A.sk == mm)
+c = INT.modrand(O)
+MM = { C1 = M1.c1 * c,
+	   C2 = M1.c2 * c }
+mm = m1 * c
+assert(MM.C2 - MM.C1 * A.sk == mm)
+
+print('ELGAMAL HOMOMORPHISM OK')
 print''
