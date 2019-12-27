78ab4960a1d40e946ad59c295c8314ea80ea4c5e
Puria Nafisi Azizi <puria@dyne.org>
2019-12-27 12:45:00 +0100

    📄 metadata for python release version bump

--- bindings/python3/setup.py
+++ bindings/python3/setup.py
@@ -2,13 +2,9 @@ import setuptools
 import os
 import sys
 
-VERSION="1.0.7rc2"
-
 if sys.argv[-1] == "publish":
-    #os.system(f"git tag -a {VERSION}")
-    #os.system(f"git push origin {VERSION}")
     os.system("python setup.py sdist bdist_wheel")
     os.system("twine upload dist/*")
     sys.exit()
 
-setuptools.setup(version=VERSION)
+setuptools.setup()
