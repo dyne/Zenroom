c7a09befb389b2d5c51a5a16b6ef4ceb43fd3cfb
Puria Nafisi Azizi <puria@dyne.org>
2020-03-02 20:26:46 +0100

    🏷Add typescript typings for TS projects

---
+++ bindings/javascript/index.d.ts
@@ -0,0 +1,14 @@
+declare module 'zenroom' {
+	export function script(script: string):any;
+	export function success():any;
+	export function error():any;
+	export function print(text: string): any;
+	export function printErr(text:string): any;
+	export function keys(keys: any): any
+	export function data(data: any): any
+	export function conf(conf: string): any;
+	export function zenroom_exec(): any;
+	export function zencode_exec(): any;
+	export function init(options: any): any;
+	export function reset(): any;
+}
