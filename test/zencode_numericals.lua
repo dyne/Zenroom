be115349d8e0c4e1ca3decbcda0c039abf938fe6
Jaromil <jaromil@dyne.org>
2020-07-13 10:31:29 +0200

    zencode test extra check for numerical comparison in base 16

--- test/zencode_numericals.lua
+++ test/zencode_numericals.lua
@@ -37,3 +37,15 @@ and number 'left' is less than 'right'
 Then print 'OK'
 ]])
 ZEN:run()
+
+
+ZEN:begin()
+ZEN:parse([[
+rule check version 1.0.0
+Given nothing
+When I set 'left' to '0a' base '16'
+and I set 'right' to '14' base '16'
+and number 'left' is less than 'right'
+Then print 'OK'
+]])
+ZEN:run()
