8eaaf6b93a472a1512b760ecebf5ea1239428528
Abbettowski <Darkangel_37@msn.com>
2020-10-12 21:07:47 +0200

    added lagrange.lua test

---
+++ test/lagrange.lua
@@ -0,0 +1,68 @@
+
+--all integers here are computed modulo the order of the curve ECP.order()
+
+Q = 5 -- quorum
+N = 9 -- participants
+assert(Q < N)
+
+shares = { }
+coeff = { }
+
+--generation of the coefficients of the secret polynomial
+for i=1,Q,1 do
+    coeff[i] = BIG.random()
+end
+
+--generation of the shares
+for i=1,N,1 do
+    local x = BIG.random()
+    while (x == 0) do       --provides trivial unleakability by setting the x coordinate non zero
+        x = BIG.random()
+        if x ~=0 then
+            for k in pairs(shares) do 
+                if x == k then x = 0 end    --checking that an old share is generated twice
+            end
+        end
+    end
+    local y = coeff[1]     --a_0
+    local x_n = BIG.new(1)
+    for n=2,Q,1 do
+        x_n = x_n:modmul(x)                  --x^(n-1)
+        y = BIG.add(y, coeff[n]:modmul(x_n))  
+        y = BIG.mod(y, ECP.order())   --"+a_(n-1)x^(n-1)"
+    end
+    table.insert(shares, {x = x, y = y})
+end
+
+
+I.print(shares)
+
+--derivation of the (secret) polynomial from a selection of (at least) Q of the partecipants
+--TODO: implement by using a list of selected T shares
+--[[ function genpoly(shares){         \\it can be a subset of it
+        ...
+            ]]--
+
+local sec = BIG.new(0)
+local num
+local den
+for i = 1,Q,1 do 
+    if Q % 2 == 1 then
+        num = BIG.new(1)
+    else 
+        num = BIG.new(BIG.modneg(1))
+    end
+    den = BIG.new(1)
+    for j = 1,Q,1 do
+    if j~=i then
+        num = num:modmul(shares[j].x)
+        den = den:modmul((shares[i].x):modsub(shares[j].x, ECP.order()))
+    end
+    end
+    sec = BIG.add(sec, (shares[i].y):modmul(num:moddiv(den, ECP.order())))   
+    sec = BIG.mod(sec, ECP.order()) 
+end
+
+print(coeff[1])
+print(sec)
+assert(coeff[1] == sec)
