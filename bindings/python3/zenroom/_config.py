a3745bb72584ddb6d383b14610b5043adcf861cb
Danilo Spinella <oss@danyspin97.org>
2020-11-20 11:53:54 +0100

    Fix library path in python bindings

--- bindings/python3/zenroom/_config.py
+++ bindings/python3/zenroom/_config.py
@@ -1,4 +1,4 @@
 import pathlib
 
 _CURRENT_SOURCE_PATH = pathlib.Path(__file__).parent.resolve()
-LIBZENROOM_LOC = pathlib.Path(__file__).parent.resolve() / './libzenroom.so'
+LIBZENROOM_LOC = pathlib.Path(__file__).parent.resolve() / 'libzenroom.so'
