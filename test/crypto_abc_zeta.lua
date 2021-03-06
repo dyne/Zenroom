734286aeebc6db357e088c2319205a2865b2aa0b
Jaromil <jaromil@dyne.org>
2021-03-06 18:47:24 +0100

    lua test: fix credential
    all check-crypto tests passed

--- test/crypto_abc_zeta.lua
+++ test/crypto_abc_zeta.lua
@@ -2,47 +2,94 @@ print()
 print '= ATTRIBUTE BASED CRYPTOGRAPHY TEST (ZKP + zeta/UID)'
 print()
 
-ABC = require_once('crypto_abc')
+local ABC = require_once('crypto_abc')
+local G2 = ECP2.generator()
 
 local client = {
-	  private = INT.new(
-		 OCTET.from_base64("21WtYpNevjgZx5V2066fRr7LFxs6+qr2ULfv1zJSc+8=") ),
-	  public = ECP.new(
-		 OCTET.from_base64( "A0kLFsmfBqat7npVjj0whyVAn4nqcBCl2IF3vx/TSQ0OVPmJ73CQd0flkITWh8neug=="))
+	private = INT.new(
+		OCTET.from_base64('21WtYpNevjgZx5V2066fRr7LFxs6+qr2ULfv1zJSc+8=')
+	),
+	public = ECP.new(
+		OCTET.from_base64(
+			'A0kLFsmfBqat7npVjj0whyVAn4nqcBCl2IF3vx/TSQ0OVPmJ73CQd0flkITWh8neug=='
+		)
+	)
 }
 
-local UID = "Unique Session Identifier"
+local UID = 'Unique Session Identifier'
 
-issuer_keypair = ABC.issuer_keygen()
-issuer2_keypair = ABC.issuer_keygen()
-issuer3_keypair = ABC.issuer_keygen()
-local issuer_aggkeys = ABC.aggregate_keys({issuer_keypair.verify,
-									   issuer2_keypair.verify,
-									   issuer3_keypair.verify})
+local issuer_key = ABC.issuer_keygen()
+local issuer2_key = ABC.issuer_keygen()
+local issuer3_key = ABC.issuer_keygen()
+local issuer_verifier = {
+	alpha = G2 * issuer_key.x,
+	beta = G2 * issuer_key.y
+}
+local issuer2_verifier = {
+	alpha = G2 * issuer2_key.x,
+	beta = G2 * issuer2_key.y
+}
+local issuer3_verifier = {
+	alpha = G2 * issuer3_key.x,
+	beta = G2 * issuer3_key.y
+}
 
-Lambda = ABC.prepare_blind_sign(client.private)
-local sigma_tilde1 = ABC.blind_sign(issuer_keypair.sign, Lambda)
-local sigma_tilde2 = ABC.blind_sign(issuer2_keypair.sign, Lambda)
-local sigma_tilde3 = ABC.blind_sign(issuer3_keypair.sign, Lambda)
-client.aggsigma = ABC.aggregate_creds(client.private, {sigma_tilde1, sigma_tilde2, sigma_tilde3})
+local issuer_aggkeys =
+	ABC.aggregate_keys(
+	{issuer_verifier, issuer2_verifier, issuer3_verifier}
+)
+
+local Lambda = ABC.prepare_blind_sign(client.private)
+local sigma_tilde1 = ABC.blind_sign(issuer_key, Lambda)
+local sigma_tilde2 = ABC.blind_sign(issuer2_key, Lambda)
+local sigma_tilde3 = ABC.blind_sign(issuer3_key, Lambda)
+client.aggsigma =
+	ABC.aggregate_creds(
+	client.private,
+	{sigma_tilde1, sigma_tilde2, sigma_tilde3}
+)
 
 local Theta, Theta2
 local zeta, zeta2
 -- simple
 Theta = ABC.prove_cred(issuer_aggkeys, client.aggsigma, client.private)
 Theta2 = ABC.prove_cred(issuer_aggkeys, client.aggsigma, client.private)
-assert( ABC.verify_cred(issuer_aggkeys, Theta) )
-assert( ABC.verify_cred(issuer_aggkeys, Theta2) )
-assert(not (ZEN.serialize(Theta) == ZEN.serialize(Theta2)), "different zk proofs do not differ")
+assert(ABC.verify_cred(issuer_aggkeys, Theta))
+assert(ABC.verify_cred(issuer_aggkeys, Theta2))
+assert(
+	not (ZEN.serialize(Theta) == ZEN.serialize(Theta2)),
+	'different zk proofs do not differ'
+)
 
 -- zkp + uid + zeta
-Theta, zeta = ABC.prove_cred_uid(issuer_aggkeys, client.aggsigma, client.private, UID)
-Theta2, zeta2 = ABC.prove_cred_uid(issuer_aggkeys, client.aggsigma, client.private, UID)
+Theta, zeta =
+	ABC.prove_cred_uid(
+	issuer_aggkeys,
+	client.aggsigma,
+	client.private,
+	UID
+)
+Theta2, zeta2 =
+	ABC.prove_cred_uid(
+	issuer_aggkeys,
+	client.aggsigma,
+	client.private,
+	UID
+)
 
-assert( ABC.verify_cred_uid(issuer_aggkeys, Theta, zeta, UID), "first UID-proof verification fails" )
-assert( ABC.verify_cred_uid(issuer_aggkeys, Theta2, zeta2, UID), "second UID-proof verification fails" )
-assert(not (ZEN.serialize(Theta) == ZEN.serialize(Theta2)), "different zk UID-proofs do not differ")
-assert(zeta == zeta2, "zeta differs with same UID")
+assert(
+	ABC.verify_cred_uid(issuer_aggkeys, Theta, zeta, UID),
+	'first UID-proof verification fails'
+)
+assert(
+	ABC.verify_cred_uid(issuer_aggkeys, Theta2, zeta2, UID),
+	'second UID-proof verification fails'
+)
+assert(
+	not (ZEN.serialize(Theta) == ZEN.serialize(Theta2)),
+	'different zk UID-proofs do not differ'
+)
+assert(zeta == zeta2, 'zeta differs with same UID')
 
-print "OK"
+print 'OK'
 print()
