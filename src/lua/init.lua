7570573c9b6682158ec6049d8247c2381736ec96
Jaromil <jaromil@dyne.org>
2021-03-06 21:02:23 +0100

    small cleanups after refactoring

--- src/lua/init.lua
+++ src/lua/init.lua
@@ -1,55 +1,68 @@
 -- This file is part of Zenroom (https://zenroom.dyne.org)
 --
--- Copyright (C) 2018-2019 Dyne.org foundation
--- designed, written and maintained by Denis Roio <jaromil@dyne.org>
+-- Copyright (C) 2018-2021 Dyne.org foundation designed, written and
+-- maintained by Denis Roio <jaromil@dyne.org>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as
 -- published by the Free Software Foundation, either version 3 of the
 -- License, or (at your option) any later version.
 --
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU Affero General Public License for more details.
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Affero General Public License for more details.
 --
--- You should have received a copy of the GNU Affero General Public License
--- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-- You should have received a copy of the GNU Affero General Public
+-- License along with this program.  If not, see
+-- <https://www.gnu.org/licenses/>.
 
 -- init script embedded at compile time.  executed in
 -- zen_load_extensions(L) usually after zen_init()
 
 -- -- remap fatal and error
 function fatal(msg)
-	  if type(msg) == "string" then warn(trim(msg),2) end
-	  debug.traceback()
---	  if ZEN_traceback ~= "" then ZEN:debug() end
-	  if DEBUG > 1 then
-		 -- TODO: ZEN:backtrace() and traceback as sorted array
-		 warn(ZEN_traceback)
-	  end
-	  if DEBUG > 2 then
-		 -- TODO: ZEN:dump()
-		 I.warn({ HEAP = { IN = IN,
-						   TMP = TMP,
-						   ACK = ACK,
-						   OUT = OUT }})
-	  end
-	  ZEN:debug()
-	  msg = msg or "fatal error"
-	  error(msg,2)
+	if type(msg) == 'string' then
+		warn(trim(msg), 2)
+	end
+	debug.traceback()
+	--	  if ZEN_traceback ~= "" then ZEN:debug() end
+	if DEBUG > 1 then
+		-- TODO: ZEN:backtrace() and traceback as sorted array
+		warn(ZEN_traceback)
+	end
+	if DEBUG > 2 then
+		-- TODO: ZEN:dump()
+		I.warn(
+			{
+				HEAP = {
+					IN = IN,
+					TMP = TMP,
+					ACK = ACK,
+					OUT = OUT
+				}
+			}
+		)
+	end
+	ZEN:debug()
+	msg = msg or 'fatal error'
+	error(msg, 2)
 end
 
 -- global
-_G["REQUIRED"] = { }
+_G['REQUIRED'] = {}
 -- avoid duplicating requires (internal includes)
 function require_once(ninc)
-   local class = REQUIRED[ninc]
-   if type(class) == "table" then return class end
-   -- new require
-class = require(ninc)
-   if type(class) == "table" then REQUIRED[ninc] = class end
-   return class
+	local class = REQUIRED[ninc]
+	if type(class) == 'table' then
+		return class
+	end
+	-- new require
+	class = require(ninc)
+	if type(class) == 'table' then
+		REQUIRED[ninc] = class
+	end
+	return class
 end
 
 -- error = zen_error -- from zen_io
@@ -57,27 +70,27 @@ end
 -- ZEN = { assert = assert } -- zencode shim when not loaded
 require('zenroom_common')
 INSPECT = require('inspect')
-CBOR   = require('zenroom_cbor')
-JSON   = require('zenroom_json')
-OCTET  = require('zenroom_octet')
-ECDH   = require('zenroom_ecdh')
-AES    = require('zenroom_aes')
-BIG    = require('zenroom_big')
-ECP    = require('zenroom_ecp')
-ECP2   = require('zenroom_ecp2')
-HASH   = require('zenroom_hash')
-BENCH  = require('zenroom_bench')
+CBOR = require('zenroom_cbor')
+JSON = require('zenroom_json')
+OCTET = require('zenroom_octet')
+ECDH = require('zenroom_ecdh')
+AES = require('zenroom_aes')
+BIG = require('zenroom_big')
+ECP = require('zenroom_ecp')
+ECP2 = require('zenroom_ecp2')
+HASH = require('zenroom_hash')
+BENCH = require('zenroom_bench')
 MACHINE = require('statemachine')
 DATE = require('date')
 
-O   = OCTET  -- alias
-INT = BIG    -- alias
-H   = HASH   -- alias
-I   = INSPECT -- alias
-PAIR = ECP2  -- alias
+O = OCTET -- alias
+INT = BIG -- alias
+I = INSPECT -- alias
+H = HASH -- alias
+PAIR = ECP2 -- alias
 PAIR.ate = ECP2.miller --alias
-V   = require('semver')
-VERSION = V(VERSION)
+V = require('semver')
+ZENROOM_VERSION = V(VERSION)
 
 ZEN = require('zencode')
 -- requires zencode_ast internally after having created
@@ -87,45 +100,52 @@ ZEN = require('zencode')
 require('zencode_data') -- pick/in, conversions etc.
 require('zencode_given')
 require('zencode_when')
-require('zencode_hash')       -- when extension
-require('zencode_array')      -- when extension
-require('zencode_random')     -- when extension
+require('zencode_hash') -- when extension
+require('zencode_array') -- when extension
+require('zencode_random') -- when extension
 require('zencode_dictionary') -- when extension
-require('zencode_verify')     -- when extension
+require('zencode_verify') -- when extension
 require('zencode_then')
 -- this is to evaluate expressions or derivate a column
 -- it would execute lua code inside the zencode and is
--- therefore dangerous, switched off by default 
+-- therefore dangerous, switched off by default
 -- require('zencode_eval')
-if DEBUG > 0 then require('zencode_debug') end
+if DEBUG > 0 then
+	require('zencode_debug')
+end
 
 -- scenario are loaded on-demand
 -- scenarios can only implement "When ..." steps
-_G["Given"] = nil
-_G["Then"]  = nil
+_G['Given'] = nil
+_G['Then'] = nil
 
 -----------
 -- defaults
-_G["CONF"] = {
-   -- goldilocks is our favorite ECDH/DSA curve
-   -- other choices here include secp256k1 or ed25519 or bls383
-   -- beware this choice affects only the ECDH object
-   -- and ECDH public keys cannot function as ECP
-   -- because of IANA 7303
-   verbosity = 1,
-   input = { encoding = input_encoding('base64'),
-			 format = get_format('json'),
-			 tagged = false },
-   output = { encoding = output_encoding('base64'),
-			  format = get_format('json'),
-			  versioning = false },
-   parser = { strict_match = true },
-   hash = 'sha256',
-   heapguard = true
+_G['CONF'] = {
+	-- goldilocks is our favorite ECDH/DSA curve
+	-- other choices here include secp256k1 or ed25519 or bls383
+	-- beware this choice affects only the ECDH object
+	-- and ECDH public keys cannot function as ECP
+	-- because of IANA 7303
+	verbosity = 1,
+	input = {
+		encoding = input_encoding('base64'),
+		format = get_format('json'),
+		tagged = false
+	},
+	output = {
+		encoding = output_encoding('base64'),
+		format = get_format('json'),
+		versioning = false
+	},
+	parser = {strict_match = true},
+	hash = 'sha256',
+	heapguard = true
 }
 
 -- do not modify
-LICENSE = [[
+_G['LICENSE'] =
+	[[
 Licensed under the terms of the GNU Public License as published by
 the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.  Unless required by applicable
@@ -133,7 +153,8 @@ law or agreed to in writing, software distributed under the License
 is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied.
 ]]
-COPYRIGHT = [[
+_G['COPYRIGHT'] =
+	[[
 Forked by Jaromil on 18 January 2020 from Coconut Petition
 ]]
 SALT = ECP.hashtopoint(OCTET.from_string(COPYRIGHT .. LICENSE))
@@ -142,13 +163,16 @@ SALT = ECP.hashtopoint(OCTET.from_string(COPYRIGHT .. LICENSE))
 -- this is a sort of salted hash for advanced ZKP operations and
 -- should not be changed. It may be made configurable in future.
 function ZKP_challenge(list)
-	local challenge = ECP.generator():octet() .. ECP2.generator():octet() .. SALT:octet()
-    local ser = serialize(list)
-	return INT.new( sha256( challenge .. ser.octets .. OCTET.from_string(ser.strings)))
+	local challenge =
+		ECP.generator():octet() .. ECP2.generator():octet() .. SALT:octet()
+	local ser = serialize(list)
+	return INT.new(
+		sha256(challenge .. ser.octets .. OCTET.from_string(ser.strings))
+	)
 end
 
 -- encoding base64url (RFC4648) is the fastest and most portable in zenroom
 -- set_encoding('url64')
 -- set_format('json')
 
-collectgarbage'collect'
+collectgarbage 'collect'
--- src/lua/zenroom_ast.lua
+++ src/lua/zenroom_ast.lua
@@ -1,166 +1,173 @@
 -- This file is part of Zenroom (https://zenroom.dyne.org)
 --
--- Copyright (C) 2018-2020 Dyne.org foundation
--- designed, written and maintained by Denis Roio <jaromil@dyne.org>
+-- Copyright (C) 2018-2021 Dyne.org foundation designed, written and
+-- maintained by Denis Roio <jaromil@dyne.org>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as
 -- published by the Free Software Foundation, either version 3 of the
 -- License, or (at your option) any later version.
 --
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU Affero General Public License for more details.
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Affero General Public License for more details.
 --
--- You should have received a copy of the GNU Affero General Public License
--- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-- You should have received a copy of the GNU Affero General Public
+-- License along with this program.  If not, see
+-- <https://www.gnu.org/licenses/>.
 
 function zencode_iscomment(b)
-   local x = string.char(b:byte(1))
-   if x == '#' then
-	  return true
-   else return false
-end end
+	local x = string.char(b:byte(1))
+	if x == '#' then
+		return true
+	else
+		return false
+	end
+end
 function zencode_isempty(b)
-   if b == nil or b == '' then
-	   return true
-   else return false
-end end
+	if b == nil or b == '' then
+		return true
+	else
+		return false
+	end
+end
 -- returns an iterator for newline termination
 function zencode_newline_iter(text)
-   s = trim(text) -- implemented in zen_io.c
-   if s:sub(-1)~="\n" then s=s.."\n" end
-   return s:gmatch("(.-)\n") -- iterators return functions
+	s = trim(text) -- implemented in zen_io.c
+	if s:sub(-1) ~= '\n' then
+		s = s .. '\n'
+	end
+	return s:gmatch('(.-)\n') -- iterators return functions
 end
 
 function set_sentence(self, event, from, to, ctx)
-   local reg = ctx.Z[self.current.."_steps"]
-   ctx.Z.OK = false
-   ZEN.assert(reg, "Steps register not found: "..self.current.."_steps")
-   for pattern,func in pairs(reg) do
-	  if (type(func) ~= "function") then
-		 error("Zencode function missing: "..pattern, 2)
-		 return false
-	  end
-	  -- TODO: optimize in C
-	  -- remove '' contents, lower everything, expunge prefixes
-	  local tt = string.gsub(trim(ctx.msg),"'(.-)'","''")
-	  tt = tt:gsub(' +',' ') -- eliminate multiple internal spaces
-	  tt = string.gsub(tt,"I ","", 1)
-	  tt = string.gsub(tt:lower() ,"when " ,"", 1)
-	  tt = string.gsub(tt,"then " ,"", 1)
-	  tt = string.gsub(tt,"given ","", 1)
-	  tt = string.gsub(tt,"and "  ,"", 1) -- TODO: expunge only first 'and'
-	  tt = string.gsub(tt,"that " ,"", 1)
-	  tt = string.gsub(tt,"valid " ,"", 1) -- backward compat
-	  tt = string.gsub(tt,"known as ", "", 1)
-	  tt = string.gsub(tt,"all ", "", 1)
-	  tt = string.gsub(tt," inside "," in ", 1) -- equivalence
-	  tt = string.gsub(tt," an "," a ", 1)
-	  if strcasecmp(tt, pattern) then
-		 local args = {} -- handle multiple arguments in same string
-		 for arg in string.gmatch(ctx.msg,"'(.-)'") do
-			-- convert all spaces to underscore in argument strings
-			arg = string.gsub(arg, ' ', '_')
-			table.insert(args,arg)
-		 end
-		 ctx.Z.id = ctx.Z.id + 1
-		 -- AST data prototype
-		 table.insert(ctx.Z.AST,
-					  { id = ctx.Z.id, -- ordered number
-						args = args,  -- array of vars
-						source = ctx.msg, -- source text
-						section = self.current,
-						hook = func       }) -- function
-		 ctx.Z.OK = true
-		 break
-	  end
-   end
-   if not ctx.Z.OK and CONF.parser.strict_match then
-	  debug_traceback()
-   	  exitcode(1)
-   	  error("Zencode pattern not found: "..trim(ctx.msg), 1)
-   	  return false
-   elseif not ctx.Z.OK and not CONF.parser.strict_match then
-	  warn("Zencode pattern ignored: "..trim(ctx.msg), 1)
-   end
+	local reg = ctx.Z[self.current .. '_steps']
+	ctx.Z.OK = false
+	ZEN.assert(
+		reg,
+		'Steps register not found: ' .. self.current .. '_steps'
+	)
+	for pattern, func in pairs(reg) do
+		if (type(func) ~= 'function') then
+			error('Zencode function missing: ' .. pattern, 2)
+			return false
+		end
+		-- TODO: optimize in C
+		-- remove '' contents, lower everything, expunge prefixes
+		local tt = string.gsub(trim(ctx.msg), "'(.-)'", "''")
+		tt = tt:gsub(' +', ' ') -- eliminate multiple internal spaces
+		tt = string.gsub(tt, 'I ', '', 1)
+		tt = string.gsub(tt:lower(), 'when ', '', 1)
+		tt = string.gsub(tt, 'then ', '', 1)
+		tt = string.gsub(tt, 'given ', '', 1)
+		tt = string.gsub(tt, 'and ', '', 1) -- TODO: expunge only first 'and'
+		tt = string.gsub(tt, 'that ', '', 1)
+		tt = string.gsub(tt, 'valid ', '', 1) -- backward compat
+		tt = string.gsub(tt, 'known as ', '', 1)
+		tt = string.gsub(tt, 'all ', '', 1)
+		tt = string.gsub(tt, ' inside ', ' in ', 1) -- equivalence
+		tt = string.gsub(tt, ' an ', ' a ', 1)
+		if strcasecmp(tt, pattern) then
+			local args = {} -- handle multiple arguments in same string
+			for arg in string.gmatch(ctx.msg, "'(.-)'") do
+				-- convert all spaces to underscore in argument strings
+				arg = string.gsub(arg, ' ', '_')
+				table.insert(args, arg)
+			end
+			ctx.Z.id = ctx.Z.id + 1
+			-- AST data prototype
+			table.insert(
+				ctx.Z.AST,
+				{
+					id = ctx.Z.id, -- ordered number
+					args = args, -- array of vars
+					source = ctx.msg, -- source text
+					section = self.current,
+					hook = func
+				}
+			) -- function
+			ctx.Z.OK = true
+			break
+		end
+	end
+	if not ctx.Z.OK and CONF.parser.strict_match then
+		debug_traceback()
+		exitcode(1)
+		error('Zencode pattern not found: ' .. trim(ctx.msg), 1)
+		return false
+	elseif not ctx.Z.OK and not CONF.parser.strict_match then
+		warn('Zencode pattern ignored: ' .. trim(ctx.msg), 1)
+	end
 end
 
-
--- TODO: investigate use of lua-faces
 function set_rule(text)
-   local res = false
-   local tr = text.msg:gsub(' +',' ') -- eliminate multiple internal spaces
-   local rule = strtok( trim(tr):lower() )
-   if rule[2] == 'check' and rule[3] == 'version' and rule[4] then
-	  SEMVER = require_once('semver')
-	  local ver = SEMVER(rule[4])
-	  if ver == VERSION then
-		 act("Zencode version match: "..VERSION.original)
-		 res = true
-	  elseif ver < VERSION then
-		 warn("Zencode written for an older version: "..ver.original)
-		 res = true
-	  elseif ver > VERSION then
-		 warn("Zencode written for a newer version: "..ver.original)
-		 res = true
-	  else
-		 error("Version check error: "..rule[4])
-	  end
-	  text.Z.checks.version = res
-      -- TODO: check version of running VM
-	  -- elseif rule[2] == 'load' and rule[3] then
-	  --     act("zencode extension: "..rule[3])
-	  --     require("zencode_"..rule[3])
-   elseif rule[2] == 'input' and rule[3] then
-
-      -- rule input encoding|format ''
-      if rule[3] == 'encoding' and rule[4] then
-         CONF.input.encoding = input_encoding(rule[4])
-		 res = true and CONF.input.encoding
-      elseif rule[3] == 'format' and rule[4] then
-		 CONF.input.format = get_format(rule[4])
-         res = true and CONF.input.format
-	  elseif rule[3] == 'untagged' then
-		 res = true
-		 CONF.input.tagged = false
-      end
-
-   elseif rule[2] == 'output' and rule[3] then
-      -- rule input encoding|format ''
-      if rule[3] == 'encoding' then
-         CONF.output.encoding = output_encoding(rule[4])
-		 res = true and CONF.output.encoding
-      elseif rule[3] == 'format' then
-		 CONF.output.format = get_format(rule[4])
-         res = true and CONF.output.format
-      elseif rule[3] == 'versioning' then
-		 CONF.output.versioning = true
-         res = true
-      elseif strcasecmp(rule[3],'ast') then
-		 CONF.output.AST = true
-		 res = true
-	  end
-
-	  -- TODO: rule debug [ format | encoding ]
-
-   elseif rule[2] == 'unknown' and rule[3] then
-	  if rule[3] == 'ignore' then
-		 CONF.parser.strict_match = false
-		 res = true
-	  end
-
-   elseif rule[2] == 'set' and rule[4] then
-
-      CONF[rule[3]] = tonumber(rule[4]) or rule[4]
-      res = true and CONF[rule[3]]
-
-   end
-   if not res then error("Rule invalid: "..text.msg, 3)
-   else act(text.msg) end
-   return res
+	local res = false
+	local tr = text.msg:gsub(' +', ' ') -- eliminate multiple internal spaces
+	local rule = strtok(trim(tr):lower())
+	if rule[2] == 'check' and rule[3] == 'version' and rule[4] then
+		-- TODO: check version of running VM
+		-- elseif rule[2] == 'load' and rule[3] then
+		--     act("zencode extension: "..rule[3])
+		--     require("zencode_"..rule[3])
+		SEMVER = require_once('semver')
+		local ver = SEMVER(rule[4])
+		if ver == ZENROOM_VERSION then
+			act('Zencode version match: ' .. ZENROOM_VERSION.original)
+			res = true
+		elseif ver < ZENROOM_VERSION then
+			warn('Zencode written for an older version: ' .. ver.original)
+			res = true
+		elseif ver > ZENROOM_VERSION then
+			warn('Zencode written for a newer version: ' .. ver.original)
+			res = true
+		else
+			error('Version check error: ' .. rule[4])
+		end
+		text.Z.checks.version = res
+	elseif rule[2] == 'input' and rule[3] then
+		-- rule input encoding|format ''
+		if rule[3] == 'encoding' and rule[4] then
+			CONF.input.encoding = input_encoding(rule[4])
+			res = true and CONF.input.encoding
+		elseif rule[3] == 'format' and rule[4] then
+			CONF.input.format = get_format(rule[4])
+			res = true and CONF.input.format
+		elseif rule[3] == 'untagged' then
+			res = true
+			CONF.input.tagged = false
+		end
+	elseif rule[2] == 'output' and rule[3] then
+		-- TODO: rule debug [ format | encoding ]
+		-- rule input encoding|format ''
+		if rule[3] == 'encoding' then
+			CONF.output.encoding = output_encoding(rule[4])
+			res = true and CONF.output.encoding
+		elseif rule[3] == 'format' then
+			CONF.output.format = get_format(rule[4])
+			res = true and CONF.output.format
+		elseif rule[3] == 'versioning' then
+			CONF.output.versioning = true
+			res = true
+		elseif strcasecmp(rule[3], 'ast') then
+			CONF.output.AST = true
+			res = true
+		end
+	elseif rule[2] == 'unknown' and rule[3] then
+		if rule[3] == 'ignore' then
+			CONF.parser.strict_match = false
+			res = true
+		end
+	elseif rule[2] == 'set' and rule[4] then
+		CONF[rule[3]] = tonumber(rule[4]) or rule[4]
+		res = true and CONF[rule[3]]
+	end
+	if not res then
+		error('Rule invalid: ' .. text.msg, 3)
+	else
+		act(text.msg)
+	end
+	return res
 end
-
 
 return zencode_parse
