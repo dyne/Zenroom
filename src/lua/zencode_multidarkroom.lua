dd9658b7d91d2322e8e5fdfd1aa6e672f059a4b8
Jaromil <jaromil@dyne.org>
2021-03-09 16:37:24 +0100

    zencode multidarkroom new scenario
    upcoming paper, zkp powered multi-signature scheme in multi-party
    computation for untreacable signatures.

---
+++ src/lua/zencode_multidarkroom.lua
@@ -0,0 +1,158 @@
+-- This file is part of Zenroom (https://zenroom.dyne.org)
+--
+-- Copyright (C) 2020-2021 Dyne.org foundation
+-- designed and written by Denis Roio
+-- with help by Alberto Ibrisevich and Andrea D'Intino
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public
+-- License along with this program.  If not, see
+-- <https://www.gnu.org/licenses/>.
+
+ABC = require_once('crypto_credential')
+
+G2 = ECP2.generator()
+
+local function import_multidarkroom_session_fingerprints_f(o)
+    if not o then return {} end
+    local rawarr = deepmap(CONF.input.encoding.fun, o)
+    local arr = {}
+    for _, v in ipairs(rawarr) do table.insert(arr, ECP.new(v)) end
+    return arr
+end
+
+ZEN.add_schema({
+    bls_public_key = function(obj)
+        return ECP2.new( CONF.input.encoding.fun(obj) )
+    end,
+    multidarkroom_session = function(obj)
+        return {
+            UID = ZEN.get(obj, 'UID', ECP.new),
+            SM = ZEN.get(obj, 'SM', ECP.new),
+            verifier = ZEN.get(obj, 'verifier', ECP2.new),
+            fingerprints = import_multidarkroom_session_fingerprints_f(obj.fingerprints)
+        }
+    end,
+    multidarkroom_signature = function(obj)
+        return {
+            UID = ZEN.get(obj, 'UID', ECP.new),
+            signature = ZEN.get(obj, 'signature', ECP.new),
+            proof = import_credential_proof_f(obj.proof),
+            zeta = ZEN.get(obj, 'zeta', ECP.new)
+        }
+    end
+})
+
+
+When("create the bls key", function()
+    -- keygen: δ = r.O ; γ = δ.G2
+    initkeys'bls'
+    ACK.keys.bls = INT.random() -- BLS secret signing key
+end)
+
+When("create the bls public key", function()
+    empty 'bls public key'
+    havekey'bls'
+    ACK.bls_public_key = G2 * ACK.keys.bls
+end)
+
+When("create the multidarkroom session with UID ''", function(uid)
+    empty 'multidarkroom session'
+    have(uid)
+    -- TODO: more checks
+    -- init random and uid
+    local r = INT.random()
+    local UID = ECP.hashtopoint(uid)
+    -- session public key
+    local PM = G2 * r
+    for k, v in pairs(ACK.bls_public_key) do PM = PM + v end
+    ACK.multidarkroom_session = {UID = UID, SM = UID * r, verifier = PM}
+end)
+
+When("create the multidarkroom signature", function()
+    empty 'multidarkroom signature'
+    have 'multidarkroom session'
+    have 'issuer public key'
+    -- aggregate all credentials
+    local pubcred = false
+    for k, v in pairs(ACK.issuer_public_key) do
+        if not pubcred then
+            pubcred = v
+        else
+            pubcred = {pubcred.alpha + v.alpha, pubcred.beta + v.beta}
+        end
+    end
+    local p, z = ABC.prove_cred_uid(pubcred, ACK.credentials,
+                                    ACK.keys.credential,
+                                    ACK.multidarkroom_session.UID)
+    ACK.multidarkroom_signature = {
+        UID = ACK.multidarkroom_session.UID,
+        signature = ACK.multidarkroom_session.UID * ACK.keys.bls,
+        proof = p,
+        zeta = z
+    }
+end)
+
+When("prepare credentials for verification", function()
+    have 'credential'
+    local res = false
+    for k, v in pairs(ACK.issuer_public_key) do
+        if not res then
+            res = {alpha = v.alpha, beta = v.beta}
+        else
+            res.alpha = res.alpha + v.alpha
+            res.beta = res.beta + v.beta
+        end
+    end
+    ACK.verifiers = res
+end)
+
+When("verify the multidarkroom signature credential", function()
+    have 'multidarkroom_signature'
+    have 'verifiers'
+    have 'multidarkroom_session'
+    ZEN.assert(ABC.verify_cred_uid(ACK.verifiers, ACK.multidarkroom_signature.proof,
+                                   ACK.multidarkroom_signature.zeta, ACK.multidarkroom_session.UID),
+               "Signature has an invalid credential to sign")
+end)
+
+When("check the multidarkroom signature fingerprint is new", function()
+    have 'multidarkroom_signature'
+    have 'multidarkroom_session'
+    if not ACK.multidarkroom_session.fingerprints then return end
+    ZEN.assert(not ACK.multidarkroom_session.fingerprints[ACK.multidarkroom_signature.zeta],
+               "Signature fingerprint is not new")
+end)
+
+When("add the multidarkroom fingerprint to the multidarkroom session", function()
+    have 'multidarkroom_signature'
+    have 'multidarkroom_session'
+    if not ACK.multidarkroom_session.fingerprints then
+        ACK.multidarkroom_session.fingerprints = { ACK.multidarkroom_signature.zeta }
+    else
+        table.insert(ACK.multidarkroom_session.fingerprints, ACK.multidarkroom_signature.zeta)
+    end
+end)
+
+When("add the multidarkroom signature to the multidarkroom session", function()
+    have 'multidarkroom_session'
+    have 'multidarkroom_signature'
+    ACK.multidarkroom_session.SM = ACK.multidarkroom_session.SM + ACK.multidarkroom_signature.signature
+end)
+
+When("verify the multidarkroom session is valid", function()
+    have 'multidarkroom_session'
+    ZEN.assert(
+        ECP2.miller(ACK.multidarkroom_session.verifier, ACK.multidarkroom_session.UID) ==
+            ECP2.miller(G2, ACK.multidarkroom_session.SM),
+        "multidarkroom session doesn't validates")
+end)
---
+++ test/zencode_multidarkroom/run.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+####################
+# common script init
+if ! test -r ../utils.sh; then
+	echo "run executable from its own directory: $0"; exit 1; fi
+. ../utils.sh
+Z="`detect_zenroom_path` `detect_zenroom_conf`"
+####################
+
+sideload='../../src/lua/zencode_multidarkroom.lua'
+
+## ISSUER
+cat <<EOF | zexe issuer_keygen.zen | tee issuer_keypair.json
+Scenario credential
+Given I am 'The Authority'
+when I create the issuer key
+Then print my 'keys'
+EOF
+
+cat <<EOF | zexe issuer_verifier.zen -a issuer_keypair.json | tee issuer_verifier.json
+Scenario credential: publish verifier
+Given that I am known as 'The Authority'
+and I have my 'keys'
+When I create the issuer public key
+Then print my 'issuer public key'
+EOF
+
+generate_participant() {
+    local name=$1
+    ## PARTICIPANT
+	cat <<EOF | zexe keygen_${1}.zen | tee keypair_${1}.json
+Scenario multidarkroom
+Scenario credential
+Given I am '${1}'
+When I create the BLS key
+and I create the credential key
+Then print my 'keys'
+EOF
+
+	cat <<EOF | zexe pubkey_${1}.zen -k keypair_${1}.json | tee verifier_${1}.json
+Scenario multidarkroom
+Given I am '${1}'
+and I have my 'keys'
+When I create the BLS public key
+Then print my 'bls public key'
+EOF
+
+	cat <<EOF | zexe request_${1}.zen -k keypair_${1}.json | tee request_${1}.json
+Scenario credential
+Given I am '${1}'
+and I have my 'keys'
+When I create the credential request
+Then print my 'credential request'
+EOF
+	##
+
+	## ISSUER SIGNS
+	cat <<EOF | zexe issuer_sign_${1}.zen -k issuer_keypair.json -a request_${1}.json | tee issuer_signature_${1}.json
+Scenario credential
+Given I am 'The Authority'
+and I have my 'keys'
+and I have a 'credential request' inside '${1}'
+when I create the credential signature
+and I create the issuer public key
+Then print the 'credential signature'
+and print the 'issuer public key'
+EOF
+	##
+
+	## PARTICIPANT AGGREGATES SIGNED CREDENTIAL
+	cat <<EOF | zexe aggr_cred_${1}.zen -k keypair_${1}.json -a issuer_signature_${1}.json | tee verified_credential_${1}.json
+Scenario credential
+Given I am '${1}'
+and I have my 'keys'
+and I have a 'credential signature'
+when I create the credentials
+then print my 'credentials'
+and print my 'keys'
+EOF
+	##
+
+}
+
+# generate two signed credentials
+generate_participant "Alice"
+generate_participant "Bob"
+
+# join the verifiers of signed credentials
+json_join verifier_Alice.json verifier_Bob.json > public_keys.json
+
+echo "{\"today\": \"`date +'%s'`\"}" > uid.json
+
+# anyone can start a session
+
+# SIGNING SESSION
+cat <<EOF | zexe session_start.zen -k uid.json -a public_keys.json > multidarkroom_session.json
+Scenario multidarkroom
+Given I have a 'bls public key' from 'Alice'
+and I have a 'bls public key' from 'Bob'
+and I have a 'string' named 'today'
+When I create the multidarkroom session with uid 'today'
+Then print the 'multidarkroom session'
+EOF
+#
+
+# anyone can require a verified credential to be able to sign, chosing
+# the right issuer verifier for it
+json_join issuer_verifier.json multidarkroom_session.json > credential_to_sign.json
+
+
+# PARTICIPANT SIGNS (function)
+function participant_sign() {
+	local name=$1
+	cat <<EOF | zexe sign_session.zen -a credential_to_sign.json -k verified_credential_$name.json | tee signature_$name.json
+Scenario multidarkroom
+Scenario credential
+Given I am '$name'
+and I have my 'credentials'
+and I have my 'keys'
+and I have a 'multidarkroom session'
+and I have a 'issuer public key' from 'The Authority'
+When I create the multidarkroom signature
+Then print the 'multidarkroom signature'
+EOF
+}
+
+participant_sign 'Alice'
+participant_sign 'Bob'
+
+function collect_sign() {
+	local name=$1
+	local tmp_msig=`mktemp`
+	local tmp_sig=`mktemp`
+	cp -v multidarkroom_session.json $tmp_msig
+	json_join issuer_verifier.json signature_$name.json > $tmp_sig
+	cat << EOF | zexe collect_sign.zen -a $tmp_msig -k $tmp_sig | tee multidarkroom_session.json
+Scenario multidarkroom
+Scenario credential
+Given I have a 'multidarkroom session'
+and I have a 'issuer public key' in 'The Authority'
+and I have a 'multidarkroom signature'
+When I aggregate all the issuer public keys
+and I verify the multidarkroom signature credential
+and I check the multidarkroom signature fingerprint is new
+and I add the multidarkroom fingerprint to the multidarkroom session
+and I add the multidarkroom signature to the multidarkroom session
+Then print the 'multidarkroom session'
+EOF
+	rm -f $tmp_msig $tmp_sig
+}
+
+# COLLECT UNIQUE SIGNATURES
+collect_sign 'Alice'
+collect_sign 'Bob'
+
+# VERIFY SIGNATURE
+cat << EOF | zexe verify_sign.zen -a multidarkroom_session.json | jq .
+Scenario multidarkroom
+Given I have a 'multidarkroom session'
+When I verify the multidarkroom session is valid
+Then print 'SUCCESS'
+and print the 'multidarkroom session'
+EOF
+
