54db55d6c5db5b064ea63b5424b8f0c3b2859469
Jaromil <jaromil@dyne.org>
2021-04-09 06:39:42 +0200

    improved verification test

--- test/zencode_numbers/run.sh
+++ test/zencode_numbers/run.sh
@@ -15,13 +15,55 @@ fi
 Z="`detect_zenroom_path` `detect_zenroom_conf`"
 ####################
 
-cat <<EOF | zexe hash_string.zen | tee hex.json
-rule output encoding hex
+cat <<EOF | zexe hash_left.zen | tee left.json
 Given nothing
-When I write string 'a string to be hashed' in 'source'
+When I write string 'a left string to be hashed' in 'source'
 and I create the hash of 'source'
-Then print the 'hash'
+and I rename 'hash' to 'left'
+Then print 'left'
 EOF
+
+cat <<EOF | zexe hash_right.zen | tee right.json
+Given nothing
+When I write string 'a right string to be hashed' in 'source'
+and I create the hash of 'source'
+and I rename 'hash' to 'right'
+Then print 'right'
+EOF
+
+cat <<EOF | zexe hash_eq.zen -a left.json -k right.json
+Given I have a 'base64' named 'left'
+When I write string 'a left string to be hashed' in 'source'
+and I create the hash of 'source'
+When I verify 'left' is equal to 'hash'
+Then print the string 'OK'
+EOF
+
+# cat <<EOF | zexe hash_neq.zen -a left.json -k right.json
+# Given I have a 'base64' named 'left'
+# and I have a 'base64' named 'right'
+# When I verify 'left' is equal to 'right'
+# Then print the string 'OK'
+# EOF
+
+cat <<EOF | zexe num_eq_base10.zen
+rule check version 1.0.0
+Given nothing
+When I write number '42' in 'left'
+and I write number '42' in 'right'
+and I verify 'left' is equal to 'right'
+Then print the string 'OK'
+EOF
+
+# cat <<EOF | zexe num_neq_base10.zen
+# rule check version 1.0.0
+# Given nothing
+# When I write number '142' in 'left'
+# and I write number '42' in 'right'
+# and I verify 'left' is equal to 'right'
+# Then print the string 'OK'
+# EOF
+
 
 cat <<EOF | zexe cmp_base10.zen
 rule check version 1.0.0
@@ -31,6 +73,16 @@ and I write number '20' in 'right'
 and number 'left' is less or equal than 'right'
 Then print the string 'OK'
 EOF
+
+# cat <<EOF | zexe cmp_nlt_base10.zen
+# rule check version 1.0.0
+# Given nothing
+# When I write number '10' in 'left'
+# and I write number '20' in 'right'
+# and number 'right' is less than 'left'
+# Then print the string 'OK'
+# EOF
+
 
 
 cat <<EOF | zexe cmp_base16.zen
