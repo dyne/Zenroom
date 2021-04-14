ef711fc092600b602b4c7a57729fbbe48331afc1
Danilo Spinella <oss@danyspin97.org>
2021-04-14 16:27:44 +0200

    Update go bindings

--- bindings/golang/zenroom/zenroom.go
+++ bindings/golang/zenroom/zenroom.go
@@ -3,86 +3,29 @@
 // wrap is only available for Linux.
 package zenroom
 
-// #cgo CFLAGS: -IC:${SRCDIR}
-// #cgo LDFLAGS: -L${SRCDIR}/lib -Wl,-rpath=${SRCDIR}/lib -lzenroomgo
+// #cgo CFLAGS: -I${SRCDIR}/src
+// #cgo LDFLAGS: -L${SRCDIR}/lib -Wl,-rpath=${SRCDIR}/lib -lzenroom
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include <string.h>
 // #include "zenroom.h"
-import (
-	"C"
-)
+// typedef int(*fun)(char*, char*, char*, char*, char*, size_t, char*, size_t);
+//
+// int wrapper(fun exec, char* script, char* conf, char* data, char* keys, char* stdout, size_t stdout_size, char* stderr, size_t stderr_size) {
+//     return exec(script, conf, data, keys, stdout, stdout_size, stderr, stderr_size);
+// }
+import "C"
 
 import (
-	"fmt"
 	"unsafe"
-
-	// dummy import to include binary in dependencies
-	_ "github.com/dyne/Zenroom/bindings/golang/zenroom/lib"
 )
 
 // maxString is zenroom defined buffer MAX_STRING size
-const maxString = 1048576
+const BUFSIZE = 2 * 1024 * 1024
 
-// config is an unexported config struct used to handle the functional variadic
-// configuration options defined below.
-type config struct {
-	Keys      []byte
-	Data      []byte
-	Conf      string
-	Verbosity int
-}
-
-// Option is a type alias we use to supply optional configuration to the primary
-// Exec method this library exposes. Option is a functional configuration type
-// alias for a function that takes as a parameter an unexported configuration
-// object. Callers can supply keys, data, conf and verbosity via an Option using
-// one of the WithXXX helper functions.
-type Option func(*config)
-
-// WithKeys is a configuration helper that allows the caller to pass in a value
-// for the KEYS parameter that Zenroom supports. The value of KEYS is typically
-// a string, often JSON formatted, that contains one or more keys that the
-// primary script requires in order to operate. These are passed separately from
-// the main script as they will typically have different security requirements
-// than the main script contents. Keys must be passed in to the helper in a byte
-// slice.
-func WithKeys(keys []byte) Option {
-	return func(c *config) {
-		c.Keys = keys
-	}
-}
-
-// WithData is a configuration helper that allows the caller to pass in a value
-// for the DATA parameter that Zenroom supports. The value of KEYS is a string,
-// often but not required to be JSON formatted, containing data over which the
-// script should operate. As with the KEYS property, DATA is passed separately
-// from either the SCRIPT or the KEYS as these different values will often have
-// different security requirements or may come from different sources. DATA must
-// be passed as a byte slice.
-func WithData(data []byte) Option {
-	return func(c *config) {
-		c.Data = data
-	}
-}
-
-// WithConf is a configuration helper that allows the caller to pass in a value
-// for the CONF parameter that Zenroom supports. The default for this value if
-// not supplied is an empty string.
-func WithConf(conf string) Option {
-	return func(c *config) {
-		c.Conf = conf
-	}
-}
-
-// WithVerbosity is a configuration helper that allows the caller to specify how
-// verbose Zenroom should be with its output. The value of this configuration
-// must be an integer from 1 to 3, where 1 is the least verbose, and 3 is the
-// most. The default if this value is not supplied is 1.
-func WithVerbosity(verbosity int) Option {
-	return func(c *config) {
-		c.Verbosity = verbosity
-	}
+type ZenResult struct {
+    output string;
+    logs string;
 }
 
 // ZenroomExec is our primary public API method, and it is here that we call Zenroom's
@@ -97,60 +40,66 @@ func WithVerbosity(verbosity int) Option {
 //
 // Returns the output of the execution of the Zenroom virtual machine, or an
 // error.
-func ZenroomExec(script []byte, options ...Option) ([]byte, error) {
+func ZenroomExec(script string, conf string, keys string, data string) (ZenResult, bool) {
+	exec := C.fun(C.zenroom_exec_tobuf)
+	return wrapper(exec, script, conf, keys, data)
+}
+
+func ZencodeExec(script string, conf string, keys string, data string) (ZenResult, bool) {
+	exec := C.fun(C.zencode_exec_tobuf)
+	return wrapper(exec, script, conf, keys, data)
+}
+
+func wrapper(fun C.fun, script string, conf string, keys string, data string) (ZenResult, bool) {
 	var (
-		cScript                   *C.char
-		optKeys, optData, optConf *C.char
+		cScript, cConf, cKeys, cData *C.char
 	)
 
-	// capture the required script parameter
-	if script == nil {
-		return nil, fmt.Errorf("missing required script to process")
-	}
-	cScript = (*C.char)(unsafe.Pointer(&script[0]))
+	cScript = C.CString(script)
+	defer C.free(unsafe.Pointer(cScript))
 
-	// set up our default config
-	conf := &config{
-		Conf:      "",
-		Verbosity: 1,
+	if conf != "" {
+		cConf = C.CString(conf)
+		defer C.free(unsafe.Pointer(cConf))
+	} else {
+		cConf = nil
 	}
 
-	// and now we iterate through our options, to update our config object
-	for _, option := range options {
-		option(conf)
+
+	if data != "" {
+		cData = C.CString(data)
+		defer C.free(unsafe.Pointer(cData))
+	} else {
+		cData = nil
 	}
 
-	if conf.Keys != nil {
-		optKeys = (*C.char)(unsafe.Pointer(&conf.Keys[0]))
-	}
-
-	if conf.Data != nil {
-		optData = (*C.char)(unsafe.Pointer(&conf.Data[0]))
-	}
-
-	if conf.Conf != "" {
-		optConf = C.CString(conf.Conf)
-		defer C.free(unsafe.Pointer(optConf))
+	if keys != "" {
+		cKeys = C.CString(keys)
+		defer C.free(unsafe.Pointer(cKeys))
+	} else {
+		cKeys = nil
 	}
 
 	// create empty strings to capture zenroom's output
-	stdout := emptyString(maxString)
-	stderr := emptyString(maxString)
+	stdout := emptyString(BUFSIZE)
+	stderr := emptyString(BUFSIZE)
 	defer C.free(unsafe.Pointer(stdout))
 	defer C.free(unsafe.Pointer(stderr))
 
-	res := C.zenroom_exec_tobuf(
+	res := C.wrapper(
+		fun,
 		cScript,
-		optConf, optKeys, optData, C.int(conf.Verbosity),
-		stdout, maxString,
-		stderr, maxString,
+		cConf, cKeys, cData,
+		stdout, BUFSIZE,
+		stderr, BUFSIZE,
 	)
 
-	if res != 0 {
-		return nil, fmt.Errorf("error calling zenroom: %s ", C.GoString(stderr))
+	zen_res := ZenResult{
+		output: C.GoString(stdout),
+		logs: C.GoString(stderr),
 	}
 
-	return C.GoBytes(unsafe.Pointer(stdout), C.int(C.strlen(stdout))), nil
+	return zen_res, res == 0
 }
 
 // ZencodeExec is our primary public API method, and it is here that we call Zenroom's
@@ -165,72 +114,9 @@ func ZenroomExec(script []byte, options ...Option) ([]byte, error) {
 //
 // Returns the output of the execution of the Zenroom virtual machine, or an
 // error.
-func ZencodeExec(script []byte, options ...Option) ([]byte, error) {
-	var (
-		cScript                   *C.char
-		optKeys, optData, optConf *C.char
-	)
-
-	// capture the required script parameter
-	if script == nil {
-		return nil, fmt.Errorf("missing required script to process")
-	}
-	cScript = (*C.char)(unsafe.Pointer(&script[0]))
-
-	// set up our default config
-	conf := &config{
-		Conf:      "",
-		Verbosity: 1,
-	}
-
-	// and now we iterate through our options, to update our config object
-	for _, option := range options {
-		option(conf)
-	}
-
-	if conf.Keys != nil {
-		optKeys = (*C.char)(unsafe.Pointer(&conf.Keys[0]))
-	}
-
-	if conf.Data != nil {
-		optData = (*C.char)(unsafe.Pointer(&conf.Data[0]))
-	}
-
-	if conf.Conf != "" {
-		optConf = C.CString(conf.Conf)
-		defer C.free(unsafe.Pointer(optConf))
-	}
-
-	// create empty strings to capture zenroom's output
-	stdout := emptyString(maxString)
-	stderr := emptyString(maxString)
-	defer C.free(unsafe.Pointer(stdout))
-	defer C.free(unsafe.Pointer(stderr))
-
-	res := C.zencode_exec_tobuf(
-		cScript,
-		optConf, optKeys, optData, C.int(conf.Verbosity),
-		stdout, maxString,
-		stderr, maxString,
-	)
-
-	if res != 0 {
-		return nil, fmt.Errorf("error calling zenroom: %s ", C.GoString(stderr))
-	}
-
-	return C.GoBytes(unsafe.Pointer(stdout), C.int(C.strlen(stdout))), nil
-}
 
 // reimplementation of https://golang.org/src/strings/strings.go?s=13172:13211#L522
 func emptyString(size int) *C.char {
-	p := C.malloc(C.size_t(size + 1))
-	// largest array size that can be used on all architectures
-	pp := (*[1 << 30]byte)(p)
-	bp := copy(pp[:], " ")
-	for bp < size {
-		copy(pp[bp:], pp[:bp])
-		bp *= 2
-	}
-	pp[size] = 0
+	p := C.malloc(C.size_t(size))
 	return (*C.char)(p)
 }
--- bindings/golang/zenroom/zenroom_test.go
+++ bindings/golang/zenroom/zenroom_test.go
@@ -1,91 +1,77 @@
-package zenroom_test
+package zenroom
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
-
-	"github.com/dyne/Zenroom/bindings/golang/zenroom"
 )
 
 func TestBasicCall(t *testing.T) {
-	script := []byte(`print (1)`)
-	res, err := zenroom.ZenroomExec(script)
-	if err != nil {
-		t.Error(err)
-	}
-	if !reflect.DeepEqual(res, []byte("1\n")) {
-		t.Errorf("calling print (1), got:%s len:%d", res, len(res))
+	script := "print (1)"
+	res, _ := ZenroomExec(script, "", "", "")
+	if res.output == "1" {
+		t.Errorf("calling print (1), got:%s", res.output)
 	}
 }
 
 func TestVersion(t *testing.T) {
-	script := []byte(`print(VERSION)`)
-	res, err := zenroom.ZenroomExec(script)
-	if err != nil {
-		t.Error(err)
-	}
+	script := "print(VERSION)"
+	res, _ := ZenroomExec(script, "", "", "")
 
-	fmt.Printf("Zenroom version: %s\n", res)
+	fmt.Printf("Zenroom version: %s\n", res.output)
 }
 
 func TestCallStrings(t *testing.T) {
 	testcases := []struct {
 		label  string
-		script []byte
-		data   []byte
-		resp   []byte
+		script string
+		resp   string
 	}{
 		{
 			label:  "string variable",
-			script: []byte(`hello = 'Hello World!' print(hello)`),
-			resp:   []byte("Hello World!\n"),
+			script: "hello = 'Hello World!' print(hello)",
+			resp:   "Hello World!\n",
 		},
 		{
 			label:  "naked string",
-			script: []byte(`print('hello')`),
-			resp:   []byte("hello\n"),
+			script: "print('hello')",
+			resp:   "hello\n",
 		},
 	}
 	for _, testcase := range testcases {
 		t.Run(testcase.label, func(t *testing.T) {
-			res, err := zenroom.ZenroomExec(testcase.script, zenroom.WithData(testcase.data))
-			if err != nil {
-				t.Error(err)
-			}
-
-			if !reflect.DeepEqual(res, testcase.resp) {
-				t.Errorf("calling [%s] got %s of len %d", testcase.script, res, len(res))
+			res, _ := ZenroomExec(testcase.script, "", "", "")
+			if res.output != testcase.resp {
+				t.Errorf("calling [%s] got %s", testcase.script, res.output)
 			}
 		})
 	}
 }
 
-func BenchmarkBasicPrint(b *testing.B) {
-	script := []byte(`print ('hello')`)
-	for n := 0; n < b.N; n++ {
-		_, _ = zenroom.ZenroomExec(script)
-	}
-}
+// func BenchmarkBasicPrint(b *testing.B) {
+// 	script := []byte(`print ('hello')`)
+// 	for n := 0; n < b.N; n++ {
+// 		_, _ = ZenroomExec(script)
+// 	}
+// }
 
-func BenchmarkBasicKeyandEncrypt(b *testing.B) {
-	script := []byte(`
-	msg = str(DATA)
-	kr = ECDH.new()
-	kr:keygen()
-	encrypted = ECDH.encrypt(kr, kr:public(), msg, kr:public())
-	print (encrypted)
-	`)
-	data := []byte(`temperature:25.1`)
+// func BenchmarkBasicKeyandEncrypt(b *testing.B) {
+// 	script := []byte(`
+// 	msg = str(DATA)
+// 	kr = ECDH.new()
+// 	kr:keygen()
+// 	encrypted = ECDH.encrypt(kr, kr:public(), msg, kr:public())
+// 	print (encrypted)
+// 	`)
+// 	data := []byte(`temperature:25.1`)
 
-	for n := 0; n < b.N; n++ {
-		_, _ = zenroom.ZenroomExec(script, zenroom.WithData(data))
-	}
-}
+// 	for n := 0; n < b.N; n++ {
+// 		_, _ = ZenroomExec(script, WithData(data))
+// 	}
+// }
 
-func ExampleZenroomExec() {
-	script := []byte(`print("hello")`)
-	res, _ := zenroom.ZenroomExec(script)
-	fmt.Println(string(res))
-	// Output: hello
-}
+// func ExampleZenroomExec() {
+// 	script := []byte(`print("hello")`)
+// 	res, _ := ZenroomExec(script)
+// 	fmt.Println(string(res))
+// 	// Output: hello
+// }
