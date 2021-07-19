1c53a1f120a22ed81b065e836b6c9f1ccb0b5186
Puria Nafisi Azizi <puria@dyne.org>
2021-07-19 18:30:14 +0200

    fix(javascript): 💚  Update tests with new debug output format (#213)
    * fix(javascript): 💚  Update tests with new debug output format
    
    Fixes: #212

--- bindings/javascript/src/index.spec.ts
+++ bindings/javascript/src/index.spec.ts
@@ -69,7 +69,7 @@ test("handle broken zencode", async (t) => {
   } catch (e) {
     t.true(
       e.logs.includes(
-        `[!] [string "ZEN:begin()..."]:2: Invalid Zencode line 1: sapodksapodk`
+        `[!] /zencode.lua:462: Invalid Zencode line 1: sapodksapodk` 
       )
     );
   }
