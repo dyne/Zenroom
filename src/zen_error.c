f5e47ca3e36665598878658917feebe56842b085
Jaromil <jaromil@dyne.org>
2021-07-27 18:50:09 +0200

    zen_error internal functions implementations

--- src/zen_error.c
+++ src/zen_error.c
@@ -1,6 +1,6 @@
 /* This file is part of Zenroom (https://zenroom.dyne.org)
  *
- * Copyright (C) 2017-2019 Dyne.org foundation
+ * Copyright (C) 2017-2021 Dyne.org foundation
  * designed, written and maintained by Denis Roio <jaromil@dyne.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,6 +17,11 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
+
+
+#ifdef __ANDROID__
+#include <android/log.h>
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,8 +40,8 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
-#include <zen_error.h>
 #include <zenroom.h>
+#include <zen_error.h>
 
 int lerror(lua_State *L, const char *fmt, ...) {
 	va_list argp;
@@ -62,3 +67,135 @@ int zencode_traceback(lua_State *L) {
 	lua_pop(L,1);
 	return 0;
 }
+
+// ANSI colors for terminal
+const char* ANSI_RED     = "\x1b[1;31m";
+const char* ANSI_GREEN   = "\x1b[1;32m";
+const char* ANSI_YELLOW  = "\x1b[1;33m";
+const char* ANSI_BLUE    = "\x1b[1;34m";
+const char* ANSI_MAGENTA = "\x1b[35m";
+const char* ANSI_CYAN    = "\x1b[36m";
+const char* ANSI_RESET   = "\x1b[0m";
+
+extern zenroom_t *Z;
+static char pfx[MAX_STRING];
+
+extern int zen_write_err_va(const char *fmt, va_list va);
+
+static int verbosity = 1;
+int get_debug() { return(verbosity); }
+void set_debug(int lev) {
+  lev = lev<0 ? 0 : lev;
+  lev = lev>3 ? 3 : lev;
+  verbosity = lev;
+}
+
+static int color = 0;
+void set_color(int on) { color = on; }
+
+#ifdef __ANDROID__
+void error(lua_State *L, const char *format, ...) {
+	va_list arg;
+	va_start(arg, format);
+	__android_log_vprint(ANDROID_LOG_ERROR, "ZEN", format, arg);
+	va_end(arg);
+}
+void warning(lua_State *L, const char *format, ...) {
+	if(verbosity<1) return;
+	va_list arg;
+	va_start(arg, format);
+	__android_log_vprint(ANDROID_LOG_WARN, "ZEN", format, arg);
+	va_end(arg);
+}
+void notice(lua_State *L, const char *format, ...) {
+	if(verbosity<1) return;
+	va_list arg;
+	va_start(arg, format);
+	__android_log_vprint(ANDROID_LOG_INFO, "ZEN", format, arg);
+	va_end(arg);
+}
+void act(lua_State *L, const char *format, ...) {
+	if(verbosity<2) return;
+	va_list arg;
+	va_start(arg, format);
+	__android_log_vprint(ANDROID_LOG_DEBUG, "ZEN", format, arg);
+	va_end(arg);
+}
+void func(void *L, const char *format, ...) {
+	if(verbosity<3) return;
+	va_list arg;
+	va_start(arg, format);
+	__android_log_vprint(ANDROID_LOG_VERBOSE, "ZEN", format, arg);
+	va_end(arg);
+}
+#else
+
+void notice(lua_State *L, const char *format, ...) {
+	(void)L;
+	if(!verbosity) return;
+	va_list arg;
+	snprintf_t pr = Z ? Z->snprintf : &snprintf;
+	if(color)
+		(*pr)(pfx, MAX_STRING-1, "%s[*]%s %s\n",ANSI_GREEN,ANSI_RESET,format);
+	else
+		(*pr)(pfx, MAX_STRING-1, "[*] %s\n",format);
+	va_start(arg, format);
+	zen_write_err_va(pfx, arg);
+	va_end(arg);
+}
+
+void func(void *L, const char *format, ...) {
+	(void)L;
+	if(verbosity<3) return;
+	va_list arg;
+	snprintf_t pr = Z ? Z->snprintf : &snprintf;
+	(*pr)(pfx, MAX_STRING-1, "[D] %s\n",format);
+	va_start(arg, format);
+	zen_write_err_va(pfx, arg);
+	va_end(arg);
+
+}
+
+void error(lua_State *L, const char *format, ...) {
+	(void)L;
+	if(!format) return;
+	va_list arg;
+	snprintf_t pr = Z ? Z->snprintf : &snprintf;
+	if(color)
+		(*pr)(pfx, MAX_STRING-1, "%s[!]%s %s\n",ANSI_RED,ANSI_RESET,format);
+	else
+		(*pr)(pfx, MAX_STRING-1, "[!] %s\n",format);
+	va_start(arg, format);
+	zen_write_err_va(pfx, arg);
+	va_end(arg);
+	if(Z) Z->errorlevel = 3;
+	// exit(1); // calls teardown (signal 11) TODO: check if OK with seccomp
+}
+
+void act(lua_State *L, const char *format, ...) {
+	(void)L;
+	if(!verbosity) return;
+	va_list arg;
+	snprintf_t pr = Z ? Z->snprintf : &snprintf;
+	(*pr)(pfx, MAX_STRING-1, " .  %s\n",format);
+	va_start(arg, format);
+	zen_write_err_va(pfx, arg);
+	va_end(arg);
+}
+
+void warning(lua_State *L, const char *format, ...) {
+	(void)L;
+	if(verbosity<2) return;
+	va_list arg;
+	snprintf_t pr = Z ? Z->snprintf : &snprintf;
+	if(color)
+		(*pr)(pfx, MAX_STRING-1, "%s[W]%s %s\n",ANSI_YELLOW,ANSI_RESET,format);
+	else
+		(*pr)(pfx, MAX_STRING-1, "[W] %s\n",format);
+	va_start(arg, format);
+	zen_write_err_va(pfx, arg);
+	va_end(arg);
+	if(Z) Z->errorlevel = 2;
+}
+
+#endif
