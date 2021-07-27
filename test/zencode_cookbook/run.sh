896f7e482e1733876de162233b9cf9a5cea0214a
Jaromil <jaromil@dyne.org>
2021-07-27 20:27:58 +0200

    meson fix to run all tests without long output

---
+++ test/zencode_cookbook/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# without output on screen, used by meson tests
+
+./run-dictionaries.sh "$1" > /dev/null
+./run-given.sh "$1" > /dev/null
+./run-hash-pdf.sh "$1" > /dev/null
+./run-intro.sh "$1" > /dev/null
+./run-scenarios-ecdh-encrypt-json.sh "$1" > /dev/null
+./run-scenarios-ecdh.sh "$1" > /dev/null
+
+./run-then.sh "$1" > /dev/null
+
+./run-when.sh "$1" > /dev/null
+
