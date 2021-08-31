a7ae6b31c5a4d9da926ce8a0c72bc309508313ac
Alberto Lerda <albertolerda97@gmail.com>
2021-08-31 09:22:03 +0200

    Used empty octet O.new instead of nil

--- test/ethereum.lua
+++ test/ethereum.lua
@@ -5,14 +5,12 @@ function encodeRLP(data)
    local header = nil
    local res = nil
    local byt = nil
-   if data == nil then
-      return O.from_hex('80')
-   end
+
    if type(data) == 'table' then
       -- empty octet
       res = O.new()
-      for i=2, data[1]+1, 1 do
-	    res = res .. encodeRLP(data[i])
+      for _, v in pairs(data) do
+	 res = res .. encodeRLP(v)
       end
       if #res < 56 then
 	 res = INT.new(192+#res):octet() .. res
@@ -28,7 +26,10 @@ function encodeRLP(data)
 
       -- Empty octet?
       -- index single bytes of an octet
-      local byt = INT.new( res:chop(1) )
+      local byt = INT.new(0)
+      if #res > 0 then
+	 byt = INT.new( res:chop(1) )
+      end
 
       if #res ~= 1 or byt >= INT.new(128) then
 	 if #res < 56 then
@@ -52,15 +53,15 @@ end
 assert(encodeRLP(O.from_hex('7f')) == O.from_hex('7f'))
 assert(encodeRLP(O.from_hex('ff')) == O.from_hex('81ff'))
 -- ATTENTION empty sequence
-assert(encodeRLP(nil) == O.from_hex('80'))
+assert(encodeRLP(O.new()) == O.from_hex('80'))
 assert(encodeRLP(O.from_hex('00')) == O.from_hex('00'))
 assert(encodeRLP(O.from_hex('1122334455667788112233445566778811223344556677881122334455667788')) == O.from_hex('a01122334455667788112233445566778811223344556677881122334455667788'))
 assert(encodeRLP(O.from_hex('11223344556677881122334455667788112233445566778811223344556677881122334455667788112233445566778811223344556677881122334455667788')) == O.from_hex('b84011223344556677881122334455667788112233445566778811223344556677881122334455667788112233445566778811223344556677881122334455667788'))
 
-assert(encodeRLP({2, O.from_hex('11223344556677881122334455667788'), O.from_hex('1122334455667788')}) == O.from_hex('da9011223344556677881122334455667788881122334455667788'))
+assert(encodeRLP({O.from_hex('11223344556677881122334455667788'), O.from_hex('1122334455667788')}) == O.from_hex('da9011223344556677881122334455667788881122334455667788'))
 
-assert(encodeRLP({3, O.from_hex('627306090abab3a6e1400e9345bc60c78a8bef57'), O.from_hex('ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'), O.from_hex('8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63')}) == O.from_hex('f85794627306090abab3a6e1400e9345bc60c78a8bef57a0ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162fa08f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63'))
-assert(encodeRLP({4, O.from_hex('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'), O.from_hex('627306090abab3a6e1400e9345bc60c78a8bef57'), O.from_hex('ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'), O.from_hex('8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63')}) == O.from_hex('f878a0c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d394627306090abab3a6e1400e9345bc60c78a8bef57a0ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162fa08f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63'))
+assert(encodeRLP({O.from_hex('627306090abab3a6e1400e9345bc60c78a8bef57'), O.from_hex('ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'), O.from_hex('8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63')}) == O.from_hex('f85794627306090abab3a6e1400e9345bc60c78a8bef57a0ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162fa08f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63'))
+assert(encodeRLP({O.from_hex('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'), O.from_hex('627306090abab3a6e1400e9345bc60c78a8bef57'), O.from_hex('ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'), O.from_hex('8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63')}) == O.from_hex('f878a0c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d394627306090abab3a6e1400e9345bc60c78a8bef57a0ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162fa08f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63'))
 
 
 -- the transaction I want to encode is (e.g.)
@@ -80,22 +81,22 @@ assert(encodeRLP({4, O.from_hex('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5
 -- 0 is encoded as the empty octet, which is treated as nil
 
 tx = {}
-tx["nonce"] = nil
+tx["nonce"] = O.new()
 tx["gasPrice"] = INT.new(1000)
 tx["gasLimit"] = INT.new(25000) 
 tx["to"] = O.from_hex('627306090abaB3A6e1400e9345bC60c78a8BEf57')
 tx["value"] = O.from_hex('11')
-tx["data"] = nil
+tx["data"] = O.new()
 -- v contains the chain id (when the transaction is not signed)
 tx["v"] = INT.new(1337)
-tx["r"] = nil
-tx["s"] = nil
+tx["r"] = O.new()
+tx["s"] = O.new()
 
 from = O.from_hex('ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f')
 
 
 function encodeTransaction(tx)
-   local fields = {9, tx["nonce"], tx["gasPrice"], tx["gasLimit"], tx["to"],
+   local fields = {tx["nonce"], tx["gasPrice"], tx["gasLimit"], tx["to"],
 	     tx["value"], tx["data"], tx["v"], tx["r"], tx["s"]}
    return encodeRLP(fields)
 end
