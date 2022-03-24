09cdf58600b4251e4c304194e87349ee24e0f202
matteocristino <cristinomatteo4@gmail.com>
2022-03-24 17:11:38 +0100

    updated to match the new schnorr functions

--- test/schnorr.lua
+++ test/schnorr.lua
@@ -6,15 +6,15 @@ local SCH = require("crypto_schnorr_signature")
 
 print()
 print'SCHNORR SIGN/VERIFY TEST'
-local kp = SCH.keygen()
+local sk = SCH.keygen()
 
 local m = OCTET.random(32)
-print'ietrate 100 tests of sign/verify and some checks'
+print'ietrate 100 tests of pubgen, sign, verify and some checks'
 for i=1,100 do
-   assert(SCH.pubgen(kp.private) == kp.public, "schnorr pubgen failed")
-   assert(SCH.pubcheck(kp.public), "schnorr pubchek failed")
-   assert(SCH.seccheck(kp.private), "schnorr seccheck failed")
-   local sig = SCH.sign(kp.private, m)
+   local pk = SCH.pubgen(sk)
+   assert(SCH.pubcheck(pk), "schnorr pubchek failed")
+   assert(SCH.seccheck(sk), "schnorr seccheck failed")
+   local sig = SCH.sign(sk, m)
    assert(SCH.sigcheck(sig), "schnorr sigcheck failed")
-   assert(SCH.verify(kp.public, m, sig), "schnorr verify failed")
+   assert(SCH.verify(pk, m, sig), "schnorr verify failed")
 end
