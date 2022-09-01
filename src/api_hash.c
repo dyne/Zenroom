30f0b32d5fcddb9246103d24f2dc8bcf0b7ba29f
Jaromil <jaromil@dyne.org>
2022-09-01 18:30:04 +0200

    external hash api change to use stdout
    facilitate wrapping in js using stdout as pass-through
    
    extend tests to different processes
    evolve test unit to use bats

--- src/api_hash.c
+++ src/api_hash.c
@@ -32,9 +32,17 @@
  // first byte is type
 #define SHA512 '4'
 
+inline void print_ctx_hex(char prefix, void *sh, int len) {
+  char *hash_ctx = malloc((len<<1)+4);
+  hash_ctx[0] = prefix;
+  buf2hex(hash_ctx+1, (const char*)sh, (const size_t)len);
+  hash_ctx[(len<<1)+2] = 0x0; // null terminated string
+  fprintf(stdout, "%s\n", hash_ctx);
+  free(hash_ctx);
+}
+
 // returns a fills hash_ctx, which must be pre-allocated externally
-int zenroom_hash_init(const char *hash_type,
-		      char *hash_ctx, const int hash_ctx_size) {
+int zenroom_hash_init(const char *hash_type) {
   register char prefix = '0';
   // size tests
   register int len = 0; 
@@ -49,23 +57,13 @@ int zenroom_hash_init(const char *hash_type,
     zerror(NULL, "%s :: invalid hash type: %s", __func__, hash_type);
     return 4; // ERR_INIT
   }
-  if(hash_ctx_size < (len+4)<<1) { // size*2 because hex encoded
-    free(sh);
-    zerror(NULL, "%s :: invalid hash context size: %u <= %u",
-	   __func__, hash_ctx_size, (len+4)<<1);
-    return 4;
-  }
-
-  // serialize
-  hash_ctx[0] = prefix;
-  buf2hex(hash_ctx+1, (const char*)sh, (const size_t)len);
-  hash_ctx[(len<<1)+2] = 0x0; // null terminated string
+  print_ctx_hex(prefix, sh, len);
   free(sh);
   return 0;
 }
 
 // returns hash_ctx updated
-int zenroom_hash_update(char *hash_ctx,
+int zenroom_hash_update(const char *hash_ctx,
 			const char *buffer, const int buffer_size) {
   register char prefix = hash_ctx[0];
   register int len;
@@ -78,28 +76,24 @@ int zenroom_hash_update(char *hash_ctx,
     for(c=0; c<buffer_size; c++) {
       HASH512_process((hash512*)sh, buffer[c]);
     }
-    buf2hex(hash_ctx+1, (const char*)sh, (const size_t)len);
   } else {
     zerror(NULL, "%s :: invalid hash context prefix: %c", __func__, prefix);
     return 3;
   }
+  print_ctx_hex(prefix, sh, len);
   free(sh);
   return 0;
 }
 
 // returns the hash string base64 encoded
-int zenroom_hash_final(const char *hash_ctx,
-		       char *hash_result, const int hash_result_size) {
-  register int len;
+int zenroom_hash_final(const char *hash_ctx) {
   register char prefix = hash_ctx[0];
+  register int len;
+  char *hash_result;
   octet tmp;
   char *sh;
   if(prefix==SHA512) {
-    if(hash_result_size<90) { // base64 is 88 with padding
-      zerror(NULL, "%s :: invalid hash result size: %u <= %u",
-	     __func__, hash_result_size, 64);
-      return 3;
-    }
+    hash_result = malloc(90); // base64 is 88 with padding
     tmp.len = 64;
     tmp.val = (char*)malloc(64);
     // TODO: check malloc result
@@ -114,6 +108,8 @@ int zenroom_hash_final(const char *hash_ctx,
   }
   OCT_tobase64(hash_result,&tmp);
   free(tmp.val);
+  fprintf(stdout, "%s\n", hash_result);
+  free(hash_result);
   free(sh);
   return 0;
 }
---
+++ test/api/hash_init.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <zenroom.h>
+
+int main(int argc, char **argv) {
+  int res;
+  res = zenroom_hash_init("sha512");
+  if(res!=0){fprintf(stderr,"Abort on error code %u\n",res);exit(res);}
+  exit(0);
+}
