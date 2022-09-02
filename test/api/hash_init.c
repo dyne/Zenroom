0a5b01d4b0b60f0b711e446f798680e33b099f0d
Jaromil <jaromil@dyne.org>
2022-09-02 10:53:42 +0200

    fix test init from argv

--- test/api/hash_init.c
+++ test/api/hash_init.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv) {
   int res;
-  res = zenroom_hash_init("sha512");
+  res = zenroom_hash_init(argv[1]);
   if(res!=0){fprintf(stderr,"Abort on error code %u\n",res);exit(res);}
   exit(0);
 }
