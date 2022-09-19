5d8561d623ef75250d17ca133849d2e734a6ce8b
Jaromil <jaromil@dyne.org>
2022-09-19 15:22:56 +0200

    meson targets for ccache
    also adopt ccache in meson matrix builds

--- build/meson.mk
+++ build/meson.mk
@@ -13,12 +13,20 @@ prepare-meson:
 	ln -sf ${pwd}/lib/mimalloc/build/libmimalloc-static.a ${pwd}/meson/libmimalloc-static.a
 
 run-meson:
-	CC=${gcc} AR="${ar}"  CFLAGS="${cflags}" LDFLAGS="${ldflags}" LDADD="${ldadd}" \
+	CC="${gcc}" AR="${ar}"  CFLAGS="${cflags}" LDFLAGS="${ldflags}" LDADD="${ldadd}" \
 	meson -Dexamples=true -Ddocs=true -Doptimization=3 \
 	-Decdh_curve=${ecdh_curve} -Decp_curve=${ecp_curve} \
 	-Ddefault_library=both build meson
 
-meson: linux-meson-clang-debug
+meson: linux-meson
+meson-ccache: linux-meson
+meson-debug:  linux-meson
+meson-debug-ccache: linux-meson
+meson-clang-ccache: linux-meson
+meson-clang-debug-ccache: linux-meson
+
+linux-meson: ${TARGETS} prepare-meson run-meson
+	ninja -C meson
 
 linux-meson-release: ${TARGETS} prepare-meson run-meson
 	ninja -C meson
