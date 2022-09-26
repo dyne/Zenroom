4e805df34d2fda83179df97956c5e30a53629ff7
Jaromil <jaromil@dyne.org>
2022-09-26 18:29:30 +0200

    pass lua context to big_init
    fixes correct messaging

--- src/zen_ecdh.c
+++ src/zen_ecdh.c
@@ -508,7 +508,7 @@ static int ecdh_order(lua_State *L) {
                 return 0;
         }
 	big *o = big_new(L); SAFE(o);
-        big_init(o);
+        big_init(L,o);
         BIG_fromBytesLen(o->val, ECDH.order, ECDH.mod_size);
 	return 1;
 }
@@ -525,7 +525,7 @@ static int ecdh_prime(lua_State *L) {
                 return 0;
         }
 	big *p = big_new(L); SAFE(p);
-        big_init(p);
+        big_init(L,p);
         BIG_fromBytesLen(p->val, ECDH.prime, ECDH.mod_size);
 	return 1;
 }
