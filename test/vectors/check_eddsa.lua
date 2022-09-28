027425e2b53d19aa7a21e4fed10afbd7dd5e0268
matteo-cristino <cristino.matteo97@gmail.com>
2022-09-28 12:13:01 +0200

    remove print left by mistake

--- test/vectors/check_eddsa.lua
+++ test/vectors/check_eddsa.lua
@@ -5,24 +5,19 @@ local function newline_iter(text)
 	return s:gmatch('(.-)\n') -- iterators return functions
 end
 
-local nr = 0
 for line in newline_iter(DATA) do
-   nr = nr + 1
-   print('############')
    local tokens = strtok(line, "[^:]*")
-   I.spy(tokens)
    local sk = O.from_hex(tokens[1]:sub(1, 64))
    local pk = O.from_hex(tokens[2])
    assert(ED.pubgen(sk) == pk)
    local m = nil
-   if tokens[3] ~= "" and tokens[4] then
+   if tokens[3] ~= "" then
       m = O.from_hex(tokens[3])
    else
       m = O.new()
    end
-   print(nr)
    local sig = O.from_hex(tokens[4]:sub(1,128))
-   
+
    assert(ED.sign(sk, m) == sig)
    assert(ED.verify(pk, sig, m))
 end
