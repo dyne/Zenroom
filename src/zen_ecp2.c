38f9e8cdd1d28525dcbb936a0aad0b76890e187f
matteo-cristino <cristino.matteo97@gmail.com>
2022-10-14 14:46:37 +0200

    refactor zen_ecp2.c

--- src/zen_ecp2.c
+++ src/zen_ecp2.c
@@ -91,8 +91,10 @@ ecp2* ecp2_dup(lua_State *L, ecp2* in) {
 }
 
 int ecp2_destroy(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1);
 	SAFE(e);
+	END();
 	return 0;
 }
 
@@ -114,7 +116,7 @@ If only the first two arguments are provided (X and Xi), then Y and Yi are calcu
     @function ECP2.new(X, Xi, Y, Yi)
 */
 static int lua_new_ecp2(lua_State *L) {
-
+	BEGIN();
 // TODO: unsafe and only needed when running tests
 #ifdef DEBUG
 	void *tx  = luaL_testudata(L, 1, "zenroom.big");
@@ -147,21 +149,37 @@ static int lua_new_ecp2(lua_State *L) {
 			warning(L, "new ECP2 value out of curve (points to infinity)");
 		return 1; }
 #endif
-
-	octet *o = o_arg(L, 1); SAFE(o);
+	char *failed_msg = NULL;
+	octet *o = NULL;
+	o = o_arg(L, 1);
+	if(o == NULL) {
+		failed_msg = "failed to allocate space for ecp2 point";
+		goto end;
+	}
 	ecp2 *e = ecp2_new(L); SAFE(e);
-	if(! ECP2_fromOctet(&e->val, o) )
-		lerror(L, "Octet doesn't contains a valid ECP2");
+	if(! ECP2_fromOctet(&e->val, o) ) {
+		failed_msg = "Octet doesn't contains a valid ECP2";
+		goto end;
+	}
+end:
+	o_free(o);
+	if(failed_msg != NULL) {
+		lerror(L, failed_msg);
+		lua_pushnil(L);
+	}
+	END();
 	return 1;
 }
 
 /***
-    Returns the generator of the twisted curve: an ECP2 point to its X and Y coordinates.
+    Returns the generator of the twisted curve:
+    an ECP2 point to its X and Y coordinates.
 
     @function generator()
     @return ECP2 coordinates of the curve's generator.
 */
 static int ecp2_generator(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_new(L); SAFE(e);
 /* 	FP2 x, y;
 	FP2_from_BIGs(&x, (chunk*)CURVE_G2xa, (chunk*)CURVE_G2xb);
@@ -171,11 +189,13 @@ static int ecp2_generator(lua_State *L) {
 		return 0; }
  */
 	ECP2_generator(&e->val);
+	END();
 	return 1;
 }
 
 
 static int ecp2_millerloop(lua_State *L) {
+	BEGIN();
 	fp12 *f = fp12_new(L);   SAFE(f);
 	ecp2 *x = ecp2_arg(L, 1); SAFE(x);
 	ecp  *y = ecp_arg(L, 2);  SAFE(y);
@@ -183,6 +203,7 @@ static int ecp2_millerloop(lua_State *L) {
 	ECP_affine(&y->val);
 	PAIR_ate(&f->val, &x->val, &y->val);
 	PAIR_fexp(&f->val);
+	END();
 	return 1;
 }
 
@@ -195,9 +216,11 @@ static int ecp2_millerloop(lua_State *L) {
     @return affine version of the ECP2 point
 */
 static int ecp2_affine(lua_State *L) {
+	BEGIN();
 	ecp2 *in = ecp2_arg(L, 1); SAFE(in);
 	ecp2 *out = ecp2_dup(L, in); SAFE(out);
 	ECP2_affine(&out->val);
+	END();
 	return 1;
 }
 
@@ -208,8 +231,10 @@ static int ecp2_affine(lua_State *L) {
     @return ECP2 pointing to infinity (out of the curve).
 */
 static int ecp2_get_infinity(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_new(L); SAFE(e);
 	ECP2_inf(&e->val);
+	END();
 	return 1;
 }
 
@@ -220,13 +245,17 @@ static int ecp2_get_infinity(lua_State *L) {
     @return false if point is on curve, true if its off curve into infinity.
 */
 static int ecp2_isinf(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	lua_pushboolean(L, ECP2_isinf(&e->val));
+	END();
 	return 1;
 }
 
 /***
-    Add two ECP2 points to each other (commutative and associative operation). Can be made using the overloaded operator `+` between two ECP2 objects just like they would be numbers.
+    Add two ECP2 points to each other (commutative and associative operation).
+    Can be made using the overloaded operator `+` between two ECP2 objects
+    just like they would be numbers.
 
     @param first ECP2 point to be summed
     @param second ECP2 point to be summed
@@ -234,17 +263,21 @@ static int ecp2_isinf(lua_State *L) {
     @return sum resulting from the addition
 */
 static int ecp2_add(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	ecp2 *q = ecp2_arg(L, 2); SAFE(q);
 	ecp2 *p = ecp2_dup(L, e); // push
 	SAFE(p);
 	ECP2_add(&p->val, &q->val);
+	END();
 	return 1;
 }
 
 
 /***
-    Subtract an ECP2 point from another (commutative and associative operation). Can be made using the overloaded operator `-` between two ECP2 objects just like they would be numbers.
+    Subtract an ECP2 point from another (commutative and associative operation).
+    Can be made using the overloaded operator `-` between two ECP2 objects
+    just like they would be numbers.
 
     @param first ECP2 point from which the second should be subtracted
     @param second ECP2 point to use in the subtraction
@@ -252,11 +285,13 @@ static int ecp2_add(lua_State *L) {
     @return new ECP2 point resulting from the subtraction
 */
 static int ecp2_sub(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	ecp2 *q = ecp2_arg(L, 2); SAFE(q);
 	ecp2 *p = ecp2_dup(L, e); // push
 	SAFE(p);
 	ECP2_sub(&p->val, &q->val);
+	END();
 	return 1;
 }
 
@@ -266,15 +301,19 @@ static int ecp2_sub(lua_State *L) {
     @function negative()
 */
 static int ecp2_negative(lua_State *L) {
+	BEGIN();
 	ecp2 *in = ecp2_arg(L, 1); SAFE(in);
 	ecp2 *out = ecp2_dup(L, in); SAFE(out);
 	ECP2_neg(&out->val);
+	END();
 	return 1;
 }
 
 
 /***
-    Compares two ECP2 points and returns true if they indicate the same point on the curve (they are equal) or false otherwise. It can also be executed by using the `==` overloaded operator.
+    Compares two ECP2 points and returns true if they indicate the same
+    point on the curve (they are equal) or false otherwise.
+    It can also be executed by using the `==` overloaded operator.
 
     @param first ECP2 point to be compared
     @param second ECP2 point to be compared
@@ -282,6 +321,7 @@ static int ecp2_negative(lua_State *L) {
     @return bool value: true if equal, false if not equal
 */
 static int ecp2_eq(lua_State *L) {
+	BEGIN();
 	ecp2 *p = ecp2_arg(L, 1); SAFE(p);
 	ecp2 *q = ecp2_arg(L, 2); SAFE(q);
 // TODO: is affine rly needed?
@@ -289,104 +329,142 @@ static int ecp2_eq(lua_State *L) {
 	ECP2_affine(&q->val);
 	lua_pushboolean(L, ECP2_equals(
 		                &p->val, &q->val));
+	END();
 	return 1;
 }
 
 
 /***
-    Returns an octet containing all serialized @{BIG} number coordinatesof an ECP2 point on the curve. It can be used to port the value of an ECP2 point into @{OCTET:hex} or @{OCTET:base64} encapsulation, to be later set again into an ECP2 point using @{ECP2:new}.
+    Returns an octet containing all serialized @{BIG} number coordinates
+    of an ECP2 point on the curve. It can be used to port the value of an
+    ECP2 point into @{OCTET:hex} or @{OCTET:base64} encapsulation,
+    to be later set again into an ECP2 point using @{ECP2:new}.
 
     @function octet()
     @return an OCTET sequence
 */
 static int ecp2_octet(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	octet *o = o_new(L, (MODBYTES<<2)+1);
 	SAFE(o);
 	ECP2_toOctet(o, &e->val);
+	END();
 	return 1;
 }
 
 static int ecp2_mul(lua_State *L) {
+	BEGIN();
 	ecp2 *p = ecp2_arg(L, 1); SAFE(p);
 	big  *b = big_arg(L, 2); SAFE(b);
-	ecp2 *r = ecp2_dup(L, p); SAFE(r);	
+	ecp2 *r = ecp2_dup(L, p); SAFE(r);
 	PAIR_G2mul(&r->val, b->val);
+	END();
 	return 1;
 }
 
 
 /***
-    Map a @{BIG} number to a point of the curve, where the BIG number should be the output of some hash function.
+    Map a @{BIG} number to a point of the curve,
+    where the BIG number should be the output of some hash function.
 
     @param BIG number resulting from an hash function
     @function mapit(BIG)
 */
 static int ecp2_mapit(lua_State *L) {
-	octet *o = o_arg(L, 1); SAFE(o);
+	BEGIN();
+	char *failed_msg = NULL;
+	octet *o = NULL;
+	o = o_arg(L, 1);
+	if(o == NULL) {
+		failed_msg = "failed to allocate space for ecp2 point";
+		goto end;
+	}
 	if(o->len != 64) {
-		zerror(L, "octet length is %u instead of 64 (need to use sha512)", o->len);
-		lerror(L, "Invalid argument to ECP2.mapit(), not an hash");
-		return 0; }
+		zerror(L, "octet length is %u instead of 64 (need to use sha512)",
+		       o->len);
+		failed_msg = "Invalid argument to ECP2.mapit(), not an hash";
+		goto end;
+	}
 	ecp2 *e = ecp2_new(L); SAFE(e);
 	ECP2_mapit(&e->val, o);
+end:
+	o_free(o);
+	if(failed_msg != NULL) {
+		lerror(L, failed_msg);
+		lua_pushnil(L);
+	}
+	END();
 	return 1;
 }
 
 // get the x coordinate real part as BIG
 static int ecp2_get_xr(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	FP fx;
 	big *xa = big_new(L); big_init(L,xa); SAFE(xa);
 	FP_copy(&fx, &e->val.x.a);
 	FP_reduce(&fx); FP_redc(xa->val, &fx);
+	END();
 	return 1;
 }
 // get the x coordinate imaginary part as BIG
 static int ecp2_get_xi(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	FP fx;
 	big *xb = big_new(L); big_init(L,xb); SAFE(xb);
 	FP_copy(&fx, &e->val.x.b);
 	FP_reduce(&fx); FP_redc(xb->val, &fx);
+	END();
 	return 1;
 }
 
 // get the y coordinate real part as BIG
 static int ecp2_get_yr(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	FP fy;
 	big *ya = big_new(L); big_init(L,ya); SAFE(ya);
 	FP_copy(&fy, &e->val.y.a);
 	FP_reduce(&fy); FP_redc(ya->val, &fy);
+	END();
 	return 1;
 }
 static int ecp2_get_yi(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	FP fy;
 	big *yb = big_new(L); big_init(L,yb); SAFE(yb);
 	FP_copy(&fy, &e->val.y.b);
 	FP_reduce(&fy); FP_redc(yb->val, &fy);
+	END();
 	return 1;
 }
 static int ecp2_get_zr(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	FP fz;
 	big *za = big_new(L); big_init(L,za); SAFE(za);
 	FP_copy(&fz, &e->val.z.a);
 	FP_reduce(&fz); FP_redc(za->val, &fz);
+	END();
 	return 1;
 }
 static int ecp2_get_zi(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	FP fz;
 	big *zb = big_new(L); big_init(L,zb); SAFE(zb);
 	FP_copy(&fz, &e->val.z.b);
 	FP_reduce(&fz); FP_redc(zb->val, &fz);
+	END();
 	return 1;
 }
 
 static int ecp2_output(lua_State *L) {
+	BEGIN();
 	ecp2 *e = ecp2_arg(L, 1); SAFE(e);
 	if (ECP2_isinf(&e->val)) { // Infinity
 		octet *o = o_new(L, 3); SAFE(o);
@@ -397,6 +475,7 @@ static int ecp2_output(lua_State *L) {
 	SAFE(o); lua_pop(L, 1);
 	_ecp2_to_octet(o, e);
 	push_octet_to_hex_string(L, o);
+	END();
 	return 1;
 }
 
