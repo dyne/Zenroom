26869e70c5b01b8e16fb5780875f2944f1e539c2
matteo-cristino <cristino.matteo97@gmail.com>
2022-11-22 18:19:31 +0100

    refactor(python): use strip also for version

--- bindings/python3/setup.py
+++ bindings/python3/setup.py
@@ -21,7 +21,7 @@ def get_versions():
     # performed in ZENROOM_ROOT
     # first char is v, last one is a newline
     python_version = subprocess.run(['git', 'describe', '--tags', '--abbrev=0'],
-                                     stdout=subprocess.PIPE).stdout.decode('utf-8')[1:-1]
+                                     stdout=subprocess.PIPE).stdout.decode('utf-8').strip('v\n')
     zenroom_version = python_version
     branch = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
                             stdout=subprocess.PIPE).stdout.decode('utf-8')
