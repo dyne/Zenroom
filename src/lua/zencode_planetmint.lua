fc573382886d721a0371da328b24bd310d7edb98
Alberto Lerda <albertolerda97@gmail.com>
2022-11-25 14:49:05 +0100

    add some tests

--- src/lua/zencode_planetmint.lua
+++ src/lua/zencode_planetmint.lua
@@ -3,8 +3,8 @@ local ED = require'ed'
 
 When("create the planetmint output signatures of ''", function(tx_name)
     empty'planetmint_output_signatures'
-    local serialized_tx = have(tx_name)
-    local tx = JSON.decode(serialized_tx:string())
+    local serialized_tx = have(tx_name):string()
+    local tx = JSON.decode(serialized_tx)
 
     local sk = havekey'eddsa'
     local planetmint_output_signatures = {}
@@ -13,12 +13,11 @@ When("create the planetmint output signatures of ''", function(tx_name)
     for index, input in pairs(tx.inputs) do
         local transactionUniqueFulfillment = nil
         if input.fulfills then
-            error("NOT IMPLEMENTED")
             transactionUniqueFulfillment = serialized_tx .. input.fulfills.transaction_id .. input.fulfills.output_index
         else
             transactionUniqueFulfillment = serialized_tx
         end
-        local transactionHash = SHA256:process(transactionUniqueFulfillment)
+        local transactionHash = SHA256:process(O.from_string(transactionUniqueFulfillment))
         local signature = ED.sign(sk, transactionHash)
         table.insert(planetmint_output_signatures, signature)
     end
