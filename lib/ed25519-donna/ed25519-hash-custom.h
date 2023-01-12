e208f699bc9957a6570779acdd8dc0e4f60a89e1
Alberto Lerda <albertolerda97@gmail.com>
2023-01-12 08:51:36 +0100

    fix: Use sha512 from milagro in ed25519-donna

--- lib/ed25519-donna/ed25519-hash-custom.h
+++ lib/ed25519-donna/ed25519-hash-custom.h
@@ -9,3 +9,30 @@
 	void ed25519_hash(uint8_t *hash, const uint8_t *in, size_t inlen);
 */
 
+#include <amcl.h>
+
+typedef hash512 ed25519_hash_context;
+
+extern void HASH512_init(hash512 *sh);
+extern void HASH512_process(hash512 *sh,int byt);
+extern void HASH512_hash(hash512 *sh,char *hash);
+
+void ed25519_hash_init(ed25519_hash_context *ctx) {
+	HASH512_init(ctx);
+}
+void ed25519_hash_update(ed25519_hash_context *ctx, const uint8_t *in, size_t inlen) {
+	for(size_t i = 0; i < inlen; i++) {
+		HASH512_process(ctx, in[i]);
+	}
+}
+void ed25519_hash_final(ed25519_hash_context *ctx, uint8_t *hash) {
+	HASH512_hash(ctx, hash);
+}
+
+static void
+ed25519_hash(uint8_t *hash, const uint8_t *in, size_t inlen) {
+	ed25519_hash_context ctx;
+	ed25519_hash_init(&ctx);
+	ed25519_hash_update(&ctx, in, inlen);
+	ed25519_hash_final(&ctx, hash);
+}
