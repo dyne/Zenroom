5e18a7f36f28e10f9843e7ae9e086deba470f07f
matteo-cristino <cristino.matteo97@gmail.com>
2023-01-23 13:34:49 +0100

    refactor(bindings): ♻️ reduce python binding source distribution

--- bindings/python3/prepare.sh
+++ bindings/python3/prepare.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-mkdir -p src
+mkdir -p src/docs/pages
+cp ../../docs/pages/python.md src/docs/pages/python.md
 cp ../../Makefile src/Makefile
 cp -r ../../build src/build
 cp -r ../../lib src/lib
 cp -r ../../src src/src
 cp -r ../../test src/test
-cp -r ../../docs src/docs
-cp -r ../../.git src/.git
+
+py_version=`git describe --tags --abbrev=0 | sed 's/^v//'`
+branch=`git rev-parse --abbrev-ref HEAD`
+hash=""
+time=""
+if [ "$branch" != "master" ]; then
+    hash="+$(git rev-parse --short HEAD)"
+    time=".dev$(git show -s --format=%ct HEAD)"
+fi
+echo "${py_version}${hash}" > src/git_utils
+echo "${py_version}${time}" >> src/git_utils
