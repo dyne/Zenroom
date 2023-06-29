fa9d03ac28b1d82ba0143c2ce838ae46a2f4f26d
Jaromil <jaromil@dyne.org>
2023-06-29 17:27:04 +0200

    improve zencode execution and support base64 encoded scripts
    this change removes one malloc and one memcpy for each zencode
    executed, as well introduces support for executing base64 encoded
    zencode scripts (autodetected using is_base64)
    
    it now takes advantage of zenroom internals to convert all base64 safely in
    protected mode, removing all dynamic allocations

--- src/zencode-exec.c
+++ src/zencode-exec.c
@@ -17,26 +17,14 @@
  * If not, see http://www.gnu.org/licenses/agpl.txt
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
-#include <amcl.h> // OCT_frombase64
-
 #include <zenroom.h>
 
 #include <sys/poll.h>
-
-char *alloc_from_b64(char *b64) {
-  octet o;
-  int encoded_size = strlen(b64);
-  o.max = ((encoded_size+3)>>2)*3;
-  o.val = malloc(o.max+1);
-  OCT_frombase64(&o, b64);
-  o.val[o.len-4] = '\0'; // -4 avoids some weird padding
-  return(o.val);
-}
-
 
 int main(int argc, char **argv) {
   (void)argc;
@@ -54,11 +42,6 @@ int main(int argc, char **argv) {
   data_b64[0] = 0x0;
   conf[0] = 0x0;
 
-  char *script = NULL;
-  char *keys = NULL;
-  char *data = NULL;
-
-
   fds.fd = 0; // stdin
   fds.events = POLLIN;
   ret = poll(&fds, 1, 0);
@@ -70,47 +53,44 @@ int main(int argc, char **argv) {
 	exit(1);
   }
 
-  // TODO: check for spaces and newlines and invalid base64 chars
-
   if( ! fgets(script_b64, MAX_ZENCODE, stdin) ) {
-	fprintf(stderr, "fgets #1 (script): %s\n",strerror(errno));
+	fprintf(stderr, "zencode-exec missing script on first line: %s\n",strerror(errno));
 	return EXIT_FAILURE;
   }
-  if(script_b64[0] != '\n') script = alloc_from_b64(script_b64);
-//  fprintf(stderr,"script in: %s\n",script);
 
   if( fgets(keys_b64, MAX_FILE, stdin) ) {
-	if(keys_b64[0] != '\n') keys = alloc_from_b64(keys_b64);
-//	fprintf(stderr,"keys in: %s\n",keys);
+	ret = strlen(keys_b64); keys_b64[ret-1] = 0x0; // remove newline
   }
-
   if( fgets(data_b64, MAX_FILE, stdin) ) {
-	if(data_b64[0] != '\n')	data = alloc_from_b64(data_b64);
-//	fprintf(stderr,"data in: %s\n",keys);
+	ret = strlen(data_b64); data_b64[ret-1] = 0x0; // remove newline
   }
-
   if( fgets(conf, MAX_CONFIG, stdin) ) {
+	if(strlen(conf)>=MAX_CONFIG) {
+	  fprintf(stderr,"zencode-exec error: conf string out of bounds.\n");
+	  return EXIT_FAILURE;
+	}
 	if(conf[0] != '\n') strcat(conf,",logfmt=json");
   } else {
-	sprintf(conf,"logfmt=json");
+	snprintf(conf,MAX_CONFIG,"logfmt=json");
   }
 
-  Z = zen_init(conf, keys, data);
+  Z = zen_init(conf,
+			   keys_b64[0]?keys_b64:NULL,
+			   data_b64[0]?data_b64:NULL);
   if(!Z) {
 	fprintf(stderr, "\"[!] Initialisation failed\",\n");
-	if(script) free(script);
-	if(keys) free(keys);
-	if(data) free(data);
 	fprintf(stderr,"\"ZENROOM JSON LOG END\" ]\n");
 	return EXIT_FAILURE;
   }
+  // heap and trace dumps in base64 encoded json
   zen_exec_script(Z, "CONF.debug.format='compact'");
-  zen_exec_zencode(Z, script);
+  // import DATA and KEYS from base64
+  zen_exec_script(Z, "CONF.input.format.fun = function(obj) return JSON.decode(OCTET.from_base64(obj):str()) end");
+
+  ret = strlen(script_b64); script_b64[ret-1] = 0x0; // remove newline
+  zen_exec_zencode(Z, script_b64);
 
   register int exitcode = Z->exitcode;
   zen_teardown(Z);
-  if(script) free(script);
-  if(keys) free(keys);
-  if(data) free(data);
   return exitcode;
 }
