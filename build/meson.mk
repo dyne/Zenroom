652c44dbac3818a8082b353f44248649dcdebe02
Jaromil <jaromil@dyne.org>
2023-08-23 23:51:27 +0200

    feat: upgrade to Lua 5.4 series
    embedded with very minimal modifications (just random in lua)
    
    some tests are not passing

--- build/meson.mk
+++ build/meson.mk
@@ -7,7 +7,7 @@ prepare-meson:
 	mkdir -p ${pwd}/meson
 	ln -sf ${pwd}/lib/milagro-crypto-c/build ${pwd}/meson/milagro-crypto-c
 	ln -sf ${pwd}/lib/pqclean/libqpz.a ${pwd}/meson/libqpz.a
-	ln -sf ${pwd}/lib/lua53/src/liblua.a ${pwd}/meson/liblua.a
+	ln -sf ${pwd}/lib/lua54/src/liblua.a ${pwd}/meson/liblua.a
 	ln -sf ${pwd}/lib/ed25519-donna/libed25519.a ${pwd}/meson/libed25519.a
 	ln -sf ${pwd}/lib/blake2/libblake2.a ${pwd}/meson/libblake2.a
 	ln -sf ${pwd}/lib/mimalloc/build/libmimalloc-static.a ${pwd}/meson/libmimalloc-static.a
