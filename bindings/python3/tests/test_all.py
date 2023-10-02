1e3737cb1c27cdff991c0fe95cd4fd2e5d2a2b67
matteo-cristino <cristino.matteo97@gmail.com>
2023-10-02 15:35:08 +0200

    fix(python): 🐛 update python tests
    random number generated with inline modulo are now integers instead of float

--- bindings/python3/tests/test_all.py
+++ bindings/python3/tests/test_all.py
@@ -7,17 +7,17 @@ def test_zencode_call_random_array():
     contract =  """Given nothing
 When I create the array of '64' random numbers modulo '100'
 and I create the aggregation of array 'array'
-Then print the 'array' as 'number'
-and print the 'aggregation' as 'number'
+Then print the 'array'
+and print the 'aggregation'
 """
     res = zencode_exec(
         contract
     )
-    out_regex = r'{\"aggregation\":\d{4},\"array\":\[(?:\d{1,3}\,)+\d{1,3}\]}'
+    out_regex = r'{\"aggregation\":\"\d{4}\",\"array\":\[(?:\"\d{1,3}\"\,)+\"\d{1,3}\"\]}'
     assert Regex(out_regex).validate(res.output)
     assert Schema({
-        'aggregation': int,
-        'array': [int]
+        'aggregation': str,
+        'array': [str]
     }).validate(res.result)
 
 def test_extra():
