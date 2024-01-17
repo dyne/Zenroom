5314bc38573b2200833f6f43a4a4d0a9bda958c9
Jaromil <jaromil@dyne.org>
2024-01-17 12:24:10 +0100

    improve transcend prototype for message size
    now RSK length is computed on message size + hash size
    response size must be equal to message and eventually padded
    and all AES key usage is guarded by a hash round to 32 bytes
    
    this grants full XOR coverage and high entropy on ciphertext
    also flexible message length

--- test/lua/transcend.lua
+++ test/lua/transcend.lua
@@ -17,52 +17,61 @@ print'French Servant Protocol (TRANSCEND 2024)'
 
 -- setup
 IV = OCTET.zero(32)
-SS = OCTET.from_hex('1e3f2ec68a14fe9c6812df91bb5f76e5b8c221afebf03a44975342e76e2a2ff9')
-alice = OCTET.from_string('My very secret message')
-bob = OCTET.from_string('My very secret response')
+SS = OCTET.zero(64)
+-- message = OCTET.from_string('My very secret message'):pad(32)
+-- response = OCTET.from_string('My very secret response'):pad(32)
+message = OCTET.random(128)
+response = OCTET.random(12)
 hash = HASH.new('sha256')
 
-nonce = os.time() -- concatenated to a semantic parameter
-RSK_len = 32 -- chose according to message length
--- Peel_len = 32 -- for MAC and ACK
+nonce = TIME.new(os.time()) -- TODO: concatenate to semantic parameter
 
--- random session key
-RSK = OCTET.random(RSK_len)
 
+-- random session key length -32 marks the maximum message length
+-- in other words: rsk is max length + 32 (hash len)
+local max = math.max(#message, #response)
+message = message:pad(max)
+response = response:pad(max)
+RSK = OCTET.random(max + 32)
+print("MSG length: ".. max)
+print("RSK length: ".. #RSK)
 -- from the paper:
 -- Message:
 -- {<Public key>,
 -- AES(RSK, SS) XOR AES (<Public key>,SS) : Key transfer
--- (AES ((Hash(RSK XOR SS)||<Alice1>) XOR RSK,RSK) : Alice / MAC
+-- (AES ((Hash(RSK XOR SS)||<Message1>) XOR RSK,RSK) : Message / MAC
 
 
 -- sender side
-message = {
+ciphertext = {
    n = nonce,
-   k = AES.ctr_encrypt(SS, RSK, IV) ~ AES.ctr_encrypt(SS, nonce, IV),
-   p = AES.ctr_encrypt(RSK, hash:process(RSK ~ SS) .. alice, IV) ~ RSK
+   k = AES.ctr_encrypt(hash:process(SS), RSK, IV) ~ AES.ctr_encrypt(hash:process(SS), nonce, IV),
+--   p = hash:process(RSK ~ SS) .. message ~ RSK
+   p = AES.ctr_encrypt(hash:process(RSK), hash:process(RSK ~ SS) .. message, IV) ~ RSK
+--   p = AES.ctr_encrypt(RSK, hash:process(RSK ~ SS) .. message, IV) ~ RSK
 }
-I.print({message = message})
 
+I.print({ciphertext = ciphertext})
+I.print({entropy = deepmap(OCTET.entropy, ciphertext)})
 -- receiver side
-local rsk = AES.ctr_decrypt(SS, message.k ~ AES.ctr_encrypt(SS, message.n, IV), IV)
+local rsk = AES.ctr_decrypt(hash:process(SS), ciphertext.k ~ AES.ctr_encrypt(hash:process(SS), ciphertext.n, IV), IV)
 assert(rsk == RSK)
 
-local recv_alice = AES.ctr_decrypt(RSK, message.p ~ rsk, IV)
+local recv_message = AES.ctr_decrypt(hash:process(RSK), ciphertext.p ~ rsk, IV)
 local mac = hash:process(rsk ~ SS)
-assert(recv_alice == hash:process(rsk ~ SS) .. alice)
-print(recv_alice:elide_at_start(mac):string())
+assert(recv_message == hash:process(rsk ~ SS) .. message)
+assert(message == recv_message:elide_at_start(mac))
 
--- AES(RSK XOR (Hash(<Public key> XOR RSK)||<bob>), SS) : Payload / ACK
-response = {
-   ACK = AES.ctr_encrypt(SS, (hash:process(nonce ~ rsk) .. bob) ~ rsk, IV)
-}
-I.print({response = response})
+-- AES(RSK XOR (Hash(<Public key> XOR RSK)||<response>), SS) : Payload / ACK
+ciphertext = AES.ctr_encrypt(hash:process(SS), (hash:process(nonce ~ rsk) .. response) ~ rsk, IV)
+
+I.print({response = ciphertext,
+		 entropy = ciphertext:entropy()})
 
 -- sender side
-local recv_bob = AES.ctr_decrypt(SS, response.ACK, IV) ~ RSK
+local recv_response = AES.ctr_decrypt(hash:process(SS), ciphertext, IV) ~ RSK
 local mac_response = hash:process(nonce ~ RSK)
-print(recv_bob:elide_at_start(mac_response):string())
+assert(response == recv_response:elide_at_start(mac_response))
 
 
 
