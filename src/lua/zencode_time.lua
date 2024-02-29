6dbeef9c31613bd290cea3c60a5f626abd79635b
Matteo Cristino <102997993+matteo-cristino@users.noreply.github.com>
2024-02-29 12:45:49 +0100

    feat(time): ✨ statement to get timestamp from os (#815)
    * feat(time): ✨ statement to get timestamp from os
    
    * test: 🧪 add time tests to meson tests
    
    * test(time): 🧪 remove assert_success left by mistake

---
+++ src/lua/zencode_time.lua
@@ -0,0 +1,27 @@
+--[[
+--This file is part of zenroom
+--
+--Copyright (C) 2021 Dyne.org foundation
+--designed, written and maintained by Denis Roio <jaromil@dyne.org>
+--
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU Affero General Public License v3.0
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU Affero General Public License for more details.
+--
+--Along with this program you should have received a copy of the
+--GNU Affero General Public License v3.0
+--If not, see http://www.gnu.org/licenses/agpl.txt
+--
+--Last modified by Matteo Cristino
+--on Thursday, 29th February 2024
+--]]
+
+When('create timestamp', function()
+    zencode_assert(os, 'Could not find os')
+    ACK.timestamp = U.new(os.time())
+    new_codec('timestamp', { zentype = 'e', encoding = 'time'})
+end)
