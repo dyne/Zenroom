78fc5babffcb6184370bcb93d830cae4e28b6340
Jaromil <jaromil@dyne.org>
2024-04-26 09:54:49 +0200

    revert ml-dsa api changes and use externs in our code
    this avoids use of headers and declares binary resolution hooks to
    functions in ml-dsa lib directly in our code

--- src/zen_qp.c
+++ src/zen_qp.c
@@ -83,11 +83,11 @@ extern int PQCLEAN_SNTRUP761_CLEAN_crypto_kem_dec(uint8_t *ss, const uint8_t *ct
 #define pqcrystals_ml_dsa_44_ipd_PUBLICKEYBYTES 1312
 #define pqcrystals_ml_dsa_44_ipd_SECRETKEYBYTES 2560
 #define pqcrystals_ml_dsa_44_ipd_BYTES 2420
-int pqcrystals_ml_dsa_44_ipd_ref_keypair(uint8_t *pk, uint8_t *sk);
-int pqcrystals_ml_dsa_44_ipd_ref_signature(uint8_t *sig, size_t *siglen, const uint8_t *m, size_t mlen, const uint8_t *sk);
-int pqcrystals_ml_dsa_44_ipd_ref(uint8_t *sm, size_t *smlen, const uint8_t *m, size_t mlen, const uint8_t *sk);
-int pqcrystals_ml_dsa_44_ipd_ref_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *pk);
-int pqcrystals_ml_dsa_44_ipd_ref_open(uint8_t *m, size_t *mlen, const uint8_t *sm, size_t smlen, const uint8_t *pk);
+extern int pqcrystals_ml_dsa_44_ipd_ref_keypair(uint8_t *pk, uint8_t *sk);
+extern int pqcrystals_ml_dsa_44_ipd_ref_signature(uint8_t *sig, size_t *siglen, const uint8_t *m, size_t mlen, const uint8_t *sk);
+extern int pqcrystals_ml_dsa_44_ipd_ref(uint8_t *sm, size_t *smlen, const uint8_t *m, size_t mlen, const uint8_t *sk);
+extern int pqcrystals_ml_dsa_44_ipd_ref_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *pk);
+extern int pqcrystals_ml_dsa_44_ipd_ref_open(uint8_t *m, size_t *mlen, const uint8_t *sm, size_t smlen, const uint8_t *pk);
 
 
 
