06a38a427cbc9716cf8b8ceb182be0d15b0fca9b
Jaromil <jaromil@dyne.org>
2024-09-20 22:30:02 +0200

    improvements in meson build
    now meson links libzenroom statically inside the executables, plus
    adds the zencc target

--- build/meson.mk
+++ build/meson.mk
@@ -1,54 +1,39 @@
+## Initialize build defaults
+include build/init.mk
 
-TARGETS := ${BUILDS}
+BUILD_DEPS += mimalloc
+BUILD_DEPS += tinycc
+## Specific compiler settings for all built dependencies
+cflags +=  ${cflags_debug} -fPIC
 
-# .PHONY: meson meson-re meson-test
-
-prepare-meson:
-	mkdir -p ${pwd}/meson
-	ln -sf ${pwd}/lib/milagro-crypto-c/build ${pwd}/meson/milagro-crypto-c
-	ln -sf ${pwd}/lib/pqclean/libqpz.a ${pwd}/meson/libqpz.a
-	ln -sf ${pwd}/lib/lua54/src/liblua.a ${pwd}/meson/liblua.a
-	ln -sf ${pwd}/lib/ed25519-donna/libed25519.a ${pwd}/meson/libed25519.a
-	ln -sf ${pwd}/lib/mimalloc/build/libmimalloc-static.a ${pwd}/meson/libmimalloc-static.a
-
-run-meson:
-	CC="${gcc}" AR="${ar}"  CFLAGS="${cflags}" LDFLAGS="${ldflags}" LDADD="${ldadd}" \
-	meson -Dexamples=true -Ddocs=true -Doptimization=3 \
-	-Decdh_curve=${ecdh_curve} -Decp_curve=${ecp_curve} \
-	-Ddefault_library=both build meson
-
-asan-meson:
-	CC="clang" CXX="clang++" AR="llvm-ar"  CFLAGS="-fsanitize=address -fno-omit-frame-pointer" LDFLAGS="-fsanitize=address ${ldflags}" LDADD="${ldadd}" \
-	meson -Dexamples=false -Ddocs=false -Doptimization=0 \
-	-Decdh_curve=${ecdh_curve} -Decp_curve=${ecp_curve} \
-	-Ddefault_library=both -Db_sanitize=address build meson
-
-meson: linux-meson
-meson-ccache: linux-meson
-meson-debug:  linux-meson
-meson-debug-ccache: linux-meson
-meson-clang-ccache: linux-meson
-meson-clang-debug-ccache: linux-meson
-
-linux-meson: ${TARGETS} prepare-meson run-meson
+# MAIN TARGETS
+all: ${BUILD_DEPS} prepare
+	CC="${gcc}" AR="${ar}" CFLAGS="${cflags}" LDFLAGS="${ldflags}"	\
+	LDADD="${ldadd}" meson -Dexamples=true -Ddocs=true				\
+	-Doptimization=3 -Decdh_curve=${ecdh_curve}						\
+	-Decp_curve=${ecp_curve} -Ddefault_library=both build meson
 	ninja -C meson
 
-linux-meson-release: ${TARGETS} prepare-meson run-meson
+asan:
+	CC="clang" CXX="clang++" AR="llvm-ar" CFLAGS="-fsanitize=address	\
+	-fno-omit-frame-pointer" LDFLAGS="-fsanitize=address ${ldflags}"	\
+	LDADD="${ldadd}" meson -Dexamples=false -Ddocs=false				\
+	-Doptimization=0 -Decdh_curve=${ecdh_curve}							\
+	-Decp_curve=${ecp_curve} -Ddefault_library=both						\
+	-Db_sanitize=address build meson
 	ninja -C meson
 
-linux-meson-clang-release: ${TARGETS} prepare-meson run-meson
-	ninja -C meson
+# subtargets
+prepare:
+	mkdir -p meson
+	ln -sf ../lib/milagro-crypto-c/build meson/milagro-crypto-c
+	ln -sf ../lib/pqclean/libqpz.a meson/libqpz.a
+	ln -sf ../lib/lua54/src/liblua.a meson/liblua.a
+	ln -sf ../lib/ed25519-donna/libed25519.a meson/libed25519.a
+	ln -sf ../lib/mimalloc/build/libmimalloc-static.a meson/libmimalloc-static.a
+	ln -sf ../lib/tinycc/libtcc.a meson/libtcc.a
 
-linux-meson-debug: ${TARGETS} prepare-meson run-meson
-	ninja -C meson
-
-linux-meson-clang-debug: ${TARGETS} prepare-meson run-meson
-	ninja -C meson
-
-linux-meson-asan: ${TARGETS} prepare-meson asan-meson
-	ninja -C meson
-
-prepare-meson-test:
+prepare-test:
 	echo '#!/bin/sh' > ${pwd}/test/zenroom
 	echo "${pwd}/meson/zenroom "'$$*' >> ${pwd}/test/zenroom
 	chmod +x ${pwd}/test/zenroom
@@ -56,11 +41,13 @@ prepare-meson-test:
 	echo "${pwd}/meson/zencode-exec "'$$*' >> ${pwd}/test/zencode-exec
 	chmod +x ${pwd}/test/zencode-exec
 
-meson-test: prepare-meson-test
+test: prepare-test
 	ninja -C meson test
 
-meson-benchmark: prepare-meson-test
+benchmark: prepare-test
 	ninja -C meson benchmark
 
-meson-analyze:
+analyze:
 	SCANBUILD=$(pwd)/build/scanbuild.sh ninja -C meson scan-build
+
+include build/deps.mk
