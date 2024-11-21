1476f3928874c7ba5030a69bffb0f924c85dc7fa
matteo-cristino <cristino.matteo97@gmail.com>
2024-11-21 18:02:40 +0100

    docs: update script to generate list of zenroom statements
    fix zenroom executable position, extract also foreach statements and capture all the if statements

--- docs/_media/run_utterances.sh
+++ docs/_media/run_utterances.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # this file create the zencode_utterances_reworked.yaml file
 # that in turn is used by zencode-list.md to document all the
 # zenroom statements
@@ -5,23 +6,32 @@
 rm -f zencode_utterances_reworked.yaml
 
 for i in `ls ../../src/lua/zencode_*`; do
-    SCENARIO=`echo $i | cut -d _ -f 2 | cut -d . -f 1`
-    ../../src/zenroom -D $SCENARIO  2>/dev/null | jq .  > temp.json;
+    SCENARIO=`echo $i | cut -d _ -f2- | cut -d . -f1`
+    ../../zenroom -D $SCENARIO  2>/dev/null | jq .  > temp.json;
     echo "### [${SCENARIO}]" >> zencode_utterances_reworked.yaml
-    if [ $SCENARIO == "debug" ]; then
-	cat temp.json \
-	    | jq '."Given" | keys[] ' \
-	    | sed -e 's/\\\"/\"/g' -e 's/^.//g' -e 's/.$//g'  \
-	    | sort \
-		  >> zencode_utterances_reworked.yaml
+    if [ "$SCENARIO" == "debug" ]; then
+        cat temp.json \
+            | jq '."Given" | keys[] ' \
+            | sed -e 's/\\\"/\"/g' -e 's/^.//g' -e 's/.$//g'  \
+            | sort \
+            >> zencode_utterances_reworked.yaml
     else
-	cat temp.json \
-	    | jq '.["Given", "If", "When", "Then"] | keys[] ' \
-	    | sed -e 's/\\\"/\"/g' -e 's/^.//g' -e 's/.$//g'  \
-	    | sort \
-		  >> zencode_utterances_reworked.yaml
+        cat temp.json \
+            | jq '.If | keys[] ' \
+            | sed -e 's/\\\"/\"/g' -e 's/^.//g' -e 's/.$//g'  \
+            >> temp_if.txt
+        cat temp.json \
+            | jq '.["Given", "If", "Foreach", "When", "Then"] | keys[] ' \
+            | sed -e 's/\\\"/\"/g' -e 's/^.//g' -e 's/.$//g'  \
+            | sort \
+            >> zencode_utterances_reworked.yaml
     fi
     echo "### [${SCENARIO}]" >> zencode_utterances_reworked.yaml
 done
 
+echo "### [if_subset]" >> zencode_utterances_reworked.yaml
+cat temp_if.txt | sort  >> zencode_utterances_reworked.yaml
+echo "### [if_subset]" >> zencode_utterances_reworked.yaml
+
 rm temp.json
+rm temp_if.txt
