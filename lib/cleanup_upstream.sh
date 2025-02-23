33df2ee65e5484973929a14e9d8c51eed11550be
Jaromil <jaromil@dyne.org>
2025-02-23 21:38:03 +0100

    fix: add reuse entry for lib/mlkem
    clean docs

--- lib/cleanup_upstream.sh
+++ lib/cleanup_upstream.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright 2025 Dyne.org foundation
+# SPDX-FileCopyrightText: 2025 Dyne.org foundation
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 
 # script that cleans up libs imported from upstream
 # this helps remember what needs to be deleted on import
@@ -17,5 +21,6 @@ rm -rf "$i/mlkem/fips202/native"
 rm -rf "$i/scripts"
 rm -rf "$i/integration"
 rm -rf "$i/examples"
-
-
+rm -rf "$i/META.sh"
+rm -rf "$i/test/notrandombytes"
+find "$i" -type f -name "*.md" -exec rm -f {} +
--- lib/mlkem/SECURITY.md
+++
@@ -1,8 +0,0 @@
-[//]: # (SPDX-License-Identifier: CC-BY-4.0)
-
-# Security Policy
-
-## Reporting security bugs
-
-If you think you have found a security bug in mlkem-native, please report the vulnerability through
-Github's [private vulnerability reporting](https://github.com/pq-code-package/mlkem-native/security).
