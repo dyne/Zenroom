3c529c3caf16acbb8af7f5fd02402856db4e614e
Jaromil <jaromil@dyne.org>
2025-02-25 15:56:23 +0100

    fix: convert fastalloc32.c license to AGPLv3 in zenroom

--- src/fastalloc32.c
+++ src/fastalloc32.c
@@ -1,5 +1,5 @@
 /* SPDX-FileCopyrightText: 2025 Dyne.org foundation
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  *
  * Fast memory allocator for 32 bit 'puters
  *
