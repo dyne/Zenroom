329916d471eac51f6251ec92f37523a8130f31ae
matteo-cristino <cristino.matteo97@gmail.com>
2025-03-06 14:47:05 +0100

    fix(random): do not save lua numbers in ACK

--- src/lua/zencode_random.lua
+++ src/lua/zencode_random.lua
@@ -181,7 +181,7 @@ When(
 )
 
 When("create random array with '' numbers", function(s)
-    _create_random_array('random_array', s, 65536, _random_modulo_uniform_distribution, {encoding = 'float'})
+    _create_random_array('random_array', s, 65536, function(m) return F.new(_random_modulo_uniform_distribution(m)) end, {encoding = 'float'})
 end)
 When("create random array with '' integers", function(s)
     _create_random_array('random_array', s, nil, BIG.random, {encoding = 'integer'})
