ad491ef44108b31f47615c80b79af32df1234a72
Giulio Sacchet <giulio@dyne.org>
2025-04-10 15:21:37 +0200

    fix: add a condition in from_uuid function for the prefix string 'urn:uuid:. Update of lua tests

--- test/lua/uuid.lua
+++ test/lua/uuid.lua
@@ -1,12 +1,22 @@
 
 uuids = {"550e8400-e29b-41d4-a716-446655440000",
          "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-         "cdaed56d-8712-414d-b346-01905d0026fe"}
+         "cdaed56d-8712-414d-b346-01905d0026fe",
+         "urn:uuid:550e8400-e29b-41d4-a716-446655440000",
+         "urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
+         "urn:uuid:cdaed56d-8712-414d-b346-01905d0026fe"}
 
-for _,v in ipairs(uuids) do
-    oct = O.from_uuid(v)
-    assert(oct:uuid()==v)
-end
+oct1 = O.from_uuid(uuids[1])
+oct2 = O.from_uuid(uuids[2])
+oct3 = O.from_uuid(uuids[3])
+oct4 = O.from_uuid(uuids[4])
+oct5 = O.from_uuid(uuids[5])
+oct6 = O.from_uuid(uuids[6])
+
+assert(oct1:uuid()==oct4:uuid() and oct1:uuid()==uuids[1] and oct4:uuid()==uuids[1])
+assert(oct2:uuid()==oct5:uuid() and oct2:uuid()==uuids[2] and oct5:uuid()==uuids[2])
+assert(oct3:uuid()==oct6:uuid() and oct3:uuid()==uuids[3] and oct6:uuid()==uuids[3])
+
 print("✅ All valid input tests passed!")
 
 success, err = pcall(OCTET.to_uuid, nil)
