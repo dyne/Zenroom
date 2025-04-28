368aa6e53280cdcb9464227c2e3bba882186d6d5
matteo-cristino <cristino.matteo97@gmail.com>
2025-04-29 00:53:41 +0200

    fix(zenroom): add context input to zencode_exec api
    add also some basic test for zencode_exec api

--- src/zenroom.c
+++ src/zenroom.c
@@ -500,15 +500,17 @@ int _check_zenroom_result(zenroom_t *zz) {
   return(exitcode);
 }
 
-int zencode_exec(const char *script, const char *conf, const char *keys, const char *data, const char *extra) {
+int zencode_exec(const char *script, const char *conf, const char *keys, const char *data,
+	const char *extra, const char *context) {
 
-	const char *c, *k, *d, *e;
+	const char *c, *k, *d, *e, *x;
 	c = conf ? (conf[0] == '\0') ? NULL : conf : NULL;
 	k = keys ? (keys[0] == '\0') ? NULL : keys : NULL;
 	d = data ? (data[0] == '\0') ? NULL : data : NULL;
 	e = extra ? (extra[0] == '\0') ? NULL : extra : NULL;
+	x = context ? (context[0] == '\0') ? NULL : context : NULL;
 
-	zenroom_t *Z = zen_init_extra(c, k, d, e, NULL);
+	zenroom_t *Z = zen_init_extra(c, k, d, e, x);
 	if (_check_zenroom_init(Z) != SUCCESS) return ERR_INIT;
 	if (_check_script_arg(Z, script) != SUCCESS) return ERR_INIT;
 
---
+++ test/api/zencode.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <zenroom.h>
+
+int main(int argc, char **argv) {
+  int res;
+  res = zencode_exec(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
+  if(res!=0){fprintf(stderr,"Abort on error code %u\n",res);exit(res);}
+  exit(0);
+}
