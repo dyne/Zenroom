dffaf1842beddfd5327eed8095dc095d1d4e7ad5
matteo-cristino <cristino.matteo97@gmail.com>
2025-05-02 13:23:20 +0200

    feat: add extra and context input to zencode_exec_tobuf and zenroom_exec_tobuf
    add also api tests

--- test/api/zencode.c
+++ test/api/zencode.c
@@ -3,8 +3,8 @@
 #include <zenroom.h>
 
 int main(int argc, char **argv) {
-  int res;
-  res = zencode_exec(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
-  if(res!=0){fprintf(stderr,"Abort on error code %u\n",res);exit(res);}
-  exit(0);
+	int res;
+	res = zencode_exec(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
+	if(res!=0){fprintf(stderr,"Abort on error code %u\n",res);exit(res);}
+	exit(0);
 }
