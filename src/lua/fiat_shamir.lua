93fdd784ef8654ae6e3f7e98145301657d6d8a85
Giulio Sacchet <giulio@dyne.org>
2025-05-07 18:07:02 +0200

    feat: new generate_nat() function implementation

--- src/lua/fiat_shamir.lua
+++ src/lua/fiat_shamir.lua
@@ -14,4 +14,31 @@ local function fiat_shamir(transcript,n_bytes,start_index)
 end
     
 
+function generate_nat(m, transcript)
+--generates a random natural between 0 and m-1 inclusive
+    assert(type(m) == "zenroom.big", "m is not a BIG")
+    assert(type(transcript) == "zenroom.octet", "transcript is not an octet")
+    local l = big.new(0) 
+    while big.new(2):modpower(l,ECP.order()):__lt(m) do
+        l = big.zenadd(l,big.new(1))
+    end
+    local n_bytes = l:__div(big.new(8)):__add(big.new(1)):int()
+    local mod = big.new(2):modpower(l,ECP.order())
+    local r = m
+    local start_index = 0 
+    while m:__lte(r) do 
+        local b = fiat_shamir(transcript, n_bytes, start_index)
+        local k = big.new(b:reverse())
+        r = k:__mod(mod)
+        start_index = start_index+n_bytes
+    end 
+    return r
+end 
+
+transcript = O.from_string("tesefwfwddddfewf")
+m = big.from_decimal("13675127219174")
+result = generate_nat(m,transcript)
+print(result:decimal())
+print(m:decimal())
+assert(result:__lte(m), "generate_nat() generated a number over the indicated limit")
 
