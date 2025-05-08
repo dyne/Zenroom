83dd65321c416398fcf756299600db0fdf3879b5
matteo-cristino <cristino.matteo97@gmail.com>
2025-05-08 09:47:58 +0200

    test(merkle_tree): update vector tests to use crypto_merkle.lua

--- test/vectors/check_merkle_tree.lua
+++ test/vectors/check_merkle_tree.lua
@@ -1,16 +1,11 @@
+
+local MT = require'crypto_merkle'
+
 local function _hash(data, hashtype)
     hashtype = hashtype or CONF.hash
-    --default hashtype: sha256
-    --possible hashtype:
-    --sha512
-    --sha3_256
-    --sha3_512
-    --shake256
-    --keccak256
     local _hf <const> = HASH:init(hashtype)
     return _hf:process(data)
 end
-
 
 print("TEST VECTORS from Frigo's RFC")
 
@@ -25,11 +20,8 @@ local root = O.from_hex("f22f4501ffd3bdffcecc9e4cd6828a4479aeedd6aa484eb7c1f808c
 local leaves = {leaves1, leaves2, leaves3, leaves4, leaves5}
 
 -- Used a special version for the creation of the merkle tree that supposes test vectors already hashed
-local tree = _create_merkle_tree_for_tests(leaves, "sha256")
-
-assert(tree[2] == root)
-
-
+local tree = MT.create_merkle_tree_from_table_of_hashes(leaves, "sha256")
+assert(tree[2] == root, "The root of the tree does not match the expected value")
 
 print("TEST from Frigo's code")
 
@@ -40,15 +32,15 @@ local leaves4 = "103"
 
 local leaves = {leaves1, leaves2, leaves3, leaves4}
 
-local tree = _create_merkle_tree(leaves, "sha256")
-local root = _create_merkle_root(leaves,"sha256")
+local tree = MT.create_merkle_tree(leaves, "sha256")
+local root = MT.create_merkle_root(leaves,"sha256")
 
 for i = 1, 4 do
-    assert(tree[4 + i] == _hash(leaves[i], "sha256"))
+    assert(tree[4 + i] == _hash(leaves[i], "sha256"), "Leaf "..i.." does not match the expected value")
 end
 
-assert(tree[4] == _hash(_hash(leaves[3],"sha256") .. _hash(leaves[4], "sha256"), "sha256"))
-assert(tree[3] == _hash(_hash(leaves[1],"sha256") .. _hash(leaves[2], "sha256"), "sha256"))
+assert(tree[4] == _hash(_hash(leaves[3],"sha256") .. _hash(leaves[4], "sha256"), "sha256"), "Leaf 4 does not match the expected value")
+assert(tree[3] == _hash(_hash(leaves[1],"sha256") .. _hash(leaves[2], "sha256"), "sha256"), "Leaf 3 does not match the expected value")
 
 local leaves5 = _hash(_hash(leaves[1],"sha256") .. _hash(leaves[2], "sha256"), "sha256")
 local leaves6 = _hash(_hash(leaves[3],"sha256") .. _hash(leaves[4], "sha256"), "sha256")
@@ -56,8 +48,8 @@ local leaves6 = _hash(_hash(leaves[3],"sha256") .. _hash(leaves[4], "sha256"), "
 --tree[2] is the root
 --used both since zencode_merkle.lua has a function that generates directly the root
 
-assert(tree[2] == _hash(leaves5 .. leaves6, "sha256"))
-assert(root == _hash(leaves5 .. leaves6, "sha256"))
+assert(tree[2] == _hash(leaves5 .. leaves6, "sha256"), "Leaf 2 does not match the expected value")
+assert(root == _hash(leaves5 .. leaves6, "sha256"), "Root does not match the expected value")
 
 
 
