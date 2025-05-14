fe63eb95929870c3c7b9deed09b3efaee2b969e5
matteo-cristino <cristino.matteo97@gmail.com>
2025-05-14 16:41:20 +0200

    fix(api_hash): free allocated memory in case of error
    also fix the fact that buffer should be a hex

--- src/api_hash.c
+++ src/api_hash.c
@@ -38,39 +38,41 @@
 #define ZEN_SHA512 '4'
 #define ZEN_SHA256 '2'
 
-void print_ctx_hex(char prefix, void *sh, int len) {
+int print_ctx_hex(char prefix, void *sh, int len) {
   char *hash_ctx = malloc((len<<1)+2);
+  if(!hash_ctx) {_err( "%s :: cannot allocate hash_ctx",__func__);return FAIL();}
   hash_ctx[0] = prefix;
   buf2hex(hash_ctx+1, (const char*)sh, (const size_t)len);
   hash_ctx[(len<<1)+1] = 0x0; // null terminated string
   _out("%s",hash_ctx);
   free(hash_ctx);
+  return OK();
 }
 
 // returns a fills hash_ctx, which must be pre-allocated externally
 int zenroom_hash_init(const char *hash_type) {
   register char prefix = '0';
   // size tests
-  register int len = 0; 
-  void *sh;
+  register int len = 0;
+  void *sh = NULL;
   if(strcasecmp(hash_type, "sha512") == 0) {    
     prefix = ZEN_SHA512;
     len = sizeof(hash512); // amcl struct
     sh = calloc(len, 1);
-    // TODO: check what malloc returns
+	if(!sh) {_err( "%s :: cannot allocate sh",__func__);return FAIL();}
     HASH512_init((hash512*)sh); // amcl init
   } else if(strcasecmp(hash_type, "sha256") == 0) {    
     prefix = ZEN_SHA256;
-    len = sizeof(hash256); // amcl struct
-    sh = calloc(len, 1);
-    // TODO: check what malloc returns
+    len = sizeof(hash256);
+	sh = calloc(len, 1);
+	if(!sh) {_err( "%s :: cannot allocate hash",__func__);return FAIL();}
     HASH256_init((hash256*)sh); // amcl init
   }
   else {
 	_err("%s :: invalid hash type: %s", __func__, hash_type);
 	return FAIL();
   }
-  print_ctx_hex(prefix, sh, len);
+  if(print_ctx_hex(prefix, sh, len) == 1) {free(sh);return FAIL();};
   free(sh);
   return OK();
 }
@@ -79,29 +81,34 @@ int zenroom_hash_init(const char *hash_type) {
 int zenroom_hash_update(const char *hash_ctx,
 			const char *buffer, const int buffer_size) {
   register char prefix = hash_ctx[0];
-  register int len;
-  char *sh;
+  register int len, c;
+  char *sh = NULL;
+  char *hex_buf = calloc(buffer_size<<1, 1);
+  if(!hex_buf) {_err( "%s :: cannot allocate hex_buf",__func__);return FAIL();}
+  if(hex2buf(hex_buf, buffer)<0) {free(hex_buf);_err("%s :: cannot do hex2buf %s",__func__, buffer);return FAIL();}
   if(prefix==ZEN_SHA512) {
     len = sizeof(hash512);
-    sh = (char*)calloc(len, 1);
-    hex2buf(sh, hash_ctx+1);
-    register int c;
-    for(c=0; c<buffer_size; c++) {
-      HASH512_process((hash512*)sh, buffer[c]);
+	sh = calloc(len, 1);
+	if(!sh) {free(hex_buf);_err( "%s :: cannot allocate hash",__func__);return FAIL();}
+	if(hex2buf(sh, hash_ctx+1)<0) {free(sh);free(hex_buf);_err("%s :: cannot do hex2buf %s",__func__, hash_ctx+1);return FAIL();}
+    for(c=0; c<buffer_size<<1; c++) {
+      HASH512_process((hash512*)sh, hex_buf[c]);
     }
   } else if(prefix==ZEN_SHA256) {
     len = sizeof(hash256);
-    sh = (char*)calloc(len, 1);
-    hex2buf(sh, hash_ctx+1);
-    register int c;
-    for(c=0; c<buffer_size; c++) {
-      HASH256_process((hash256*)sh, buffer[c]);
+	sh = calloc(len, 1);
+	if(!sh) {free(hex_buf);_err( "%s :: cannot allocate hash",__func__);return FAIL();}
+	if(hex2buf(sh, hash_ctx+1)<0) {free(sh);free(hex_buf);_err("%s :: cannot do hex2buf %s",__func__, hash_ctx+1);return FAIL();}
+    for(c=0; c<buffer_size<<1; c++) {
+      HASH256_process((hash256*)sh, hex_buf[c]);
     }
   } else {
+	free(hex_buf);
     _err("%s :: invalid hash context prefix: %c", __func__, prefix);
 	return FAIL();
   }
-  print_ctx_hex(prefix, sh, len);
+  if(print_ctx_hex(prefix, sh, len) == 1) {free(sh);return FAIL();};
+  free(hex_buf);
   free(sh);
   return OK();
 }
@@ -110,30 +117,30 @@ int zenroom_hash_update(const char *hash_ctx,
 int zenroom_hash_final(const char *hash_ctx) {
   register char prefix = hash_ctx[0];
   register int len;
-  char *hash_result;
+  char *hash_result = malloc(90);
+  if(!hash_result) {_err( "%s :: cannot allocate hash_result",__func__);return FAIL();}
   octet tmp;
   char *sh;
   if(prefix==ZEN_SHA512) {
-    hash_result = malloc(90); // base64 is 88 with padding
     tmp.len = 64;
     tmp.val = (char*)malloc(64);
-    // TODO: check malloc result
+	if(!tmp.val) {free(hash_result);_err( "%s :: cannot allocate tmp.val",__func__);return FAIL();}
     len = sizeof(hash512);
     sh = (char*)calloc(len, 1);
-    // TODO: check malloc result
-    hex2buf(sh, hash_ctx+1);
+	if(!sh) {free(tmp.val);free(hash_result);_err( "%s :: cannot allocate sh",__func__);return FAIL();}
+    if(hex2buf(sh, hash_ctx+1)<0) {free(sh);free(tmp.val);free(hash_result);_err("%s :: cannot do hex2buf %s",__func__, hash_ctx+1);return FAIL();};
     HASH512_hash((hash512*)sh, tmp.val);
   } else if(prefix==ZEN_SHA256) {
-    hash_result = malloc(90); // base64 is 88 with padding
     tmp.len = 32;
     tmp.val = (char*)malloc(32);
-    // TODO: check malloc result
+	if(!tmp.val) {free(hash_result);_err( "%s :: cannot allocate tmp.val",__func__);return FAIL();}
     len = sizeof(hash256);
     sh = (char*)calloc(len, 1);
-    // TODO: check malloc result
-    hex2buf(sh, hash_ctx+1);
+    if(!sh) {free(tmp.val);free(hash_result);_err( "%s :: cannot allocate sh",__func__);return FAIL();}
+    if(hex2buf(sh, hash_ctx+1)<0) {free(sh);free(tmp.val);free(hash_result);_err("%s :: cannot do hex2buf %s",__func__, hash_ctx+1);return FAIL();};
     HASH256_hash((hash256*)sh, tmp.val);
   } else {
+	free(hash_result);
     _err("%s :: invalid hash context prefix: %c", __func__, prefix);
 	return FAIL();
   }
