6d8b27862ade9f1e3c64de7a133a3e2f2a866f56
Jaromil <jaromil@dyne.org>
2025-05-30 12:27:17 +0200

    longfellow crypto util provided by zenroom
    crypto primitives substituting openssl

---
+++ lib/longfellow-zk/util/randombytes.cc
@@ -0,0 +1,282 @@
+// The MIT License
+
+// Copyright (c) 2017 Daan Sprenkels <hello@dsprenkels.com>
+// Copyright (c) 2018-2025 Dyne.org foundation
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#if defined(WITHOUT_OPENSSL)
+
+#include "randombytes.h"
+
+#if defined(ARCH_WIN)
+/* Windows */
+# include <windows.h>
+# include <wincrypt.h> /* CryptAcquireContext, CryptGenRandom */
+#endif /* defined(_WIN32) */
+
+
+#if defined(__linux__)
+/* Linux */
+# if !defined(_GNU_SOURCE)
+#  define _GNU_SOURCE
+# endif
+# if defined(__MUSL__)
+#  undef SYS_getrandom
+# endif
+# include <assert.h>
+# include <errno.h>
+# include <fcntl.h>
+# if defined(SYS_getrandom)
+#  include <linux/random.h>
+# endif
+# include <poll.h>
+# include <stdint.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <sys/syscall.h>
+# include <sys/types.h>
+
+// We need SSIZE_MAX as the maximum read len from /dev/urandom
+# if !defined(SSIZE_MAX)
+#  define SSIZE_MAX (SIZE_MAX / 2 - 1)
+# endif /* defined(SSIZE_MAX) */
+
+#endif /* defined(__linux__) */
+
+
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+/* Dragonfly, FreeBSD, NetBSD, OpenBSD (has arc4random) */
+# include <sys/param.h>
+# if defined(BSD)
+#  include <stdlib.h>
+# endif
+#endif
+
+#ifdef __EMSCRIPTEN__
+#include <stdlib.h>
+#include <emscripten.h>
+int randombytes_js_randombytes_nodejs(void *buf, size_t n) {
+	size_t c;
+	char *bytes = (char*) EM_ASM_INT({
+		var nodeRandomBytes = function() { return require("crypto").randomBytes; };
+		var browserRandomBytes = function(n) {
+			var crypto = (self.crypto || self.msCrypto);
+			var QUOTA = 65536;
+			return function(n) {
+				var arr = new Uint8Array(n);
+				for (var i = 0; i < n; i += QUOTA) {
+					crypto.getRandomValues(
+							arr.subarray(i, i + Math.min(n - i, QUOTA)));
+				}
+				return arr;
+			}
+		};
+		var getRandomBytes = ((typeof self !== 'undefined' && 
+							   (self.crypto || self.msCrypto)) 
+							 ? browserRandomBytes 
+							 : nodeRandomBytes)();
+		var out = _malloc($0);
+		writeArrayToMemory(getRandomBytes($0), out);
+		return out;
+		}, n);
+	for(c=0;c<n;c++)
+		((char*)buf)[c] = bytes[c];
+	free(bytes);
+	return 0;
+}
+#endif
+
+#if defined(ARCH_WIN)
+static int randombytes_win32_randombytes(void* buf, const size_t n)
+{
+	HCRYPTPROV ctx;
+	BOOL tmp;
+
+	tmp = CryptAcquireContext(&ctx, NULL, NULL, PROV_RSA_FULL,
+	                          CRYPT_VERIFYCONTEXT);
+	if (tmp == FALSE) return -1;
+
+	tmp = CryptGenRandom(ctx, n, (BYTE*) buf);
+	if (tmp == FALSE) return -1;
+
+	tmp = CryptReleaseContext(ctx, 0);
+	if (tmp == FALSE) return -1;
+
+	return 0;
+}
+#endif /* defined(_WIN32) */
+
+#ifdef ARCH_CORTEX
+#  undef SYS_getrandom
+static int randombytes_cortexm(void *buf, size_t n)
+{
+    /* Certified to be random by fair dice roll */
+    memset(buf, 4, n);
+    return n;
+}
+
+#endif
+
+
+#if defined(__linux__) && defined(SYS_getrandom)
+static int randombytes_linux_randombytes_getrandom(void *buf, size_t n)
+{
+	/* I have thought about using a separate PRF, seeded by getrandom, but
+	 * it turns out that the performance of getrandom is good enough
+	 * (250 MB/s on my laptop).
+	 */
+	size_t offset = 0, chunk;
+	int ret;
+	while (n > 0) {
+		/* getrandom does not allow chunks larger than 33554431 */
+		chunk = n <= 33554431 ? n : 33554431;
+		do {
+			ret = syscall(SYS_getrandom, (char *)buf + offset, chunk, 0);
+		} while (ret == -1 && errno == EINTR);
+		if (ret < 0) return ret;
+		offset += ret;
+		n -= ret;
+	}
+	assert(n == 0);
+	return 0;
+}
+#endif /* defined(__linux__) && defined(SYS_getrandom) */
+
+#if defined(__linux__)
+# if defined(SYS_getrandom)
+static int randombytes_linux_get_entropy_avail(int fd)
+{
+	int ret;
+	ioctl(fd, RNDGETENTCNT, &ret);
+	return ret;
+}
+
+static int randombytes_linux_wait_for_entropy(int device)
+{
+	/* We will block on /dev/random, because any increase in the OS' entropy
+	 * level will unblock the request. I use poll here (as does libsodium),
+	 * because we don't *actually* want to read from the device. */
+	const int bits = 128;
+	struct pollfd pfd;
+	int fd, retcode; /* Used as file descriptor *and* poll() return code */
+
+	/* If the device has enough entropy already, we will want to return early */
+	if (randombytes_linux_get_entropy_avail(device) >= bits) {
+		return 0;
+	}
+
+	do {
+		fd = open("/dev/random", O_RDONLY);
+	} while (fd == -1 && errno == EINTR); /* EAGAIN will not occur */
+	if (fd == -1) {
+		/* Unrecoverable IO error */
+		return -1;
+	}
+
+	pfd.fd = fd;
+	pfd.events = POLLIN;
+	do {
+		retcode = poll(&pfd, 1, -1);
+	} while ((retcode == -1 && (errno == EINTR || errno == EAGAIN)) ||
+	         randombytes_linux_get_entropy_avail(device) < bits);
+	if (retcode != 1) {
+		do {
+			retcode = close(fd);
+		} while (retcode == -1 && errno == EINTR);
+		return -1;
+	}
+	retcode = close(fd);
+	return retcode;
+}
+
+# endif //defined(SYS_getrandom)
+
+static int randombytes_linux_randombytes_urandom(void *buf, size_t n)
+{
+	int fd;
+	size_t offset = 0, count;
+	ssize_t tmp;
+	do {
+		fd = open("/dev/urandom", O_RDONLY);
+	} while (fd == -1 && errno == EINTR);
+
+# if defined(SYS_getrandom)
+	if (randombytes_linux_wait_for_entropy(fd) == -1) return -1;
+# endif
+
+	while (n > 0) {
+		count = n <= SSIZE_MAX ? n : SSIZE_MAX;
+		tmp = read(fd, (char *)buf + offset, count);
+		if (tmp == -1 && (errno == EAGAIN || errno == EINTR)) {
+			continue;
+		}
+		if (tmp == -1) return -1; /* Unrecoverable IO error */
+		offset += tmp;
+		n -= tmp;
+	}
+	close(fd);
+	assert(n == 0);
+	return 0;
+}
+#endif /* defined(__linux__) */
+
+
+#if defined(BSD)
+static int randombytes_bsd_randombytes(void *buf, size_t n)
+{
+	arc4random_buf(buf, n);
+	return 0;
+}
+#endif /* defined(BSD) */
+#include <string.h>
+int randombytes(void *buf, size_t n)
+{
+	if(!n) return 0;
+#if defined(__EMSCRIPTEN__)
+# pragma message("Using crypto api from NodeJS")
+	return randombytes_js_randombytes_nodejs(buf, n);
+#elif defined(__linux__)
+# if defined(SYS_getrandom)
+#  pragma message("Using getrandom system call")
+	/* Use getrandom system call */
+	(void) randombytes_linux_randombytes_urandom; // no warnings
+	return randombytes_linux_randombytes_getrandom(buf, n);
+# else
+#  pragma message("Using /dev/urandom device")
+	/* When we have enough entropy, we can read from /dev/urandom */
+	return randombytes_linux_randombytes_urandom(buf, n);
+# endif
+#elif defined(BSD)
+# pragma message("Using arc4random system call")
+	/* Use arc4random system call */
+	return randombytes_bsd_randombytes(buf, n);
+#elif defined(ARCH_WIN)
+# pragma message("Using Windows cryptographic API")
+	/* Use windows API */
+	return randombytes_win32_randombytes(buf, n);
+#elif defined(ARCH_CORTEX)
+# pragma message("Using Cortex-M support for random API")
+    return randombytes_cortexm(buf, n);
+#else
+# error "randombytes(...) is not supported on this platform"
+#endif
+}
+#endif // WITHOUT_OPENSSL
---
+++ lib/longfellow-zk/util/sha256.cc
@@ -0,0 +1,354 @@
+/* Based on the public domain implementation in
+ * crypto_hash/sha512/ref/ from http://bench.cr.yp.to/supercop.html
+ * by D. J. Bernstein */
+
+#if defined(WITHOUT_OPENSSL)
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdlib.h>
+
+#include "sha256.h"
+#define SHA256CTX_BYTES 40
+
+static uint32_t load_bigendian_32(const uint8_t *x) {
+    return (uint32_t)(x[3]) | (((uint32_t)(x[2])) << 8) |
+           (((uint32_t)(x[1])) << 16) | (((uint32_t)(x[0])) << 24);
+}
+
+static void store_bigendian_32(uint8_t *x, uint64_t u) {
+    x[3] = (uint8_t) u;
+    u >>= 8;
+    x[2] = (uint8_t) u;
+    u >>= 8;
+    x[1] = (uint8_t) u;
+    u >>= 8;
+    x[0] = (uint8_t) u;
+}
+
+static uint64_t load_bigendian_64(const uint8_t *x) {
+    return (uint64_t)(x[7]) | (((uint64_t)(x[6])) << 8) |
+           (((uint64_t)(x[5])) << 16) | (((uint64_t)(x[4])) << 24) |
+           (((uint64_t)(x[3])) << 32) | (((uint64_t)(x[2])) << 40) |
+           (((uint64_t)(x[1])) << 48) | (((uint64_t)(x[0])) << 56);
+}
+
+static void store_bigendian_64(uint8_t *x, uint64_t u) {
+    x[7] = (uint8_t) u;
+    u >>= 8;
+    x[6] = (uint8_t) u;
+    u >>= 8;
+    x[5] = (uint8_t) u;
+    u >>= 8;
+    x[4] = (uint8_t) u;
+    u >>= 8;
+    x[3] = (uint8_t) u;
+    u >>= 8;
+    x[2] = (uint8_t) u;
+    u >>= 8;
+    x[1] = (uint8_t) u;
+    u >>= 8;
+    x[0] = (uint8_t) u;
+}
+
+#define SHR(x, c) ((x) >> (c))
+#define ROTR_32(x, c) (((x) >> (c)) | ((x) << (32 - (c))))
+
+#define Ch(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
+#define Maj(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+
+#define Sigma0_32(x) (ROTR_32(x, 2) ^ ROTR_32(x,13) ^ ROTR_32(x,22))
+#define Sigma1_32(x) (ROTR_32(x, 6) ^ ROTR_32(x,11) ^ ROTR_32(x,25))
+#define sigma0_32(x) (ROTR_32(x, 7) ^ ROTR_32(x,18) ^ SHR(x, 3))
+#define sigma1_32(x) (ROTR_32(x,17) ^ ROTR_32(x,19) ^ SHR(x,10))
+
+#define M_32(w0, w14, w9, w1) w0 = sigma1_32(w14) + (w9) + sigma0_32(w1) + (w0);
+
+#define EXPAND_32           \
+    M_32(w0, w14, w9, w1)   \
+    M_32(w1, w15, w10, w2)  \
+    M_32(w2, w0, w11, w3)   \
+    M_32(w3, w1, w12, w4)   \
+    M_32(w4, w2, w13, w5)   \
+    M_32(w5, w3, w14, w6)   \
+    M_32(w6, w4, w15, w7)   \
+    M_32(w7, w5, w0, w8)    \
+    M_32(w8, w6, w1, w9)    \
+    M_32(w9, w7, w2, w10)   \
+    M_32(w10, w8, w3, w11)  \
+    M_32(w11, w9, w4, w12)  \
+    M_32(w12, w10, w5, w13) \
+    M_32(w13, w11, w6, w14) \
+    M_32(w14, w12, w7, w15) \
+    M_32(w15, w13, w8, w0)
+
+#define F_32(w, k)                                   \
+    T1 = h + Sigma1_32(e) + Ch(e, f, g) + (k) + (w); \
+    T2 = Sigma0_32(a) + Maj(a, b, c);                \
+    h = g;                                           \
+    g = f;                                           \
+    f = e;                                           \
+    e = d + T1;                                      \
+    d = c;                                           \
+    c = b;                                           \
+    b = a;                                           \
+    a = T1 + T2;
+
+static size_t crypto_hashblocks_sha256(uint8_t *statebytes,
+                                       const uint8_t *in, size_t inlen) {
+    uint32_t state[8];
+    uint32_t a;
+    uint32_t b;
+    uint32_t c;
+    uint32_t d;
+    uint32_t e;
+    uint32_t f;
+    uint32_t g;
+    uint32_t h;
+    uint32_t T1;
+    uint32_t T2;
+
+    a = load_bigendian_32(statebytes + 0);
+    state[0] = a;
+    b = load_bigendian_32(statebytes + 4);
+    state[1] = b;
+    c = load_bigendian_32(statebytes + 8);
+    state[2] = c;
+    d = load_bigendian_32(statebytes + 12);
+    state[3] = d;
+    e = load_bigendian_32(statebytes + 16);
+    state[4] = e;
+    f = load_bigendian_32(statebytes + 20);
+    state[5] = f;
+    g = load_bigendian_32(statebytes + 24);
+    state[6] = g;
+    h = load_bigendian_32(statebytes + 28);
+    state[7] = h;
+
+    while (inlen >= 64) {
+        uint32_t w0  = load_bigendian_32(in + 0);
+        uint32_t w1  = load_bigendian_32(in + 4);
+        uint32_t w2  = load_bigendian_32(in + 8);
+        uint32_t w3  = load_bigendian_32(in + 12);
+        uint32_t w4  = load_bigendian_32(in + 16);
+        uint32_t w5  = load_bigendian_32(in + 20);
+        uint32_t w6  = load_bigendian_32(in + 24);
+        uint32_t w7  = load_bigendian_32(in + 28);
+        uint32_t w8  = load_bigendian_32(in + 32);
+        uint32_t w9  = load_bigendian_32(in + 36);
+        uint32_t w10 = load_bigendian_32(in + 40);
+        uint32_t w11 = load_bigendian_32(in + 44);
+        uint32_t w12 = load_bigendian_32(in + 48);
+        uint32_t w13 = load_bigendian_32(in + 52);
+        uint32_t w14 = load_bigendian_32(in + 56);
+        uint32_t w15 = load_bigendian_32(in + 60);
+
+        F_32(w0, 0x428a2f98)
+        F_32(w1, 0x71374491)
+        F_32(w2, 0xb5c0fbcf)
+        F_32(w3, 0xe9b5dba5)
+        F_32(w4, 0x3956c25b)
+        F_32(w5, 0x59f111f1)
+        F_32(w6, 0x923f82a4)
+        F_32(w7, 0xab1c5ed5)
+        F_32(w8, 0xd807aa98)
+        F_32(w9, 0x12835b01)
+        F_32(w10, 0x243185be)
+        F_32(w11, 0x550c7dc3)
+        F_32(w12, 0x72be5d74)
+        F_32(w13, 0x80deb1fe)
+        F_32(w14, 0x9bdc06a7)
+        F_32(w15, 0xc19bf174)
+
+        EXPAND_32
+
+        F_32(w0, 0xe49b69c1)
+        F_32(w1, 0xefbe4786)
+        F_32(w2, 0x0fc19dc6)
+        F_32(w3, 0x240ca1cc)
+        F_32(w4, 0x2de92c6f)
+        F_32(w5, 0x4a7484aa)
+        F_32(w6, 0x5cb0a9dc)
+        F_32(w7, 0x76f988da)
+        F_32(w8, 0x983e5152)
+        F_32(w9, 0xa831c66d)
+        F_32(w10, 0xb00327c8)
+        F_32(w11, 0xbf597fc7)
+        F_32(w12, 0xc6e00bf3)
+        F_32(w13, 0xd5a79147)
+        F_32(w14, 0x06ca6351)
+        F_32(w15, 0x14292967)
+
+        EXPAND_32
+
+        F_32(w0, 0x27b70a85)
+        F_32(w1, 0x2e1b2138)
+        F_32(w2, 0x4d2c6dfc)
+        F_32(w3, 0x53380d13)
+        F_32(w4, 0x650a7354)
+        F_32(w5, 0x766a0abb)
+        F_32(w6, 0x81c2c92e)
+        F_32(w7, 0x92722c85)
+        F_32(w8, 0xa2bfe8a1)
+        F_32(w9, 0xa81a664b)
+        F_32(w10, 0xc24b8b70)
+        F_32(w11, 0xc76c51a3)
+        F_32(w12, 0xd192e819)
+        F_32(w13, 0xd6990624)
+        F_32(w14, 0xf40e3585)
+        F_32(w15, 0x106aa070)
+
+        EXPAND_32
+
+        F_32(w0, 0x19a4c116)
+        F_32(w1, 0x1e376c08)
+        F_32(w2, 0x2748774c)
+        F_32(w3, 0x34b0bcb5)
+        F_32(w4, 0x391c0cb3)
+        F_32(w5, 0x4ed8aa4a)
+        F_32(w6, 0x5b9cca4f)
+        F_32(w7, 0x682e6ff3)
+        F_32(w8, 0x748f82ee)
+        F_32(w9, 0x78a5636f)
+        F_32(w10, 0x84c87814)
+        F_32(w11, 0x8cc70208)
+        F_32(w12, 0x90befffa)
+        F_32(w13, 0xa4506ceb)
+        F_32(w14, 0xbef9a3f7)
+        F_32(w15, 0xc67178f2)
+
+        a += state[0];
+        b += state[1];
+        c += state[2];
+        d += state[3];
+        e += state[4];
+        f += state[5];
+        g += state[6];
+        h += state[7];
+
+        state[0] = a;
+        state[1] = b;
+        state[2] = c;
+        state[3] = d;
+        state[4] = e;
+        state[5] = f;
+        state[6] = g;
+        state[7] = h;
+
+        in += 64;
+        inlen -= 64;
+    }
+
+    store_bigendian_32(statebytes + 0, state[0]);
+    store_bigendian_32(statebytes + 4, state[1]);
+    store_bigendian_32(statebytes + 8, state[2]);
+    store_bigendian_32(statebytes + 12, state[3]);
+    store_bigendian_32(statebytes + 16, state[4]);
+    store_bigendian_32(statebytes + 20, state[5]);
+    store_bigendian_32(statebytes + 24, state[6]);
+    store_bigendian_32(statebytes + 28, state[7]);
+
+    return inlen;
+}
+
+static const uint8_t iv_256[32] = {
+    0x6a, 0x09, 0xe6, 0x67, 0xbb, 0x67, 0xae, 0x85,
+    0x3c, 0x6e, 0xf3, 0x72, 0xa5, 0x4f, 0xf5, 0x3a,
+    0x51, 0x0e, 0x52, 0x7f, 0x9b, 0x05, 0x68, 0x8c,
+    0x1f, 0x83, 0xd9, 0xab, 0x5b, 0xe0, 0xcd, 0x19
+};
+
+void sha256_inc_init(sha256ctx *state) {
+    state->ctx = (uint8_t*)malloc(SHA256CTX_BYTES);
+    if (state->ctx == NULL) {
+        exit(111);
+    }
+    for (size_t i = 0; i < 32; ++i) {
+        state->ctx[i] = iv_256[i];
+    }
+    for (size_t i = 32; i < 40; ++i) {
+        state->ctx[i] = 0;
+    }
+}
+
+void sha256_inc_ctx_clone(sha256ctx *stateout, const sha256ctx *statein) {
+    stateout->ctx = (uint8_t*)malloc(SHA256CTX_BYTES);
+    if (stateout->ctx == NULL) {
+        exit(111);
+    }
+    memcpy(stateout->ctx, statein->ctx, SHA256CTX_BYTES);
+}
+
+/* Destroy the hash state. */
+void sha256_inc_ctx_release(sha256ctx *state) {
+    free(state->ctx);
+}
+
+void sha256_inc_blocks(sha256ctx *state, const uint8_t *in, size_t inblocks) {
+    uint64_t bytes = load_bigendian_64(state->ctx + 32);
+
+    crypto_hashblocks_sha256(state->ctx, in, 64 * inblocks);
+    bytes += 64 * inblocks;
+
+    store_bigendian_64(state->ctx + 32, bytes);
+}
+
+void sha256_inc_finalize(uint8_t *out, sha256ctx *state, const uint8_t *in, size_t inlen) {
+    uint8_t padded[128];
+    uint64_t bytes = load_bigendian_64(state->ctx + 32) + inlen;
+
+    crypto_hashblocks_sha256(state->ctx, in, inlen);
+    in += inlen;
+    inlen &= 63;
+    in -= inlen;
+
+    for (size_t i = 0; i < inlen; ++i) {
+        padded[i] = in[i];
+    }
+    padded[inlen] = 0x80;
+
+    if (inlen < 56) {
+        for (size_t i = inlen + 1; i < 56; ++i) {
+            padded[i] = 0;
+        }
+        padded[56] = (uint8_t) (bytes >> 53);
+        padded[57] = (uint8_t) (bytes >> 45);
+        padded[58] = (uint8_t) (bytes >> 37);
+        padded[59] = (uint8_t) (bytes >> 29);
+        padded[60] = (uint8_t) (bytes >> 21);
+        padded[61] = (uint8_t) (bytes >> 13);
+        padded[62] = (uint8_t) (bytes >> 5);
+        padded[63] = (uint8_t) (bytes << 3);
+        crypto_hashblocks_sha256(state->ctx, padded, 64);
+    } else {
+        for (size_t i = inlen + 1; i < 120; ++i) {
+            padded[i] = 0;
+        }
+        padded[120] = (uint8_t) (bytes >> 53);
+        padded[121] = (uint8_t) (bytes >> 45);
+        padded[122] = (uint8_t) (bytes >> 37);
+        padded[123] = (uint8_t) (bytes >> 29);
+        padded[124] = (uint8_t) (bytes >> 21);
+        padded[125] = (uint8_t) (bytes >> 13);
+        padded[126] = (uint8_t) (bytes >> 5);
+        padded[127] = (uint8_t) (bytes << 3);
+        crypto_hashblocks_sha256(state->ctx, padded, 128);
+    }
+
+    for (size_t i = 0; i < 32; ++i) {
+        out[i] = state->ctx[i];
+    }
+    sha256_inc_ctx_release(state);
+}
+
+void sha256(uint8_t *out, const uint8_t *in, size_t inlen) {
+    sha256ctx state;
+
+    sha256_inc_init(&state);
+    sha256_inc_finalize(out, &state, in, inlen);
+}
+
+#endif // WITHOUT_OPENSSL
