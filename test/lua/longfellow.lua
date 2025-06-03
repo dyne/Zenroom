b68fb36ba3e4eeb4476c9711cfcc58f9de206302
Jaromil <jaromil@dyne.org>
2025-06-03 18:48:32 +0200

    run more builtin tests

--- test/lua/longfellow.lua
+++ test/lua/longfellow.lua
@@ -2,38 +2,80 @@
 
 ZK = require'crypto_longfellow'
 
-example = ZK.mdoc_example(1)
-I.schema({example = example})
+print'ZK Circuit generation... please wait...'
+local circ <const> = ZK.generate_circuit(1)
+I.schema({system = circ.system:string(),
+          circuit = circ})
 
-circ = ZK.generate_circuit(1)
-print(ZK.circuit_id(circ))
+-- calculate circuit hash and check that is same as zk_spec hash
+assert( ZK.circuit_id(circ) == circ.hash )
 
--- single
-attributes = {
-    {
-        id = 'age_over_18',
-        value = ZK.yes
-    }
-}
+-- test attributes
+age_over_18 = {{ id = 'age_over_18', value = ZK.yes }}
+not_over_18 = {{ id = 'age_over_18', value = ZK.no }}
+familyname_mustermann = {{ id = 'family_name', value = 'Mustermann' }}
+birthdate_1971_09_01 = {{ id = 'birth_date', value = '1971-09-01' }}
+birthdate_1998_09_04 = {{ id = 'birth_date', value = '1998-09-04' }}
+height_175 = {{ id = 'heighth', value = O.from_hex'18af' }} -- typo?!
 
-print'generate proof'
-proof = ZK.mdoc_prover(circ,
-                       example.mdoc,
-                       example.pkx,
-                       example.pky,
-                       example.transcript, -- goes into proof
-                       attributes,
-                       example.now)
-I.schema({proof=proof})
-print'verify proof'
-assert( ZK.mdoc_verifier(circ,
-                         proof,
-                         example.pkx,
-                         example.pky,
-                         attributes,
-                         example.now,
-                         example.doc_type
-) )
+-- successful
+for i = 1, 3 do
+    print''
+    print('EXAMPLE: '..i)
+    local example <const> = ZK.mdoc_example(i)
+    I.schema({doc_type = example.doc_type:string(),
+              example = example})
+    print'=============='
+    print'generate proof'
+    proof = ZK.mdoc_prover(circ,
+                           example.mdoc,
+                           example.pkx,
+                           example.pky,
+                           example.transcript, -- goes into proof
+                           age_over_18,
+                           example.now)
+    I.schema({proof=proof})
+    print'============'
+    print'verify proof'
+    local res <const> = ZK.mdoc_verifier(circ,
+                                         proof,
+                                         example.pkx,
+                                         example.pky,
+                                         age_over_18,
+                                         example.now,
+                                         example.doc_type)
+    print''
+end
+
+-- wrong witness
+for i = 1, 3 do
+    print''
+    print('EXAMPLE: '..i)
+    local example <const> = ZK.mdoc_example(i)
+    I.schema({doc_type = example.doc_type:string(),
+              example = example})
+    print'=============='
+    print'generate proof'
+    proof = ZK.mdoc_prover(circ,
+                           example.mdoc,
+                           example.pkx,
+                           example.pky,
+                           example.transcript, -- goes into proof
+                           not_over_18,
+                           example.now)
+    I.schema({proof=proof})
+    print'============'
+    print'verify proof'
+    local res <const> = ZK.mdoc_verifier(circ,
+                                         proof,
+                                         example.pkx,
+                                         example.pky,
+                                         not_over_18,
+                                         example.now,
+                                         example.doc_type)
+    print''
+end
+
 
 -- circ = LFZK.gen_circuit(2)
 -- describe_circuit('v2',circ)
