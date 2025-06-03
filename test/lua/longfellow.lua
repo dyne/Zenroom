51a8585da31e1748242550c8cc50e530e851e9ec
Jaromil <jaromil@dyne.org>
2025-06-03 16:04:38 +0200

    new lua wrapper in crypto_longfellow
    this will make it easier to extend structures, carry metadata (as with
    zkspec along the circuit) and check incoming arguments for validity

--- test/lua/longfellow.lua
+++ test/lua/longfellow.lua
@@ -1,26 +1,20 @@
 -- test the lib/longfellow-zk primitives in zenroom lua
 
-function describe_circuit(ver, circuit)
-    local size = #circuit
-    print("circuit size: "..size)
-    print("recalculated circuit id:")
-    print(LFZK.circuit_id(circuit, ver):hex())
-end
-LFZK = require'longfellow'
+ZK = require'crypto_longfellow'
 
-example = LFZK.mdoc_example(1)
+example = I.spy(ZK.mdoc_example(1))
 
-circ = LFZK.gen_circuit(1)
-describe_circuit('1',circ)
+circ = ZK.generate_circuit(1)
+print(ZK.circuit_id(circ))
 
 print'generate proof'
-proof = LFZK.mdoc_prove(circ,
-                        example.mdoc,
-                        example.pkx,
-                        example.pky,
-                        example.transcript,
-                        {{id='age_over_18',value=O.from_hex'f5'}},
-                        example.now, 1)
+proof = ZK.mdoc_prover(circ,
+                      example.mdoc,
+                      example.pkx,
+                      example.pky,
+                      example.transcript,
+                      {{id='age_over_18',value=O.from_hex'f5'}},
+                      example.now)
 
 -- circ = LFZK.gen_circuit(2)
 -- describe_circuit('v2',circ)
