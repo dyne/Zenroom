b1f70253a018f8d05de7a00e3e079bd15c7a8612
matteo-cristino <cristino.matteo97@gmail.com>
2025-06-20 11:27:17 +0200

    chore: add missing license headers

--- src/lua/crypto_loader.lua
+++ src/lua/crypto_loader.lua
@@ -1,3 +1,24 @@
+--[[
+--This file is part of zenroom
+--
+--Copyright (C) 2021-2025 Dyne.org foundation
+--designed, written and maintained by Denis Roio <jaromil@dyne.org>
+--
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU Affero General Public License v3.0
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU Affero General Public License for more details.
+--
+--Along with this program you should have received a copy of the
+--GNU Affero General Public License v3.0
+--If not, see http://www.gnu.org/licenses/agpl.txt
+--
+--Last modified by Matteo Cristino
+--on Friday, 20th June 2025
+--]]
 
 local crypto = {
     algos = {
