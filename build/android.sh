8583682ce569cb07f70e35a004a9bed92a8ea95a
Jaromil <jaromil@dyne.org>
2025-06-24 00:00:35 +0200

    fix: package android's aar with classes.jar and manifest
    easier way to package jni along with wrapper classes

--- build/android.sh
+++ build/android.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 [ -r ${ANDROID_NDK_HOME}/README.md ] || {
-	>&3 echo "ANDROID_NDK_HOME environment not set"
+	>&2 echo "ANDROID_NDK_HOME environment not set"
 	exit 1
 }
 
 command -v x86_64-linux-android21-clang > /dev/null || {
-	>&3 echo "ANDROID_NDK binaries not found in path: x86_64-linux-android21-clang"
+	>&2 echo "ANDROID_NDK binaries not found in path: x86_64-linux-android21-clang"
 	exit 1
 }
 
@@ -31,23 +31,26 @@ build() {
 		cflags="$cflags -mpclmul"
 		abi="x86_64"
 	}
-	# make clean
-	mkdir -p zenroom-android/${1}
+	make clean
+	rm -f bindings/java/zenroom_jni.o
+	mkdir -p zenroom-android/jni/${abi}
 	make -f build/android.mk all \
 		 COMPILER=${target}-linux-${platform}-clang \
 		 COMPILER_CXX=${target}-linux-${platform}-clang++ \
 		 longfellow_cflags="${cflags}" \
-		 ANDROID_ABI="${abi}" ANDROID_TARGET="${1}" \
+		 ANDROID_ABI="${abi}" ANDROID_TARGET="${target}" \
 		 ANDROID_PLATFORM="${platform}" \
 		 RELEASE=1
-
-	cp -v libzenroom.so zenroom-android/${1}/
+	cp -v libzenroom.so zenroom-android/jni/${abi}/
 }
 
-#build x86_64
-#build aarch64
+build x86_64
+build aarch64
 build i686
-#build armv7a
+build armv7a
 
->&3 echo "Build done!"
->&3 tree zenroom-android
+cp -v bindings/java/classes.jar zenroom-android/
+cd zenroom-android && zip -r -9 ../zenroom-android.aar .; cd -
+>&2 echo "=== Android build done:"
+>&2 ls -l zenroom-android.aar
+>&2 tree zenroom-android
