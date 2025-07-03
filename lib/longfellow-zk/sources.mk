1295f96fab81aa5182dac848d28380a20aa2d4c4
Jaromil <jaromil@dyne.org>
2025-07-03 20:03:38 +0200

    fix: missing build files in longfellow
    add sources.mk generated by import.sh script
    
    ccache use with C++ compilers in longfellow-zk
    
    fix zencode test unit
    
    matrix github action for c++

---
+++ lib/longfellow-zk/sources.mk
@@ -0,0 +1,15 @@
+SOURCES := \
+util/crypto.cc.o \
+util/log.cc.o \
+ec/p256.cc.o \
+algebra/nat.cc.o \
+circuits/sha/flatsha256_witness.cc.o \
+circuits/sha/sha256_constants.cc.o \
+circuits/base64/decode_util.cc.o \
+circuits/mdoc/mdoc_zk.cc.o \
+circuits/mdoc/zk_spec.cc.o \
+circuits/sha3/sha3_reference.cc.o \
+circuits/sha3/sha3_round_constants.cc.o \
+util/sha256.cc.o    \
+util/aes_ecb.cc.o    \
+
