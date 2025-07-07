eb7f45b524936c0d0bf33f40ea4282140a19eb22
Jaromil <jaromil@dyne.org>
2025-07-07 16:57:05 +0200

    use our own musl toolchain

--- build/musl.mk
+++ build/musl.mk
@@ -1,27 +1,12 @@
 ## Initialize build defaults
 include build/init.mk
 
-BUILD_DEPS := apply-patches milagro lua54 embed-lua mlkem	\
-				quantum-proof ed25519-donna longfellow-zk zstd
-
-COMPILER := musl-gcc
+COMPILER := /opt/musl-cross-dyne/bin/x86_64-linux-musl-gcc
+COMPILER_CXX := /opt/musl-cross-dyne/bin/x86_64-linux-musl-g++
 
 cflags += -static -std=gnu99 -fPIC -D'ARCH="MUSL"' -D__MUSL__ -DARCH_MUSL
 ldflags += -static
 system := Linux
-
-ldadd := ${pwd}/lib/lua54/src/liblua.a
-ldadd += ${milib}/libamcl_curve_${ecp_curve}.a
-ldadd += ${milib}/libamcl_pairing_${ecp_curve}.a
-ldadd += ${milib}/libamcl_curve_${ecdh_curve}.a
-ldadd += ${milib}/libamcl_rsa_2048.a ${milib}/libamcl_rsa_4096.a
-ldadd += ${milib}/libamcl_x509.a
-ldadd += ${milib}/libamcl_core.a
-ldadd += ${pwd}/lib/pqclean/libqpz.a
-ldadd += ${pwd}/lib/ed25519-donna/libed25519.a
-ldadd += ${pwd}/lib/mlkem/test/build/libmlkem.a
-ldadd += ${pwd}/lib/longfellow-zk/liblongfellow-zk.a
-ldadd += ${pwd}/lib/zstd/libzstd.a
 
 # activate CCACHE etc.
 include build/plugins.mk
@@ -33,12 +18,12 @@ deps: ${BUILD_DEPS}
 cli_sources := src/cli-zenroom.o src/repl.o
 zenroom: ${ZEN_SOURCES} ${cli_sources}
 	$(info === Building the zenroom CLI)
-	${zenroom_cc} ${cflags} ${ZEN_SOURCES} ${cli_sources} \
+	${cxx} ${cflags} ${ZEN_SOURCES} ${cli_sources} \
 		-o $@ ${ldflags} ${ldadd}
 
 zencode-exec: ${ZEN_SOURCES} src/zencode-exec.o
 	$(info === Building the zencode-exec utility)
-	${zenroom_cc} ${cflags} ${ZEN_SOURCES} src/zencode-exec.o \
+	${cxx} ${cflags} ${ZEN_SOURCES} src/zencode-exec.o \
 		-o $@ ${ldflags} ${ldadd}
 
 include build/deps.mk
