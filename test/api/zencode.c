d5f1d4fe63d6720bd5ce6a407d28b6db201e7239
matteo-cristino <cristino.matteo97@gmail.com>
2025-07-24 12:23:33 +0200

    test(api): replicate failing zencode when called multiple times

--- test/api/zencode.c
+++ test/api/zencode.c
@@ -4,7 +4,17 @@
 
 int main(int argc, char **argv) {
 	int res;
-	res = zencode_exec(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
-	if(res!=0){fprintf(stderr,"Abort on error code %u\n",res);exit(res);}
+	// default iteration is 1
+	int iterations = 1;
+	if(argc == 8) {
+		iterations = atoi(argv[7]);
+	}
+	for(int i = 0; i < iterations; i++) {
+		res = zencode_exec(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
+		if(res!=0) {
+			fprintf(stderr,"Abort on error code %u\n",res);
+			exit(res);
+		}
+	}
 	exit(0);
 }
