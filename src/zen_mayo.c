0dbb932014f5347fe950874daf071f595e3ccb7a
ms8421ms <109365501+ms8421ms@users.noreply.github.com>
2026-03-13 22:10:29 +0100

    chore: create zen_mayo

---
+++ src/zen_mayo.c
@@ -0,0 +1,191 @@
+#include <zenroom.h>
+#include <zen_error.h>
+#include <lua_functions.h>
+#include <zen_octet.h>
+
+// MAYO parameters and build configuration
+#define MAYO_VARIANT MAYO_5
+#define MAYO_BUILD_TYPE_REF 1
+
+#include "api.h"
+
+static int zen_mayo_keygen(lua_State *L) {
+    BEGIN();
+    char *failed_msg = NULL;
+    lua_createtable(L, 0, 2);
+    
+    int sk_len = CRYPTO_SECRETKEYBYTES; 
+    int pk_len = CRYPTO_PUBLICKEYBYTES;
+
+    octet *private = o_new(L, sk_len); SAFE_GOTO(private, "Could not create private key");
+    lua_setfield(L, -2, "private");
+    octet *public = o_new(L, pk_len); SAFE_GOTO(public, "Could not create public key");
+    lua_setfield(L, -2, "public");
+
+    crypto_sign_keypair((unsigned char*)public->val, (unsigned char*)private->val);
+    public->len = pk_len;
+    private->len = sk_len;
+
+end:
+    if(failed_msg) {
+        THROW(failed_msg);
+    }
+    END(1);
+}
+
+static int zen_mayo_sign(lua_State *L) {
+    BEGIN();
+    char *failed_msg = NULL;
+    const octet *sk = NULL, *m = NULL;
+    sk = o_arg(L, 1); SAFE_GOTO(sk, "Could not allocate secret key");
+    SAFE_GOTO(sk->len == CRYPTO_SECRETKEYBYTES, "Invalid size for MAYO_5 secret key");
+    m = o_arg(L, 2); SAFE_GOTO(m, "Could not allocate message");
+    octet *sig = o_new(L, CRYPTO_BYTES); SAFE_GOTO(sig, "Could not create signature");
+    SAFE_GOTO(
+        !crypto_sign_signature(
+            (unsigned char*)sig->val,
+            (size_t*)&sig->len,
+            (unsigned char*)m->val, m->len,
+            (unsigned char*)sk->val
+        ) || sig->len <= 0,
+        "Could not sign the message"
+    );
+end:
+    o_free(L, m);
+    o_free(L, sk);
+    if(failed_msg) {
+        THROW(failed_msg);
+    }
+    END(1);
+}
+
+static int zen_mayo_signature_pubcheck(lua_State *L) {
+    BEGIN();
+    const octet *pk = o_arg(L, 1); SAFE(pk, "Could not allocate public key");
+    if(pk->len == CRYPTO_PUBLICKEYBYTES)
+        lua_pushboolean(L, 1);
+    else
+        lua_pushboolean(L, 0);
+    o_free(L, pk);
+    END(1);
+}
+
+static int zen_mayo_signed_message(lua_State *L) {
+    BEGIN();
+    char *failed_msg = NULL;
+    const octet *sk = NULL, *m = NULL;
+    sk = o_arg(L, 1); SAFE_GOTO(sk, "Could not allocate secret key");
+    SAFE_GOTO(sk->len == CRYPTO_SECRETKEYBYTES, "Invalid size for MAYO_5 secret key");
+    m = o_arg(L, 2); SAFE_GOTO(m, "Could not allocate message");
+    octet *sig = o_new(L, CRYPTO_BYTES + m->len); SAFE_GOTO(sig, "Could not create signature");
+    SAFE_GOTO(
+        !crypto_sign(
+            (unsigned char*)sig->val,
+            (size_t*)&sig->len,
+            (unsigned char*)m->val, m->len,
+            (unsigned char*)sk->val
+        ) || sig->len <= 0,
+        "Could not sign the message"
+    );
+end:
+    o_free(L, m);
+    o_free(L, sk);
+    if(failed_msg) {
+		THROW(failed_msg);
+	}
+	END(1);
+}
+
+static int zen_mayo_verified_message(lua_State *L) {
+    BEGIN();
+    char *failed_msg = NULL;
+    const octet *pk = NULL, *sm = NULL;
+    pk = o_arg(L, 1); SAFE_GOTO(pk, "Could not allocate public key");
+    SAFE_GOTO(pk->len == CRYPTO_PUBLICKEYBYTES, "Invalid size for MAYO_5 public key");
+    sm = o_arg(L, 2); SAFE_GOTO(sm, "Could not allocate signed message");
+    octet *msg = o_new(L, sm->len); SAFE_GOTO(msg, "Could not create message");
+    int result = crypto_sign_open(
+        (unsigned char*)msg->val,
+        (size_t*)&msg->len,
+        (unsigned char*)sm->val, sm->len,
+        (unsigned char*)pk->val) == 0
+        && msg->len > 0;
+    if(!result) {
+        lua_pushboolean(L, 0);
+    }
+end:
+    o_free(L, sm);
+    o_free(L, pk);
+    if(failed_msg) {
+        THROW(failed_msg);
+    }
+    END(1);
+}
+
+static int zen_mayo_verify(lua_State *L) {
+    BEGIN();
+    char *failed_msg = NULL;
+    const octet *pk = NULL, *sig = NULL, *m = NULL;
+    pk = o_arg(L, 1); SAFE_GOTO(pk, "Could not allocate public key");
+    SAFE_GOTO(pk->len == CRYPTO_PUBLICKEYBYTES, "Invalid size for MAYO_5 public key");
+    sig = o_arg(L, 2); SAFE_GOTO(sig, "Could not allocate signature");
+    m = o_arg(L, 3); SAFE_GOTO(m, "Could not allocate message");
+    int result = crypto_sign_verify(
+        (unsigned char*)sig->val,
+        (size_t)sig->len,
+        (unsigned char*)m->val, m->len,
+        (unsigned char*)pk->val
+    );
+    lua_pushboolean(L, result == 0);
+end:
+    o_free(L, m);
+    o_free(L, sig);
+    o_free(L, pk);
+    if(failed_msg) {
+        THROW(failed_msg);
+    }
+    END(1);
+}
+
+static int zen_mayo_signature_len(lua_State *L){
+    BEGIN();
+    lua_pushinteger(L, CRYPTO_BYTES);
+    END(1);
+}
+
+static int zen_mayo_signature_check(lua_State *L) {
+    BEGIN();
+	char *failed_msg = NULL;
+	const octet *sign = o_arg(L, 1); SAFE_GOTO(sign, "Could not allocate signature");
+	if(sign->len == CRYPTO_BYTES)
+		lua_pushboolean(L, 1);
+	else
+		lua_pushboolean(L, 0);
+end:
+	o_free(L, sign);
+	if(failed_msg) {
+		THROW(failed_msg);
+	}
+	END(1);
+}
+
+int luaopen_mayo(lua_State *L) {
+    const struct luaL_Reg mayo_class[] = {
+        {"sigkeygen", zen_mayo_keygen},
+        {"sigpubcheck", zen_mayo_signature_pubcheck},
+        {"sign", zen_mayo_sign},
+        {"verify", zen_mayo_verify},
+        {"signed_msg", zen_mayo_signed_message},
+        {"verified_msg", zen_mayo_verified_message},
+        {"signature_len", zen_mayo_signature_len},
+        {"signature_check", zen_mayo_signature_check},
+        {NULL, NULL}
+    };
+    
+    const struct luaL_Reg mayo_methods[] = {
+        {NULL, NULL}
+    };
+
+    zen_add_class(L, "mayo", mayo_class, mayo_methods);
+    return 1;
+}
