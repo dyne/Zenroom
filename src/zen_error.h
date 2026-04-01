843a22afd8e84310be9087dec492197a976ac2a1
Jaromil <jaromil@dyne.org>
2026-04-02 01:15:27 +0200

    refactor: share native raw write helper

--- src/zen_error.h
+++ src/zen_error.h
@@ -88,6 +88,7 @@ typedef enum log_priority {
 #define LOG_DEFAULT " .   "
 
 void get_log_prefix(void *Z, log_priority prio, char dest[5]);
+void zen_raw_write(int fd, const void *buf, size_t count);
 
 // context free print and error messages
 HEDLEY_PRINTF_FORMAT(1,2)
