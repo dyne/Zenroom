00becb8a6a224393db3f7a74c145db6bee5da04c
Jaromil <jaromil@dyne.org>
2026-04-03 17:28:23 +0200

    fix(go): avoid pipe race when capturing binding output

--- bindings/golang/zenroom/zenroom.go
+++ bindings/golang/zenroom/zenroom.go
@@ -1,9 +1,8 @@
 package zenroom
 
 import (
+	"bytes"
 	b64 "encoding/base64"
-	"io"
-	"log"
 	"os/exec"
 	"strings"
 )
@@ -12,6 +11,8 @@ type ZenResult struct {
 	Output string
 	Logs   string
 }
+
+var execCommand = exec.Command
 
 func ZencodeExec(script string, conf string, keys string, data string) (ZenResult, bool) {
 	return ZenExecExtra("zencode-exec", script, conf, keys, data, "", "")
@@ -24,65 +25,28 @@ func LuaExec(script string, conf string, keys string, data string, extra string,
 }
 
 func ZenExecExtra(aux string, script string, conf string, keys string, data string, extra string, context string) (ZenResult, bool) {
-	execCmd := exec.Command(aux)
+	execCmd := execCommand(aux)
+	var stdin strings.Builder
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
 
-	stdout, err := execCmd.StdoutPipe()
-	if err != nil {
-		return ZenResult{}, false
-	}
+	stdin.WriteString(conf)
+	stdin.WriteString("\n")
+	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(script)))
+	stdin.WriteString("\n")
+	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(keys)))
+	stdin.WriteString("\n")
+	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(data)))
+	stdin.WriteString("\n")
+	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(extra)))
+	stdin.WriteString("\n")
+	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(context)))
+	stdin.WriteString("\n")
 
-	stderr, err := execCmd.StderrPipe()
-	if err != nil {
-		return ZenResult{}, false
-	}
+	execCmd.Stdin = strings.NewReader(stdin.String())
+	execCmd.Stdout = &stdout
+	execCmd.Stderr = &stderr
 
-	stdin, err := execCmd.StdinPipe()
-	if err != nil {
-		return ZenResult{}, false
-	}
-
-	// Start first so the child can read from stdin
-	if err := execCmd.Start(); err != nil {
-		stdin.Close()
-		return ZenResult{}, false
-	}
-
-	stdoutCh := make(chan string, 1)
-	stderrCh := make(chan string, 1)
-	go captureOutput(stdout, stdoutCh)
-	go captureOutput(stderr, stderrCh)
-
-	go func() {
-		defer stdin.Close()
-		io.WriteString(stdin, conf)
-		io.WriteString(stdin, "\n")
-		io.WriteString(stdin, b64.StdEncoding.EncodeToString([]byte(script)))
-		io.WriteString(stdin, "\n")
-		io.WriteString(stdin, b64.StdEncoding.EncodeToString([]byte(keys)))
-		io.WriteString(stdin, "\n")
-		io.WriteString(stdin, b64.StdEncoding.EncodeToString([]byte(data)))
-		io.WriteString(stdin, "\n")
-		io.WriteString(stdin, b64.StdEncoding.EncodeToString([]byte(extra)))
-		io.WriteString(stdin, "\n")
-		io.WriteString(stdin, b64.StdEncoding.EncodeToString([]byte(context)))
-		io.WriteString(stdin, "\n")
-	}()
-
-	err = execCmd.Wait()
-	stdoutStr := <-stdoutCh
-	stderrStr := <-stderrCh
-
-	return ZenResult{Output: stdoutStr, Logs: stderrStr}, err == nil
-}
-
-func captureOutput(pipe io.ReadCloser, output chan<- string) {
-	defer close(output)
-
-	buf := new(strings.Builder)
-	_, err := io.Copy(buf, pipe)
-	if err != nil {
-		log.Printf("Failed to capture output: %v", err)
-		return
-	}
-	output <- buf.String()
+	err := execCmd.Run()
+	return ZenResult{Output: stdout.String(), Logs: stderr.String()}, err == nil
 }
