5b8a8d8497019dfd6d258306de562f07cbfe6ba4
Jaromil <jaromil@dyne.org>
2026-04-03 18:35:46 +0200

    fix(golang): stream zenroom stdin payload

--- bindings/golang/zenroom/zenroom.go
+++ bindings/golang/zenroom/zenroom.go
@@ -3,8 +3,8 @@ package zenroom
 import (
 	"bytes"
 	b64 "encoding/base64"
+	"io"
 	"os/exec"
-	"strings"
 )
 
 type ZenResult struct {
@@ -26,27 +26,61 @@ func LuaExec(script string, conf string, keys string, data string, extra string,
 
 func ZenExecExtra(aux string, script string, conf string, keys string, data string, extra string, context string) (ZenResult, bool) {
 	execCmd := execCommand(aux)
-	var stdin strings.Builder
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
+	stdinReader, stdinWriter := io.Pipe()
+	writeErr := make(chan error, 1)
+	go func() {
+		defer close(writeErr)
+		err := writeRequestTo(stdinWriter, script, conf, keys, data, extra, context)
+		if err != nil {
+			_ = stdinWriter.CloseWithError(err)
+			writeErr <- err
+			return
+		}
+		writeErr <- stdinWriter.Close()
+	}()
 
-	stdin.WriteString(conf)
-	stdin.WriteString("\n")
-	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(script)))
-	stdin.WriteString("\n")
-	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(keys)))
-	stdin.WriteString("\n")
-	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(data)))
-	stdin.WriteString("\n")
-	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(extra)))
-	stdin.WriteString("\n")
-	stdin.WriteString(b64.StdEncoding.EncodeToString([]byte(context)))
-	stdin.WriteString("\n")
-
-	execCmd.Stdin = strings.NewReader(stdin.String())
+	execCmd.Stdin = stdinReader
 	execCmd.Stdout = &stdout
 	execCmd.Stderr = &stderr
 
 	err := execCmd.Run()
+	if closeErr := stdinReader.Close(); err == nil && closeErr != nil {
+		err = closeErr
+	}
+	if requestErr := <-writeErr; err == nil && requestErr != nil {
+		err = requestErr
+	}
 	return ZenResult{Output: stdout.String(), Logs: stderr.String()}, err == nil
+}
+
+func writeRequestTo(w io.Writer, script string, conf string, keys string, data string, extra string, context string) error {
+	if _, err := io.WriteString(w, conf); err != nil {
+		return err
+	}
+	if _, err := io.WriteString(w, "\n"); err != nil {
+		return err
+	}
+
+	for _, field := range []string{script, keys, data, extra, context} {
+		if err := writeBase64Line(w, field); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeBase64Line(w io.Writer, value string) error {
+	encoder := b64.NewEncoder(b64.StdEncoding, w)
+	if _, err := io.WriteString(encoder, value); err != nil {
+		_ = encoder.Close()
+		return err
+	}
+	if err := encoder.Close(); err != nil {
+		return err
+	}
+	_, err := io.WriteString(w, "\n")
+	return err
 }
