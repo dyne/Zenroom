0dc56706536ec07d7f6e16d2436bc9186ae1a6b3
Jaromil <jaromil@dyne.org>
2026-07-05 14:14:44 +0200

    build(longfellow): adapt sources.mk and GNUmakefile for dyne vendor
    Merge utility object files into sources.mk, add new dyne objects
    (crt.cc, p256k1.cc, mdoc_generate_circuit.cc, randombytes.cc),
    update base64 path to circuits/tests/base64, remove sha3 objects.
    Use -std=c++17 matching the dyne upstream default.
    Keep CFLAGS variable name for Zenroom build system compatibility.

--- lib/longfellow-zk/sources.mk
+++ lib/longfellow-zk/sources.mk
@@ -1,6 +1,7 @@
 SOURCES := \
 ec/p256.cc.o \
 algebra/nat.cc.o \
+algebra/crt.cc.o \
 circuits/sha/flatsha256_witness.cc.o \
 circuits/sha/sha256_constants.cc.o \
 circuits/tests/base64/decode_util.cc.o \
@@ -9,4 +10,8 @@ circuits/mdoc/zk_spec.cc.o \
 circuits/mdoc/mdoc_decompress.cc.o \
 circuits/mdoc/mdoc_generate_circuit.cc.o \
 ec/p256k1.cc.o \
-algebra/crt.cc.o \
+util/aes_ecb.cc.o \
+util/log.cc.o \
+util/sha256.cc.o \
+util/crypto.cc.o \
+util/randombytes.cc.o
